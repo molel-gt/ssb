62f34b9e9d036de4283a66ff2b747cf570d15da6
Leshinka Molel <emolel3@gatech.edu>
2021-07-22 18:27:00 -0400

    Reorganize study

--- dfn_half_cell.py
+++ dfn_half_cell.py
@@ -91,7 +91,6 @@ if __name__ == '__main__':
     #
     # Conduct study
     #
-    sims = []
 
     with open("study.csv", "w") as fp:
         writer = csv.DictWriter(fp, fieldnames=col_names)
@@ -117,13 +116,11 @@ if __name__ == '__main__':
                     "energy of cell [Wh.m-2]": energy, "cell energy density [Wh.kg-1]": energy / mass_cell
                 }
                 writer.writerow(row)
-                sims.append(sim)
 
-    select_sims = [sim for sim in sims in sim.name.endswith("8")]
-
-    # sim_files = [f for f in os.listdir(".") if f.startswith("L2") or f.startswith("L3") or f.startswith("L4") or f.startswith("L6") and f.endswith("8.pkl")]
-    # for sim_file in sim_files:
-    #     sim = pybamm.load(sim_file)
-    #     select_sims.append(sim)
+    select_sims = []
+    sim_files = [f for f in os.listdir(".") if f.startswith("L2") or f.startswith("L3") or f.startswith("L4") or f.startswith("L6") and f.endswith("8.pkl")]
+    for sim_file in sim_files:
+        sim = pybamm.load(sim_file)
+        select_sims.append(sim)
     pybamm.dynamic_plot(select_sims, output_variables=output_variables,
                         time_unit="hours", spatial_unit="um")
