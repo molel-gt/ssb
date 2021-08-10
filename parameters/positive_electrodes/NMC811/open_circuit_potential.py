3324945067adb291baf0f515fd1b2ecbdbd53646
Leshinka Molel <emolel3@gatech.edu>
2021-08-10 07:08:56 -0400

    latest changes

--- parameters/positive_electrodes/NMC811/open_circuit_potential.py
+++ parameters/positive_electrodes/NMC811/open_circuit_potential.py
@@ -71,20 +71,28 @@ def open_circuit_potential_min(sto):
     return u_eq
 
 
+def open_circuit_potential_sigmoid(sto):
+    u_eq = 1 / (pybamm.exp(-2 * (sto - 0.5)) + 1)
+
+    return u_eq
+
+
 def open_circuit_potential(sto):
     return open_circuit_potential_unif(sto)
 
 
 if __name__ == '__main__':
-    stos = np.linspace(0.01, 0.99, 1000)
+    stos = np.linspace(-0.99, 0.99, 1000)
     ocp_chen2020_voltages = [open_circuit_potential_chen2020(sto).value for sto in stos]
     ocp_unif_voltages = [open_circuit_potential_unif(sto).value for sto in stos]
     ocp_min_voltages = [open_circuit_potential_min(sto).value for sto in stos]
+    ocp_sigmoid_voltages = [open_circuit_potential_sigmoid(sto).value for sto in stos]
     fig, ax = plt.subplots()
     x_data = np.log(1 - stos)
     ax.plot(ocp_chen2020_voltages, x_data, label='NMC811 - Chen 2020')
     ax.plot(ocp_unif_voltages, x_data,  label='U = 4.7 - 1.2 * sto')
     ax.plot(ocp_min_voltages, x_data, label='U = 4.0 + 1.2 * (sto - 0.5) ** 2')
+    ax.plot(ocp_sigmoid_voltages, x_data, label='sigmoid')
     ax.set_ylabel("sto")
     ax.set_xlabel("OCP [V]")
     ax.set_title("Open Circuit Potential")
