0f59abed686cc5ba6a102873e1631604c3bb87f8
Leshinka Molel <emolel3@gatech.edu>
2021-09-12 13:35:48 -0400

    rename file

--- dfn.py
+++ dfn.py
@@ -1,56 +1,30 @@
 #!/usr/bin/env python3
 
 import pybamm
-import numpy as np
-
-
-pybamm.set_logging_level("INFO")
-
-options = {
-    # 'working electrode': 'both',
-}
-
-output_vars = ["Terminal voltage [V]",  # "Current density [A.m-2]",
-               "Pore-wall flux [mol.m-2.s-1]", "Electrolyte concentration [mol.m-3]",
-               "Electrolyte potential [V]",
-               [
-                   "Working electrode potential [V]", "Working electrode open circuit potential [V]",
-               ],
-               "Working particle surface concentration [mol.m-3]"]
 
 
 if __name__ == '__main__':
 
     # default parameters
-    chemistry = pybamm.parameter_sets.Molel2021
+    chemistry = pybamm.parameter_sets.Xu2019
     params = pybamm.ParameterValues(chemistry=chemistry)
-
-    params.update(
-        {
-            "Lithium counter electrode exchange-current density [A.m-2]": 12.6,
-            "Lithium counter electrode conductivity [S.m-1]": 1.0776e7,
-            "Lithium counter electrode thickness [m]": 50e-6,
-        },
-        check_already_exists=False,
-    )
 
     experiment = pybamm.Experiment(
         [
-            ("Discharge at C/5 for 5 hours or until 3.5 V",
-             "Rest for 1 hour",
-             "Charge at 1 A until 4.1 V",
+            (
+             "Discharge at C/5 for 5 hours or until 3.0 V",
+             "Rest for 3 hours",
+             "Charge at 1mA until 4.1 V",
              "Hold at 4.1 V until 50 mA",
-             "Rest for 1 hour"),
+             "Rest for 3 hours"
+             ),
         ] * 2
     )
 
-    t_max = 1 * 3600
-    t_eval = np.linspace(0, t_max, 1000)
+    model = pybamm.lithium_ion.BasicDFNHalfCell(options={"working electrode": "positive"})
+    solver = pybamm.CasadiSolver(mode="safe", atol=1e-6, rtol=1e-3)
+    sim = pybamm.Simulation(model, parameter_values=params,
+                            experiment=experiment, solver=solver)
 
-    model = pybamm.lithium_metal.DFN(name='dfn-assb', options=options)
-    safe_solver = pybamm.CasadiSolver(atol=1e-3, rtol=1e-3, mode="safe")
-    fast_solver = pybamm.CasadiSolver(mode="fast")
-    sim = pybamm.Simulation(model, parameter_values=params, experiment=experiment, solver=fast_solver)
     sim.solve()
-
-    sim.plot(time_unit="hours")
+    sim.plot(time_unit="seconds")
