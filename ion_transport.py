1033c010d275a196cb52c2750a9343984ad8528b
Leshinka Molel <emolel3@gatech.edu>
2021-10-16 15:33:43 -0400

    update

--- ion_transport.py
+++ ion_transport.py
@@ -1,9 +1,11 @@
 
 import dolfinx
+# from fenics import *
+import dolfin
 import numpy as np
 import ufl
 from dolfinx import (DirichletBC, Function, FunctionSpace, fem,
-                     UnitCubeMesh
+                     UnitCubeMesh, UnitIntervalMesh, BoxMesh, RectangleMesh
                      )
 from dolfinx.cpp.mesh import CellType
 from dolfinx.fem import locate_dofs_topological
@@ -15,18 +17,26 @@ from ufl import ds, dx, grad, inner
 
 
 # # Create mesh and define function space
-mesh = UnitCubeMesh(
-    MPI.COMM_WORLD, 90, 90, 90,
-    CellType.tetrahedron, dolfinx.cpp.mesh.GhostMode.none)
+# mesh = UnitCubeMesh(
+#     MPI.COMM_WORLD, 90, 90, 90,
+#     CellType.tetrahedron, dolfinx.cpp.mesh.GhostMode.none)
 # mesh = UnitIntervalMesh(MPI.COMM_WORLD, 3, dolfinx.cpp.mesh.GhostMode.none)
-with XDMFFile(MPI.COMM_WORLD, "mesh_tetr.xdmf", "r") as infile:
-    mesh = infile.read_mesh(dolfinx.cpp.mesh.GhostMode.none, 'Grid')
-print("done loading tetrahedral mesh")
+# mesh = RectangleMesh(
+#     MPI.COMM_WORLD,
+#     [np.array([0, 0, 0]), np.array([1, 1, 0])], [32, 32],
+#     CellType.triangle, dolfinx.cpp.mesh.GhostMode.none)
+mesh = BoxMesh(
+    MPI.COMM_WORLD, [np.array([0, 0, 0]), np.array([1, 1, 1])],
+    [10, 10, 10], CellType.tetrahedron, dolfinx.cpp.mesh.GhostMode.none)
+# with XDMFFile(MPI.COMM_WORLD, "mesh_tetr.xdmf", "r") as infile:
+#     mesh = infile.read_mesh(dolfinx.cpp.mesh.GhostMode.none, 'Grid')
+# print("done loading tetrahedral mesh")
 
-# boundaries = dol.MeshValueCollection(mesh, 2)
+# boundaries = dolfinx.cpp.mesh.MeshValueCollection("size_t", mesh, 2)
 # with XDMFFile(MPI.COMM_WORLD, "mesh_tria.xdmf") as infile:
 #     boundaries = infile.read_mvc_size_t(mesh, "all_tags")
 # print("done reading triangle mesh")
+
 V = FunctionSpace(mesh, ("Lagrange", 1))
 
 # Define boundary condition on x = 0 or x = 1
@@ -34,21 +44,22 @@ u0 = Function(V)
 with u0.vector.localForm() as u0_loc:
     u0_loc.set(0)
 u1 = Function(V)
+
 with u1.vector.localForm() as u1_loc:
     u1_loc.set(1)
 x0facet = locate_entities_boundary(mesh, 2,
-                                   lambda x: np.isclose(x[0], 0.0))
+                                   lambda x: np.isclose(x[0], 0))
 x1facet = locate_entities_boundary(mesh, 2,
-                                   lambda x: np.isclose(x[0], 1.0))
-x0bc = DirichletBC(u0, locate_dofs_topological(V, 2, x0facet))
-x1bc = DirichletBC(u1, locate_dofs_topological(V, 2, x1facet))
-
+                                   lambda x: np.isclose(x[0], 1))
 # Define variational problem
 u = ufl.TrialFunction(V)
 v = ufl.TestFunction(V)
 x = ufl.SpatialCoordinate(mesh)
 f = 0
-g = (1 - x[1]) * (90 - x[1]) * (1 - x[2]) * (90 - x[2])
+x0bc = DirichletBC(u0, locate_dofs_topological(V, 2, x0facet))
+x1bc = DirichletBC(u1, locate_dofs_topological(V, 2, x1facet))
+
+g = x[1] * (1- x[1]) * x[2] * (1 - x[2])
 a = inner(grad(u), grad(v)) * dx
 L = inner(f, v) * dx + inner(g, v) * ds
 
@@ -64,3 +75,29 @@ uh = problem.solve()
 with XDMFFile(MPI.COMM_WORLD, "ion_transport.xdmf", "w") as outfile:
     outfile.write_mesh(mesh)
     outfile.write_function(uh)
+
+
+
+# Update ghost entries and plot
+uh.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+try:
+    import pyvista
+
+    topology, cell_types = plot.create_vtk_topology(mesh, mesh.topology.dim)
+    grid = pyvista.UnstructuredGrid(topology, cell_types, mesh.geometry.x)
+    grid.point_data["u"] = uh.compute_point_values().real
+    grid.set_active_scalars("u")
+
+    plotter = pyvista.Plotter()
+    plotter.add_mesh(grid, show_edges=True)
+    warped = grid.warp_by_scalar()
+    plotter.add_mesh(warped)
+
+    # If pyvista environment variable is set to off-screen (static) plotting save png
+    if pyvista.OFF_SCREEN:
+        pyvista.start_xvfb(wait=0.1)
+        plotter.screenshot("uh.png")
+    else:
+        plotter.show()
+except ModuleNotFoundError:
+    print("pyvista is required to visualise the solution")
