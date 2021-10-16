44143116f81405e9f3b78bcb79dd94901b4476ee
Leshinka Molel <emolel3@gatech.edu>
2021-10-16 11:51:02 -0400

    cleanup

--- create_xdmf_meshfiles.py
+++ create_xdmf_meshfiles.py
@@ -1,18 +1,14 @@
 import os
+import sys
 import meshio
 import numpy as np
 
 
-data_dir = "/home/lesh/dev/ssb/"
-
-msh = meshio.read(os.path.join(data_dir, "porous.msh"))
-points = msh.points,
-cells = msh.cells
-cell_data = msh.cell_data
-field_data = msh.field_data
 
 
 def create_mesh(mesh, cell_type, prune_z=False):
+    """
+    """
     cells = mesh.get_cells_type(cell_type)
     cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
     out_mesh = meshio.Mesh(points=mesh.points,
@@ -24,11 +20,21 @@ def create_mesh(mesh, cell_type, prune_z=False):
     return out_mesh
 
 
-line_mesh = create_mesh(msh, "line")
-meshio.write("mesh_line.xdmf", line_mesh)
+if __name__ == '__main__':
+    input_meshfile = sys.argv[1]
+    msh = meshio.read(input_meshfile)
 
-triangle_mesh = create_mesh(msh, "triangle")
-meshio.write("mesh_tria.xdmf", triangle_mesh)
+    line_mesh_path = os.path.join(os.path.dirname(input_meshfile),
+                                  "mesh_line.xdmf")
+    tria_mesh_path = os.path.join(os.path.dirname(input_meshfile),
+                                  "mesh_tria.xdmf")
+    tetr_mesh_path = os.path.join(os.path.dirname(input_meshfile),
+                                  "mesh_tetr.xdmf")
+    line_mesh = create_mesh(msh, "line")
+    meshio.write(line_mesh_path, line_mesh)
 
-tetra_mesh = create_mesh(msh, "tetra")
-meshio.write("mesh_tetra.xdmf", tetra_mesh)
+    triangle_mesh = create_mesh(msh, "triangle")
+    meshio.write(tria_mesh_path, triangle_mesh)
+
+    tetra_mesh = create_mesh(msh, "tetra")
+    meshio.write(tetr_mesh_path, tetra_mesh)
