6d0890d4b05e490d538f081be45c8607500e3c2a
Leshinka Molel <emolel3@gatech.edu>
2021-10-15 20:49:49 -0400

    add geo files

---
+++ geom.py
@@ -0,0 +1,34 @@
+import os
+import meshio
+import numpy as np
+
+
+data_dir = "/home/lesh/dev/ssb/"
+
+msh = meshio.read(os.path.join(data_dir, "porous.msh"))
+points = msh.points,
+cells = msh.cells
+cell_data = msh.cell_data
+field_data = msh.field_data
+
+
+def create_mesh(mesh, cell_type, prune_z=False):
+    cells = mesh.get_cells_type(cell_type)
+    cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
+    out_mesh = meshio.Mesh(points=mesh.points,
+                           cells={cell_type: cells},
+                           cell_data={"name_to_read": [cell_data]}
+                           )
+    if prune_z:
+        out_mesh.prune_z_0()
+    return out_mesh
+
+
+line_mesh = create_mesh(msh, "line")
+meshio.write("mesh1.xdmf", line_mesh)
+
+triangle_mesh = create_mesh(msh, "triangle")
+meshio.write("mesh2.xdmf", triangle_mesh)
+
+# tetra_mesh = create_mesh(msh, "tetra")
+# meshio.write("mesh3.xdmf", tetra_mesh)
