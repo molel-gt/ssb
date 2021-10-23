4631ade6d978395cc710b201f006866291dbaabc
Leshinka Molel <emolel3@gatech.edu>
2021-10-23 13:04:48 -0400

    add number of arguments required

--- geometry.sh
+++ geometry.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+
 if [ "$1" == "-h" ]; then
   echo "Usage: `basename $0` [generates tetrahedral, triangle and line xdmf mesh files from images
         given the image files directory and shape of image file data array]
         Example: ./geometry.sh /home/ubuntu/dev 5,5
         "
   exit 0
+fi
+
+if [ $# -ne 2 ]
+  then
+    echo Error: "No arguments supplied"
+    exit 1
 fi
 
 ./create_node_files.py --files_dir=$1 --file_shape=$2
