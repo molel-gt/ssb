d05ca55754619ceeab64046f2001b8221b59b00b
Leshinka Molel <emolel3@gatech.edu>
2021-10-23 14:32:58 -0400

    use bmp_files subdirectory

--- create_node_files.py
+++ create_node_files.py
@@ -39,22 +39,35 @@ def create_nodes(data, **kwargs):
     return nodes
 
 
-def write_node_to_file(nodes):
+def write_node_to_file(nodes, node_file_path):
     """"""
+    count, _ = nodes.shape
+    meta_header = "# Node count, 3 dim, no attribute, no boundary marker"
+    header_data = [str(count), '3', '0', '1']
+    entries_header = "# Node index, node coordinates, boundary marker"
+    with open(node_file_path, "w") as fp:
+        fp.write(meta_header + '\n')
+        fp.write(' '.join(header_data) + '\n')
+        fp.write(entries_header + '\n')
+        for idx in range(count):
+            entry = [idx] + list(nodes[idx, :])
+            fp.write(' '.join([str(v) for v in entry]) + '\n')
     return
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='creates node file for meshing..')
-    parser.add_argument('--files_dir', help='image files directory', required=True)
+    parser.add_argument('--working_dir', help='bmp_files parent directory', required=True)
     parser.add_argument('--file_shape', help='shape of image data array', required=True,
                         type=lambda s: [int(item) for item in s.split(',')])
 
     args = parser.parse_args()
-    files_dir = args.files_dir
+    files_dir = os.path.join(args.working_dir, 'bmp_files')
     file_shape = args.file_shape
     files_list = [os.path.join(files_dir, f) for f in os.listdir(files_dir)
                   if f.endswith(".bmp")]
     print("loading image files to logical array..")
     image_data = load_images_to_logical_array(files_list, file_shape)
-    create_nodes(image_data)
+    node_file_path = os.path.join(args.working_dir, 'porous-solid.node')
+    nodes = create_nodes(image_data)
+    write_node_to_file(nodes, node_file_path)
--- geometry.sh
+++ geometry.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" == "-h" ]; then
   echo "Usage: `basename $0` [generates tetrahedral, triangle and line xdmf mesh files from images
-        given the image files directory and shape of image file data array]
-        Example: ./geometry.sh /home/ubuntu/dev 5,5
+        given the bmp_files parent directory and shape of image file data array]
+        Example: ./geometry.sh /home/ubuntu/dev/ 5,5
         "
   exit 0
 fi
@@ -14,10 +14,10 @@ if [ $# -ne 2 ]
     exit 1
 fi
 
-./create_node_files.py --files_dir=$1 --file_shape=$2
+./create_node_files.py --working_dir=$1 --file_shape=$2
 
-tetgen $1porous-cathode.node -ak
+tetgen $1porous-solid.node -ak
 
-gmsh -3 $1porous-cathode.geo -o $1porous-cathode.msh
+gmsh -3 $1porous-solid.geo -o $1porous-solid.msh
 
-./create_xdmf_meshfiles.py --input_meshfile=$1porous-cathode.msh
+./create_xdmf_meshfiles.py --input_meshfile=$1porous-solid.msh
