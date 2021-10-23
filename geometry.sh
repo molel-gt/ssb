02727183b946ec71130d95f86a0c8b346eff6dc1
Leshinka Molel <emolel3@gatech.edu>
2021-10-23 12:25:07 -0400

    add geom bash script

--- geometry.sh
+++ geometry.sh
@@ -3,3 +3,7 @@
 ./create_node_files.py --files_dir=/home/lesh/dev/ssb/NavneetGeometry2/ --file_shape=90,90
 
 tetgen porous-cathode.node -ak
+
+gmsh -3 porous-cathode.geo -o porous-cathode.msh
+
+./create_xdmf_meshfiles.py --input_meshfile=/home/lesh/dev/ssb/porous-cathode.gmsh
