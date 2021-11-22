f0d79218d01045edef11371fe71dd880412b2a8f
Leshinka Molel <emolel3@gatech.edu>
2021-11-22 14:21:10 -0500

    add more balls

--- raw_mesh.py
+++ raw_mesh.py
@@ -1,6 +1,7 @@
 import pygmsh
 import gmsh
 import os
+import numpy as np
 
 
 file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mesh/100_0_100", "sphere-in-box.msh")
@@ -9,8 +10,10 @@ with pygmsh.occ.Geometry() as geom:
     geom.characteristic_length_min = 5
     geom.characteristic_length_max = 5
     box1 = geom.add_box([0, 0, 0], [100, 100, 100])
-    ball1 = geom.add_ball([50, 50, 50], 15)
-    final = geom.boolean_difference(box1, ball1)
+    ball1 = geom.add_ball([10, 50, 50], 15)
+    ball2 = geom.add_ball([50, 50, 50], 15)
+    ball3 = geom.add_ball([90, 50, 50], 15)
+    final = geom.boolean_difference(box1, geom.boolean_union([ball1, ball2, ball3]))
     geom.add_physical(final, "box")
     geom.synchronize()
     mesh = geom.generate_mesh(dim=3)
