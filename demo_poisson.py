f33b0e383acfbaaeefc725b02320b81a4f30d16f
Leshinka Molel <emolel3@gatech.edu>
2022-03-01 22:34:25 -0500

    update solver

--- demo_poisson.py
+++ demo_poisson.py
@@ -1,7 +1,9 @@
+from contextlib import ExitStack
 
 import dolfinx
 import numpy as np
 import ufl
+from dolfinx import la
 from dolfinx.fem import (apply_lifting, assemble_matrix, assemble_vector, dirichletbc, Expression,
                          form, Function, FunctionSpace, locate_dofs_topological, set_bc,
                          VectorFunctionSpace
@@ -13,19 +15,52 @@ from petsc4py import PETSc
 from ufl import ds, dx, grad, inner
 
 
+def build_nullspace(V):
+    """Build PETSc nullspace for 3D elasticity"""
+
+    # Create list of vectors for building nullspace
+    index_map = V.dofmap.index_map
+    bs = V.dofmap.index_map_bs
+    ns = [la.create_petsc_vector(index_map, bs) for i in range(4)]
+    with ExitStack() as stack:
+        vec_local = [stack.enter_context(x.localForm()) for x in ns]
+        basis = [np.asarray(x) for x in vec_local]
+
+        # Get dof indices for each subspace (x, and y dofs)
+        dofs = [V.sub(i).dofmap.list.array for i in range(2)]
+
+        # Build the two translational rigid body modes
+        for i in range(2):
+            basis[i][dofs[i]] = 1.0
+
+        # Build the two rotational rigid body modes
+        x = V.tabulate_dof_coordinates()
+        dofs_block = V.dofmap.list.array
+        x0, x1 = x[dofs_block, 0], x[dofs_block, 1]
+        basis[2][dofs[0]] = -x1
+        basis[2][dofs[1]] = x0
+        basis[3][dofs[0]] = -x0
+        basis[3][dofs[1]] = x1
+
+    # Orthonormalise the six vectors
+    la.orthonormalize(ns)
+    assert la.is_orthonormal(ns)
+
+    return PETSc.NullSpace().create(vectors=ns)
+
 with XDMFFile(MPI.COMM_WORLD, "mesh.xdmf", "r") as infile3:
     mesh = infile3.read_mesh(dolfinx.cpp.mesh.GhostMode.shared_facet, 'Grid')
 print("done loading triangular mesh")
 
 tdim = mesh.topology.dim
-fdim = tdim - 1
+fdim = tdim - 2
 
 V = VectorFunctionSpace(mesh, ("Lagrange", 1))
 
 # Define boundary condition on x = 0 or x = 1
 u0 = Function(V)
 with u0.vector.localForm() as u0_loc:
-    u0_loc.set(1)
+    u0_loc.set(1.0)
 u1 = Function(V)
 with u1.vector.localForm() as u1_loc:
     u1_loc.set(0)
@@ -35,6 +70,11 @@ x1facets = locate_entities_boundary(mesh, fdim,
                                     lambda x: np.isclose(x[0], 10.0))
 x0bc = dirichletbc(u0, locate_dofs_topological(V, fdim, x0facets))
 x1bc = dirichletbc(u1, locate_dofs_topological(V, fdim, x1facets))
+
+# zero-flux sides
+# free_end_facets = np.sort(locate_entities_boundary(mesh, 1, lambda x: np.isclose(abs(x[1] - 5.0), 5)))
+# mt = dolfinx.mesh.MeshTags(mesh, 1, free_end_facets, 1)
+# ds = ufl.Measure("ds", subdomain_data=mt)
 
 # Define variational problem
 u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
@@ -54,17 +94,23 @@ apply_lifting(b, [a], bcs=[[x0bc, x1bc]])
 b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
 set_bc(b, [x0bc, x1bc])
 
+null_space = build_nullspace(V)
+A.setNearNullSpace(null_space)
+
 # Set solver options
 opts = PETSc.Options()
-opts["ksp_type"] = "cg"
-opts["ksp_rtol"] = 1.0e-10
-opts["pc_type"] = "gamg"
+opts["ksp_type"] = "cgs"
+# opts["ksp_rtol"] = 1.0e-05
+opts["pc_type"] = "lu"
+opts['pc_hypre_type'] = 'boomeramg'
+opts["pc_hypre_boomeramg_max_iter"] = 1
+opts["pc_hypre_boomeramg_cycle_type"] = "v"
 
-# Use Chebyshev smoothing for multigrid
+# # # Use Chebyshev smoothing for multigrid
 opts["mg_levels_ksp_type"] = "chebyshev"
 opts["mg_levels_pc_type"] = "jacobi"
 
-# Improve estimate of eigenvalues for Chebyshev smoothing
+# # # Improve estimate of eigenvalues for Chebyshev smoothing
 opts["mg_levels_esteig_ksp_type"] = "cg"
 opts["mg_levels_ksp_chebyshev_esteig_steps"] = 20
 
