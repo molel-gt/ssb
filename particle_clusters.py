fcf5d6872ffbd72613db56e75feb022724ff5043
Leshinka Molel <emolel3@gatech.edu>
2022-03-09 14:09:09 -0500

    temp additions

--- particle_clusters.py
+++ particle_clusters.py
@@ -3,6 +3,8 @@
 import os
 
 import argparse
+import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 
 from collections import defaultdict
@@ -57,12 +59,12 @@ def build_graph(array_chunk):
     :returns: graph
     :rtype: sparse matrix
     """
-    n = int(np.sum(array_chunk))
-    graph = np.zeros((n, n))
     points = defaultdict(lambda: -1, {})
     valid_points = set([tuple(v) for v in np.argwhere(array_chunk == 1)])
+    G = Graph(len(valid_points))
     for idx, value in enumerate(valid_points):
         points[(value[0], value[1], value[2])] = idx
+        # G.add_node(idx)
 
     for idx in valid_points:
         old_idx = (idx[0], idx[1], idx[2])
@@ -95,10 +97,9 @@ def build_graph(array_chunk):
             idx_i = points[old_idx]
             idx_j = points[new_idx]
             if idx_i != idx_j:
-                graph[(idx_i, idx_j)] = 1
-                graph[(idx_j, idx_i)] = 1
+                G.addEdge(idx_i, idx_j)
 
-    return points, graph
+    return points, G
 
 
 def chunk_array(data, chuck_max_size):
@@ -124,19 +125,66 @@ def filter_interior_points(data):
     return surface_data
 
 
+  
+class Graph:
+  
+    # init function to declare class variables
+    def __init__(self, V):
+        self.V = V
+        self.adj = [[] for i in range(V)]
+  
+    def DFSUtil(self, temp, v, visited):
+  
+        # Mark the current vertex as visited
+        visited[v] = True
+  
+        # Store the vertex to list
+        temp.append(v)
+  
+        # Repeat for all vertices adjacent
+        # to this vertex v
+        for i in self.adj[v]:
+            if visited[i] == False:
+  
+                # Update the list
+                temp = self.DFSUtil(temp, i, visited)
+        return temp
+  
+    # method to add an undirected edge
+    def addEdge(self, v, w):
+        self.adj[v].append(w)
+        self.adj[w].append(v)
+  
+    # Method to retrieve connected components
+    # in an undirected graph
+    def connectedComponents(self):
+        visited = []
+        cc = []
+        for i in range(self.V):
+            visited.append(False)
+        for v in range(self.V):
+            if visited[v] == False:
+                temp = []
+                cc.append(self.DFSUtil(temp, v, visited))
+        return cc
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='computes specific area')
-    parser.add_argument('--working_dir', help='bmp files directory', required=True)
+    parser.add_argument('--working_dir', help='bmp files directory',
+                        required=True)
 
     args = parser.parse_args()
     working_dir = args.working_dir
-    im_files = sorted([os.path.join(working_dir, f) for f in os.listdir(working_dir) if f.endswith(".bmp")])
+    im_files = sorted([os.path.join(working_dir, f) for
+                       f in os.listdir(working_dir) if f.endswith(".bmp")])
     n_files = len(im_files)
-    data = geometry.load_images_to_logical_array(im_files, x_lims=(0, 5),y_lims=(0, 5), z_lims=(0, 5))
+    data = geometry.load_images_to_logical_array(im_files, x_lims=(0, 25),
+                                                 y_lims=(0, 25), z_lims=(0, 25))
     surface_data = filter_interior_points(data)
-    points, B = build_graph(surface_data)
-    L = np.matmul(B, B.transpose())
-    ns1 = np.around(linalg.null_space(B.transpose()), 0)
-    print(ns1)
-    ns = linalg.null_space(L)
-    print("Number of pieces:", ns.shape[1])
+    points, G = build_graph(surface_data)
+    cc = G.connectedComponents()
+    # print("Connected pieces:", cc)
+    print("Grid size:", data.shape[0] * data.shape[1] * data.shape[2])
+    print("Number of connected pieces:", len(cc))
+    for item in cc:
+        print(len(item))
