ea81d4163135ea04eacf76e67dd143ee0ed5dbde
Leshinka Molel <emolel3@gatech.edu>
2022-03-13 12:51:49 -0400

    set up environment

---
+++ set-env.sh
@@ -0,0 +1,9 @@
+module load anaconda3/2021.05
+module load boost/1.70.0
+module load cmake/3.20.3
+module load eigen/3.3.7
+module load gcc/10.1.0
+module load hdf5/1.10.5-mva2
+module load mvapich2/2.3.2
+module load openblas/0.3.13
+module load petsc/3.12.2
