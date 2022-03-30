f3efb49ef9f23338b199b2cc519c513017a62abe
Leshinka Molel <emolel3@gatech.edu>
2022-03-30 14:54:37 -0400

    remove working dir var

--- examples/spheres.py
+++ examples/spheres.py
@@ -112,4 +112,4 @@ if __name__ == '__main__':
     mesh_3d = meshio.read(output_mesh_file)
     tetrahedral_mesh = create_mesh(mesh_3d, "tetra")
     meshio.write(os.path.join(task_dir, f"mesh/{grid_info}_tetr.xdmf"), tetrahedral_mesh)
-    val = subprocess.check_call(f'mpirun -n 2 python3 /home/leshinka/dev/ssb/transport.py --working_dir=/home/leshinka/dev/ssb/ --grid_info={grid_info}', shell=True)
+    val = subprocess.check_call(f'mpirun -n 2 python3 /home/leshinka/dev/ssb/transport.py --grid_info={grid_info}', shell=True)
