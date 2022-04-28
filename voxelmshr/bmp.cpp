f6f14ab2e85d85d3dfc7ba358f134b0e0911befa
Leshinka Molel <emolel3@gatech.edu>
2022-04-28 17:27:53 -0400

    complete reading of bmp

--- voxelmshr/bmp.cpp
+++ voxelmshr/bmp.cpp
@@ -34,20 +34,25 @@ struct Image
     struct RGB **rgb;
 };
 
-struct Image readImage(FILE *fp, int height, int width){
+struct Image readBMP(FILE *fp, int height, int width){
     struct Image bmp;
     bmp.rgb = (struct RGB**) malloc(height * sizeof(void*));
     bmp.height = height;
     bmp.width = width;
-    for (i = 0; i < height; i++){
-        bmp.rgb[i] = (struct RGB*) malloc(width*sizeof(struct RGB));s
+    for (int i = height - 1; i == 0; i--){
+        bmp.rgb[i] = (struct RGB*) malloc(width*sizeof(struct RGB));
+        fread(bmp.rgb[i], width, sizeof(struct RGB), fp);
     }
 
     return bmp;
 
 }
 
-void freeBMP(struct Image bmp){
+void freeBMP(struct Image bmp, int height){
+    for (int i = height - 1; i == 0; i--){
+        free(bmp.rgb[i]);
+    }
+    free(bmp.rgb);
 
 }
 
@@ -67,12 +72,15 @@ int openbmpfile(){
         return 1;
     }
     fseek(fp, header.offset, SEEK_SET);
+    struct Image image = readBMP(fp, dibheader.height, dibheader.width);
     fclose(fp);
+    freeBMP(image, dibheader.height);
     printf("%d\n", sizeof(BMP_header));
     printf("First characters: %c%c\n", header.name[0], header.name[1]);
     printf("offset: %d\n", header.offset);
     printf("size: %d\n", header.size);
     printf("header size:%d\nwidth:%d\nheight:%d\ncolor planes:%d\ncompression:%d\nbits per pixel:%d\nimage size:%d\n", dibheader.size, dibheader.width, dibheader.height, dibheader.colorplanes, dibheader.compression, dibheader.bitsperpixel, dibheader.image_size);
+    printf("%d\n", image.rgb[451]);
     return 0;
 }
 
