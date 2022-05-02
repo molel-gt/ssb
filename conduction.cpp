eb8996e2dbb05cde772a94dc0fa8f51530db659e
Leshinka Molel <emolel3@gatech.edu>
2022-05-02 10:24:16 -0400

    initialize cxx project

---
+++ conduction.cpp
@@ -0,0 +1,10 @@
+#include<dolfinx.h>
+#include<iostream>
+#include <xtensor/xtensor.hpp>
+
+using namespace std;
+
+int main(int argc, char **argv){
+    cout << "Trying out cxx dolfinx" << endl;
+    return 0;
+}
