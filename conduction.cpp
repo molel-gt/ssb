b6b9f51f2d22fe13980eb0103d3d3b60d056ce6f
Leshinka Molel <emolel3@gatech.edu>
2022-05-02 10:39:10 -0400

    add other includes

--- conduction.cpp
+++ conduction.cpp
@@ -1,5 +1,12 @@
-#include<dolfinx.h>
-#include<iostream>
+#include <cmath>
+#include <dolfinx.h>
+#include <dolfinx/fem/Constant.h>
+#include <dolfinx/fem/DirichletBC.h>
+#include <dolfinx/fem/Function.h>
+#include <dolfinx/fem/FunctionSpace.h>
+#include <dolfinx/fem/petsc.h>
+#include <dolfinx/io/XDMFFile.h>
+#include <iostream>
 #include <xtensor/xtensor.hpp>
 
 using namespace std;
