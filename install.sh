2dd01d49efe23648254d04d94b149691382a6a2b
Leshinka Molel <emolel3@gatech.edu>
2022-05-16 20:57:54 +0300

    add spack install

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ git clone https://github.com/spack/spack.git
 . ./spack/share/spack/setup-env.sh
 spack env create fenicsx-env
 spack env activate fenicsx-env
-spack add gcc@10.3
-spack location -i gcc@10.3 | xargs spack load
-spack compiler add gcc@10.3
-spack add py-fenics-dolfinx%gcc@10.3 cflags="-O3" fflags="-O3"
+spack add gcc@10.3.0
+spack install
+spack location -i gcc@10.3.0 | xargs spack load
+spack compiler add gcc@10.3.0
+spack add py-fenics-dolfinx%gcc@10.3.0 cflags="-O3" fflags="-O3"
 spack install
