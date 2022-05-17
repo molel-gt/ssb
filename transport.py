2016114368723bf9b944f820c385c7022f67a283
Leshinka Molel <emolel3@gatech.edu>
2022-05-17 15:43:59 +0300

    allow old version of dolfinx

--- transport.py
+++ transport.py
@@ -6,8 +6,8 @@ import argparse
 import dolfinx
 import numpy as np
 import ufl
-from dolfinx.fem import (dirichletbc as DirichletBC, Function, FunctionSpace)
-from dolfinx.fem import Constant, locate_dofs_topological, LinearProblem
+from dolfinx.fem import (Constant, dirichletbc as DirichletBC, Function, FunctionSpace, locate_dofs_topological)
+from dolfinx.fem import LinearProblem
 # from dolfinx.fem.petsc import LinearProblem
 from dolfinx.io import XDMFFile
 from dolfinx.mesh import locate_entities_boundary
