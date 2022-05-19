5ebaec41a5837dd6859be24f4a171cc4d3117b07
Leshinka Molel <emolel3@gatech.edu>
2022-05-19 18:04:22 +0300

    tiff reader

--- data.cpp
+++ data.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
-#include <opencv2/imgcodecs.hpp>
 #include <tiffio.h>
 #include <vector>
 
 int main(int argc, char **argv){
     TIFF* tif = TIFFOpen("Archive/3.tif46.tif116.tif105.tif102.tif", "r");
-    // #define uint32 unsigned long
-    uint32 width, height;
+    uint32 width, length;
     size_t npixels;
     uint32* raster;
-    uint32 arrData[801][451];
+    uint32 arrData[451][800];
     TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);
-    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);
-    npixels = width * height;
+    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &length);
+    npixels = width * length;
     raster = (unsigned int *) _TIFFmalloc(npixels * sizeof(unsigned int));
-    TIFFReadRGBAImage(tif, width, height, raster, 0);
-    std::cout << "width : " << width << std::endl << "height: "<< height << std::endl;
-    for (int i= 0; i < width; i++){
-        for (int j = 0; j < height; j++){
+    TIFFReadRGBAImage(tif, width, length, raster, 0);
+    std::cout << "length: " << length << std::endl << "width : "<< width << std::endl;
+    for (int i= 0; i < length; i++){
+        for (int j = 0; j < width; j++){
             int idx;
-            idx = i * height + j;
-            uint32 R=(uint32 )TIFFGetR(raster[idx]);
-            uint32 G=(uint32 )TIFFGetG(raster[idx]);
-            uint32 B=(uint32 )TIFFGetB(raster[idx]);
-            std::cout << R << G << B << std::endl;
-            if (R == 198 && G == 118 && B == 255){
+            idx = i + j * length;
+            uint32 R = (uint32 )TIFFGetR(raster[idx]);
+            uint32 G = (uint32 )TIFFGetG(raster[idx]);
+            uint32 B = (uint32 )TIFFGetB(raster[idx]);
+            // std::cout << R << ", " << G << ", "  << B << std::endl;
+            if ((B == 255) && (G == 118) && (R == 198)){
                 arrData[i][j] = 2;
-            } else if (R == 79 && G == 255 && B == 130){
+            } else if ((B == 130) && (G == 255) && (R == 79)){
                 arrData[i][j] = 1;
-            } else if (R == 255 && G == 0 && B == 0){
+            } else if ((B == 0) && (G == 0) && (R == 255)){
                 arrData[i][j] = 0;
             } else {
 
@@ -38,6 +36,6 @@ int main(int argc, char **argv){
 
     _TIFFfree(raster);
     TIFFClose(tif);
-    std::cout << arrData[800][450] << std::endl;
+    std::cout << arrData[450][800] << std::endl;
     return 0;
 }
