e9da6ce8e6aeab164dc844ffded67992c40ded84
Leshinka Molel <emolel3@gatech.edu>
2022-05-19 11:48:53 +0300

    update

--- data.cpp
+++ data.cpp
@@ -4,15 +4,16 @@
 int main(int argc, char **argv){
     TIFF* tif = TIFFOpen("/home/leshinka/dev/ssb/Archive/3.tif46.tif116.tif105.tif102.tif", "r");
     // #define uint32 unsigned long
-    unsigned int width, height;
+    uint32 width, height;
     size_t npixels;
-    unsigned int* raster;
-    TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);           // uint32 width;
-    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);        // uint32 height;
+    uint32* raster;
+    TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);
+    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);
     npixels = width * height;
     raster = (unsigned int *) _TIFFmalloc(npixels * sizeof(unsigned int));
-    TIFFReadRGBAImage(tif, width, height, raster, 0); 
-    std::cout << raster[0] << std::endl;
+    TIFFReadRGBAImage(tif, width, height, raster, 0);
+    std::cout << "width : " << width << std::endl << "height: "<< height << std::endl; 
+    std::cout << raster[100] << std::endl;
 
     _TIFFfree(raster);
     TIFFClose(tif);
