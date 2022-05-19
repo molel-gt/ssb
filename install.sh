3b15fc5dbfd407e46dbcc1a778f810b7cd5732d2
Leshinka Molel <emolel3@gatech.edu>
2022-05-19 10:38:55 +0300

    add gmsh and libtiff

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd /storage/home/hcoda1/0/emolel3/p-tf74-0/
+cd ./storage/home/hcoda1/0/emolel3/p-tf74-0/
 git clone https://github.com/spack/spack.git
-. ./spack/share/spack/setup-env.sh
+. ../spack/share/spack/setup-env.sh
 spack env create fenicsx-env
 spack env activate fenicsx-env
 spack add gcc@10.3.0
@@ -9,4 +9,6 @@ spack install
 spack load gcc@10.3.0
 spack location -i gcc@10.3.0 | xargs spack compiler add
 spack add py-fenics-dolfinx@main%gcc@10.3.0 cflags="-O3" fflags="-O3"
+spack add gmsh%gcc@10.3.0
+spack add libtiff%gcc@10.3.0
 spack install
