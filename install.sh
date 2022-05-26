9ca06951213e732687c1ed6422f05f5f1ad6e963
Leshinka Molel <emolel3@gatech.edu>
2022-05-26 09:54:13 +0300

    update install

--- install.sh
+++ install.sh
@@ -5,13 +5,11 @@ cp /storage/home/hcoda1/0/emolel3/p-tf74-0/config.yaml /storage/home/hcoda1/0/em
 . /storage/home/hcoda1/0/emolel3/p-tf74-0/spack/share/spack/setup-env.sh
 spack env create fenicsx-env
 spack env activate fenicsx-env
-spack add gcc@10.3.0
-spack install
-spack load gcc@10.3.0
-spack location -i gcc@10.3.0 | xargs spack compiler add
-spack add py-fenics-dolfinx@main%gcc@10.3.0 cflags="-O3" fflags="-O3"
-spack add gmsh@4.8.4%gcc@10.3.0
-spack add py-matplotlib%gcc@10.3.0
-spack add py-meshio%gcc@10.3.0
-spack add libtiff%gcc@10.3.0
+module load gcc/10.1.0
+spack compilers find
+spack add py-fenics-dolfinx@main%gcc@10.1.0 cflags="-O3" fflags="-O3"
+spack add gmsh@4.8.4%gcc@10.1.0
+spack add py-matplotlib%gcc@10.1.0
+spack add py-meshio%gcc@10.1.0
+spack add libtiff%gcc@10.1.0
 spack install
