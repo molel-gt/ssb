c6f08f31b5dd07ddea68a1d2e5b77498e786e7a8
Leshinka Molel <emolel3@gatech.edu>
2022-06-15 08:25:58 -0400

    update

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ for xpart in 0 50 100 150; do
     for ypart in 0 50 100 150; do
         for zpart in 0 50 100 150; do
             # nohup ./mesher.py --img_folder Archive/electrolyte --grid_info 51-51-51 --origin $xpart,$ypart,$zpart && mpirun -np 1 ./transport.py --grid_info 51-51-51 --origin $xpart,$ypart,$zpart &
-            ./run.pbs $xpart $ypart $zpart
+            qsub run.pbs $xpart $ypart $zpart
         done
     done
 done
