1acbd89985dbdb4d929e802edc1eb0a2c326f202
Leshinka Molel <emolel3@gatech.edu>
2022-06-21 15:52:40 -0400

    connected pieces

---
+++ connected_pieces.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python3
+
+import os
+
+import argparse
+import gmsh
+import logging
+import meshio
+import networkx as nx
+import numpy as np
+
+from mpi4py import MPI
+from scipy import linalg
+from scipy.io import loadmat, savemat
+from sklearn import neighbors
+
+import geometry, mesher
+
+
+FORMAT = '%(asctime)s: %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger(__file__)
+logger.setLevel('INFO')
+
+
+def build_points(data):
+    """"""
+    points = {}
+    count = 0
+    for idx, v in np.ndenumerate(data):
+        if v == 1:
+            points[idx] = count
+            count += 1
+    return points
+
+
+def build_graph(points):
+    """"""
+    G = nx.Graph()
+    for v in points.values():
+        G.add_node(v)
+    for k in points.keys():
+        x, y, z = k
+        neighbors = [
+            (int(x + 1), y, z),
+            (int(x - 1), y, z),
+            (x, int(y + 1), z),
+            (x, int(y - 1), z),
+            (x, y, int(z + 1)),
+            (x, y, int(z - 1)),
+        ]
+        p0 = points[k]
+        for neighbor in neighbors:
+            p = points.get(neighbor)
+            if p is None:
+                continue
+            G.add_edge(p0, p)
+    return G
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='computes specific area')
+    parser.add_argument('--img_folder', help='bmp files directory',
+                        required=True)
+    parser.add_argument('--grid_info', help='Nx-Ny-Nz',
+                        required=True)
+    parser.add_argument('--origin', default=(0, 0, 0), help='where to extract grid from')
+
+    args = parser.parse_args()
+    if isinstance(args.origin, str):
+        origin = tuple(map(lambda v: int(v), args.origin.split(",")))
+    else:
+        origin = args.origin
+    origin_str = "_".join([str(v) for v in origin])
+    grid_info = args.grid_info
+    grid_size = int(args.grid_info.split("-")[0])
+    Nx, Ny, Nz = [int(v) for v in args.grid_info.split("-")]
+    img_dir = args.img_folder
+    im_files = sorted([os.path.join(img_dir, f) for
+                       f in os.listdir(img_dir) if f.endswith(".bmp")])
+    n_files = len(im_files)
+
+    data = geometry.load_images_to_voxel(im_files, x_lims=(0, Nx),
+                                         y_lims=(0, Ny), z_lims=(0, Nz), origin=origin)
+    num_points = np.sum(data)
+    points = build_points(data)
+    points_view = {v: k for k, v in points.items()}
+    G = build_graph(points)
+    pieces = nx.connected_components(G)
+    pieces = [piece for piece in pieces]
+    logger.info("{:,} components".format(len(pieces)))
+    for idx, piece in enumerate(pieces):
+        largest_piece = np.zeros((Nx, Ny, Nz), dtype=np.uint8)
+        for p in piece:
+            coord = points_view[p]
+            largest_piece[coord] = 1
+        if np.all(largest_piece[:, 0, :] == 0) or np.all(largest_piece[:, Ny - 1, :] == 0):
+            logger.debug(f"Piece {idx} does not span both ends")
+            continue
+        occlusions = np.logical_not(largest_piece)
+        rectangles = mesher.make_rectangles(occlusions)
+        boxes = mesher.make_boxes(rectangles)
+        logger.info("No. voxels       : %s" % np.sum(occlusions))
+        logger.info("No. rectangles   : %s" % np.sum(rectangles))
+        logger.info("No. boxes        : %s" % np.sum(boxes))
+        output_mshfile = f"mesh/s{grid_info}o{origin_str}_porous.msh"
+        gmsh.initialize()
+        # gmsh.option.setNumber("Mesh.MeshSizeMin", 0.1)
+        # gmsh.option.setNumber("Mesh.MeshSizeMax", 0.1)
+        mesher.build_voxels_mesh(boxes, output_mshfile)
+        gmsh.finalize()
+        logger.info("writing xmdf tetrahedral mesh..")
+        msh = meshio.read(output_mshfile)
+        tetra_mesh = geometry.create_mesh(msh, "tetra")
+        meshio.write(f"mesh/s{grid_info}o{origin_str}_tetr{idx}.xdmf", tetra_mesh)
+        logger.info(f"{idx}: %.4f %.4f" % (np.average(largest_piece[:, 0, :]), np.average(largest_piece[:, Ny - 1, :])))
