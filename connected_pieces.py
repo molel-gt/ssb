14b2b8a071c2a0b8a7e8f6a1999cf13394352f1d
Leshinka Molel <emolel3@gatech.edu>
2022-06-22 17:15:04 -0400

    mesh connected pieces

--- connected_pieces.py
+++ connected_pieces.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import os
+import time
 
 import argparse
 import gmsh
@@ -8,11 +9,6 @@ import logging
 import meshio
 import networkx as nx
 import numpy as np
-
-from mpi4py import MPI
-from scipy import linalg
-from scipy.io import loadmat, savemat
-from sklearn import neighbors
 
 import geometry, mesher
 
@@ -80,6 +76,8 @@ if __name__ == "__main__":
                        f in os.listdir(img_dir) if f.endswith(".bmp")])
     n_files = len(im_files)
 
+    start_time = time.time()
+
     data = geometry.load_images_to_voxel(im_files, x_lims=(0, Nx),
                                          y_lims=(0, Ny), z_lims=(0, Nz), origin=origin)
     num_points = np.sum(data)
@@ -89,6 +87,7 @@ if __name__ == "__main__":
     pieces = nx.connected_components(G)
     pieces = [piece for piece in pieces]
     logger.info("{:,} components".format(len(pieces)))
+    connected = np.zeros((Nx, Ny, Nz), dtype=np.uint8)
     for idx, piece in enumerate(pieces):
         largest_piece = np.zeros((Nx, Ny, Nz), dtype=np.uint8)
         for p in piece:
@@ -97,20 +96,22 @@ if __name__ == "__main__":
         if np.all(largest_piece[:, 0, :] == 0) or np.all(largest_piece[:, Ny - 1, :] == 0):
             logger.debug(f"Piece {idx} does not span both ends")
             continue
-        occlusions = np.logical_not(largest_piece)
-        rectangles = mesher.make_rectangles(occlusions)
-        boxes = mesher.make_boxes(rectangles)
-        logger.info("No. voxels       : %s" % np.sum(occlusions))
-        logger.info("No. rectangles   : %s" % np.sum(rectangles))
-        logger.info("No. boxes        : %s" % np.sum(boxes))
-        output_mshfile = f"mesh/s{grid_info}o{origin_str}_porous.msh"
-        gmsh.initialize()
-        # gmsh.option.setNumber("Mesh.MeshSizeMin", 0.1)
-        # gmsh.option.setNumber("Mesh.MeshSizeMax", 0.1)
-        mesher.build_voxels_mesh(boxes, output_mshfile)
-        gmsh.finalize()
-        logger.info("writing xmdf tetrahedral mesh..")
-        msh = meshio.read(output_mshfile)
-        tetra_mesh = geometry.create_mesh(msh, "tetra")
-        meshio.write(f"mesh/s{grid_info}o{origin_str}_tetr{idx}.xdmf", tetra_mesh)
-        logger.info(f"{idx}: %.4f %.4f" % (np.average(largest_piece[:, 0, :]), np.average(largest_piece[:, Ny - 1, :])))
+        logger.info(f"Piece {idx} spans both ends along y-axis")
+        connected += largest_piece
+    occlusions = np.logical_not(connected)
+    rectangles = mesher.make_rectangles(occlusions)
+    boxes = mesher.make_boxes(rectangles)
+    logger.info("No. voxels       : %s" % np.sum(occlusions))
+    logger.info("No. rectangles   : %s" % np.sum(rectangles))
+    logger.info("No. boxes        : %s" % np.sum(boxes))
+    output_mshfile = f"mesh/s{grid_info}o{origin_str}_porous.msh"
+    gmsh.initialize()
+    gmsh.option.setNumber("Mesh.MeshSizeMin", 0.5)
+    gmsh.option.setNumber("Mesh.MeshSizeMax", 0.5)
+    mesher.build_voxels_mesh(boxes, output_mshfile)
+    gmsh.finalize()
+    logger.info("writing xmdf tetrahedral mesh..")
+    msh = meshio.read(output_mshfile)
+    tetra_mesh = geometry.create_mesh(msh, "tetra")
+    meshio.write(f"mesh/s{grid_info}o{origin_str}_tetr.xdmf", tetra_mesh)
+    logger.info("Operation took {:,} seconds".format(int(time.time() - start_time)))
