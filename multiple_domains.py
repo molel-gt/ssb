dd8e417d4b43a7bc308c75b727de1b85c8b68e4d
Leshinka Molel <emolel3@gatech.edu>
2022-06-28 15:57:53 -0400

    coverage map

--- multiple_domains.py
+++ multiple_domains.py
@@ -33,32 +33,33 @@ gmsh.initialize()
 gmsh.option.setNumber("Mesh.MeshSizeMin", 0.005)
 gmsh.option.setNumber("Mesh.MeshSizeMax", 0.005)
 gmsh.model.add("li-sse")
-# lithium phase
-x, sine_curve_y = make_shape_1(Lx, Ly)
-meshname = "serrated"
-x, sine_curve_y = make_shape_2(Lx, Ly, resolution)
-meshname = "sinecurve"
-zeros = np.zeros(len(x))
-curve_positions = list(zip(x, sine_curve_y, zeros))
-curve_positions = [gmsh.model.occ.add_point(*p) for p in curve_positions]
-arc_lines = [gmsh.model.occ.add_line(curve_positions[i], curve_positions[i+1]) for i in range(-1, len(curve_positions) - 1)]
-arc_loop = gmsh.model.occ.add_curve_loop(arc_lines)
-lithium_surf = gmsh.model.occ.add_plane_surface([arc_loop])
+meshname = 'coverage'
+# # lithium phase
+# x, sine_curve_y = make_shape_1(Lx, Ly)
+# meshname = "serrated"
+# x, sine_curve_y = make_shape_2(Lx, Ly, resolution)
+# meshname = "sinecurve"
+# zeros = np.zeros(len(x))
+# curve_positions = list(zip(x, sine_curve_y, zeros))
+# curve_positions = [gmsh.model.occ.add_point(*p) for p in curve_positions]
+# arc_lines = [gmsh.model.occ.add_line(curve_positions[i], curve_positions[i+1]) for i in range(-1, len(curve_positions) - 1)]
+# arc_loop = gmsh.model.occ.add_curve_loop(arc_lines)
+# lithium_surf = gmsh.model.occ.add_plane_surface([arc_loop])
 # electrolyte phase
-p0 = gmsh.model.occ.add_point(0, 1, 0)
+p0 = gmsh.model.occ.add_point(0, 0, 0)
 p1 = gmsh.model.occ.add_point(0, Ly, 0)
 p2 = gmsh.model.occ.add_point(Lx, Ly, 0)
-p3 = gmsh.model.occ.add_point(Lx, 1, 0)
+p3 = gmsh.model.occ.add_point(Lx, 0, 0)
 electrolyte_points = [p0, p1, p2, p3]
 lines = [gmsh.model.occ.add_line(electrolyte_points[i], electrolyte_points[i+1]) for i in range(-1, len(electrolyte_points) - 1)]
 line_loop = gmsh.model.occ.add_curve_loop(lines)
 electrolyte_surf = gmsh.model.occ.add_plane_surface([line_loop])
-full_domain = gmsh.model.occ.fragment([(2, lithium_surf)], [(2, electrolyte_surf)])
+# full_domain = gmsh.model.occ.fragment([(2, lithium_surf)], [(2, electrolyte_surf)])
 
 gmsh.model.occ.synchronize()
 surfaces = gmsh.model.getEntities(dim=2)
-lithium_surface = gmsh.model.addPhysicalGroup(2, [surfaces[0][1]], 1)
-electrolyte_surface = gmsh.model.addPhysicalGroup(2, [surfaces[1][1]], 2)
+# lithium_surface = gmsh.model.addPhysicalGroup(2, [surfaces[0][1]], 1)
+electrolyte_surface = gmsh.model.addPhysicalGroup(2, [surfaces[0][1]], 1)
 
 gmsh.model.mesh.generate(2)
 gmsh.write(f"{meshname}.msh")
