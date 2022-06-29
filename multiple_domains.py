16054db7fae86237dd92c160737098559982a377
Leshinka Molel <emolel3@gatech.edu>
2022-06-28 21:17:04 -0400

    channel with slit

--- multiple_domains.py
+++ multiple_domains.py
@@ -12,19 +12,15 @@ import geometry
 Lx = 1000
 Ly = 1
 resolution = 0.1
-gmsh.initialize()
-gmsh.model.add("li-sse")
 meshname = 'coverage'
+gmsh.initialize()
+gmsh.model.add("constriction")
+gmsh.option.setNumber("Mesh.MeshSizeMin", 0.5)
+gmsh.option.setNumber("Mesh.MeshSizeMax", 0.5)
 
-p0 = gmsh.model.occ.add_point(0, 0, 0, meshSize=resolution)
-p1 = gmsh.model.occ.add_point(0, Ly, 0, meshSize=resolution)
-p2 = gmsh.model.occ.add_point(Lx, Ly, 0, meshSize=resolution)
-p3 = gmsh.model.occ.add_point(Lx, 0, 0, meshSize=resolution)
-electrolyte_points = [p0, p1, p2, p3]
-lines = [gmsh.model.occ.add_line(electrolyte_points[i], electrolyte_points[i+1]) for i in range(-1, len(electrolyte_points) - 1)]
-line_loop = gmsh.model.occ.add_curve_loop(lines)
-electrolyte_surf = gmsh.model.occ.add_plane_surface([line_loop])
-
+channel = gmsh.model.occ.add_rectangle(0, 0, 0, Lx, Ly)
+# slit_1 = gmsh.model.occ.add_rectangle(0, 0.75 * Ly, 0, 0.25 * Lx, 0.01 * Ly)
+# gmsh.model.occ.cut([(2, channel)], [(2, slit_1)])
 gmsh.model.occ.synchronize()
 surfaces = gmsh.model.getEntities(dim=2)
 electrolyte_surface = gmsh.model.addPhysicalGroup(2, [surfaces[0][1]], 1)
