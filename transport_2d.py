f8499c8532fe4ba6f4e0662623dccdbf1e2841a1
Leshinka Molel <emolel3@gatech.edu>
2022-06-28 23:14:31 -0400

    update

--- transport_2d.py
+++ transport_2d.py
@@ -9,9 +9,9 @@ from mpi4py import MPI
 from petsc4py import PETSc
 
 Ly = int(sys.argv[1])
-Lx = int(sys.argv[4])
 meshname = sys.argv[2]
 coverage = float(sys.argv[3])
+Lx = int(sys.argv[4])
 lower_cov = 0.5 * (1 - coverage) * Lx
 upper_cov = Lx - 0.5 * (1 - coverage) * Lx
 
@@ -39,8 +39,9 @@ with u0.vector.localForm() as u0_loc:
 u1 = dolfinx.fem.Function(V)
 with u1.vector.localForm() as u1_loc:
     u1_loc.set(0)
-x0facet = dolfinx.mesh.locate_entities_boundary(msh, 0,
-                                lambda x: np.logical_and(np.isclose(x[1], 0.0), np.logical_and(lower_cov <= x[0],  x[0] <= upper_cov)))
+# partially_insulated = lambda x: np.logical_and(np.isclose(x[1], 0.0), np.logical_and(lower_cov <= x[0],  x[0] <= upper_cov))
+# x0facet = dolfinx.mesh.locate_entities_boundary(msh, 0, partially_insulated)
+x0facet = dolfinx.mesh.locate_entities_boundary(msh, 0, lambda x: np.isclose(x[1], 0.0))
 x1facet = dolfinx.mesh.locate_entities_boundary(msh, 0,
                                 lambda x: np.isclose(x[1], Ly))
 x0bc = dolfinx.fem.dirichletbc(u0, dolfinx.fem.locate_dofs_topological(V, 0, x0facet))
