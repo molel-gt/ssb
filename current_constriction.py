4c2cab074578b58cf06853320f240b1b174c4908
Leshinka Molel <emolel3@gatech.edu>
2022-07-11 20:38:54 -0400

    refactor

--- current_constriction.py
+++ current_constriction.py
@@ -1,60 +1,94 @@
 #!/usr/bin/env python3
 # coding: utf-8
-import gmsh
-import meshio
+import sys
+
+import argparse
+import dolfinx
 import numpy as np
-import pygmsh
+import ufl
 
+from mpi4py import MPI
+from petsc4py import PETSc
 
-def create_mesh(mesh, cell_type, prune_z=False):
-    cells = mesh.get_cells_type(cell_type)
-    cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
-    points = mesh.points[:,:2] if prune_z else mesh.points
-    out_mesh = meshio.Mesh(points=points, cells={cell_type: cells}, cell_data={"name_to_read":[cell_data]})
-    return out_mesh
-
+import utils
 
 if __name__ == '__main__':
-    geometry = pygmsh.geo.Geometry()
-    model = geometry.__enter__()
+    parser = argparse.ArgumentParser(description='computes specific area')
+    parser.add_argument('--coverage', help='fraction of lower current collector that is conductive', required=True)
+    parser.add_argument('--Lx', help='length', required=True)
+    parser.add_argument('--Ly', help='width', required=True)
+    parser.add_argument("--w", help='slice width along x', nargs='?', const=1, default=10, type=float)
+    parser.add_argument("--h", help='slice position along y', nargs='?', const=1, default=0.5, type=float)
 
-    resolution = 0.1
-    Lx = 10
-    points = []
-    sine_curve_x = [np.around(v, 1) for v in reversed(np.linspace(0, Lx, int((Lx / resolution) + 1)))]
-    sine_curve_y = list(0 + np.around(np.sin(2 * np.pi * np.array(sine_curve_x) / (2 * Lx)), 4))
-    sine_curve_y = [1.0 if (1.0 - v) < 0.01 else v for v in sine_curve_y]
-    zeros = np.zeros(len(sine_curve_x))
-    curve_positions = list(zip(sine_curve_x, sine_curve_y, zeros))
-    for p in curve_positions:
-        points.append(
-            model.add_point(p, mesh_size=resolution)
-        )
-    channel_lines = [model.add_line(points[i], points[i+1]) for i in range(-1, len(points)-1)]
+    args = parser.parse_args()
+    coverage = args.coverage
+    Lx = args.Lx
+    Ly = args.Ly
+    lower_cov = 0.5 * (1 - coverage) * Lx
+    upper_cov = Lx - 0.5 * (1 - coverage) * Lx
+    meshname = '{%0.3fh}-{%0.3fw}'
+    utils.make_dir_if_missing('current_constriction')
+    with dolfinx.io.XDMFFile(MPI.COMM_WORLD, f"{meshname}.xdmf", "r") as infile3:
+            msh = infile3.read_mesh(dolfinx.cpp.mesh.GhostMode.none, 'Grid')
+            ct = infile3.read_meshtags(msh, name="Grid")
 
-    channel_loop = model.add_curve_loop(channel_lines)
+    msh.topology.create_connectivity(msh.topology.dim, msh.topology.dim-1)
 
-    plane_surface = model.add_plane_surface(channel_loop)
-    model.synchronize()
+    Q = dolfinx.fem.FunctionSpace(msh, ("DG", 0))
+    kappa = 1.0
 
-    volume_marker = 6
-    model.add_physical([plane_surface], "Volume")
-    model.add_physical([channel_lines[0]], "Left")
-    model.add_physical([channel_lines[2]], "Right")
-    model.add_physical([channel_lines[1], channel_lines[3]], "Insulated")
+    V = dolfinx.fem.FunctionSpace(msh, ("Lagrange", 1))
 
+    # Dirichlet BCs
+    u0 = dolfinx.fem.Function(V)
+    with u0.vector.localForm() as u0_loc:
+        u0_loc.set(2)
 
-    geometry.generate_mesh(dim=2)
-    gmsh.write("mesh.msh")
-    gmsh.clear()
-    geometry.__exit__()
+    u1 = dolfinx.fem.Function(V)
+    with u1.vector.localForm() as u1_loc:
+        u1_loc.set(0)
+    partially_insulated = lambda x: np.logical_and(np.isclose(x[1], 0.0), np.logical_and(lower_cov <= x[0],  x[0] <= upper_cov))
+    x0facet = dolfinx.mesh.locate_entities_boundary(msh, 1, partially_insulated)
+    x1facet = dolfinx.mesh.locate_entities_boundary(msh, 1, lambda x: np.isclose(x[1], Ly))
+    x0bc = dolfinx.fem.dirichletbc(u0, dolfinx.fem.locate_dofs_topological(V, 1, x0facet))
+    x1bc = dolfinx.fem.dirichletbc(u1, dolfinx.fem.locate_dofs_topological(V, 1, x1facet))
 
-    mesh_from_file = meshio.read("mesh.msh")
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+    x = ufl.SpatialCoordinate(msh)
+    f = dolfinx.fem.Constant(msh, PETSc.ScalarType(0))
+    g = dolfinx.fem.Constant(msh, PETSc.ScalarType(0))
 
+    a = ufl.inner(kappa * ufl.grad(u), ufl.grad(v)) * ufl.dx
+    L = ufl.inner(f, v) * ufl.dx + ufl.inner(g, v) * ufl.ds
 
+    options =  {
+                "ksp_type": "gmres",
+                "pc_type": "hypre",
+                "ksp_atol": 1.0e-12,
+                "ksp_rtol": 1.0e-12
+                }
+    problem = dolfinx.fem.petsc.LinearProblem(a, L, bcs=[x0bc, x1bc], petsc_options=options)
+    uh = problem.solve()
 
-    line_mesh = create_mesh(mesh_from_file, "line", prune_z=True)
-    meshio.write("facet_mesh.xdmf", line_mesh)
+    with dolfinx.io.XDMFFile(msh.comm, f"current_constriction/{meshname}_{coverage}_potential.xdmf", "w") as file:
+        file.write_mesh(msh)
+        file.write_function(uh)
+    grad_u = ufl.grad(uh)
 
-    triangle_mesh = create_mesh(mesh_from_file, "triangle", prune_z=True)
-    meshio.write("mesh.xdmf", triangle_mesh)
+    W = dolfinx.fem.FunctionSpace(msh, ("Lagrange", 1))
+
+    current_expr = dolfinx.fem.Expression(ufl.sqrt(ufl.inner(grad_u, grad_u)), W.element.interpolation_points)
+    current_h = dolfinx.fem.Function(W)
+    current_h.interpolate(current_expr)
+
+    with dolfinx.io.XDMFFile(MPI.COMM_WORLD, f"current_constriction/{meshname}_{coverage}_current.xdmf", "w") as file:
+        file.write_mesh(msh)
+        file.write_function(current_h)
+
+    left_facets = dolfinx.mesh.locate_entities_boundary(msh, msh.topology.dim - 1, lambda x: np.logical_or(np.isclose(x[1], 0.0), np.isclose(x[1], Ly)))
+    left_dofs = dolfinx.fem.locate_dofs_topological(V, msh.topology.dim - 1, left_facets)
+    n = -ufl.FacetNormal(msh)  # outward pointing
+    dobs = ufl.Measure("ds", domain=msh)
+    solution_trace_norm = dolfinx.fem.assemble_scalar(dolfinx.fem.form(ufl.inner(ufl.grad(uh), n) ** 2 * dobs))
+    print(f"Homogeneous Neumann BC trace: {solution_trace_norm}")
--- multiple_domains.py
+++ multiple_domains.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python3
 # coding: utf-8
+import argparse
 import gmsh
 import meshio
 import numpy as np
 
 from mpi4py import MPI
 
-import geometry
+import geometry, utils
 
 
-Lx = 1000
-Ly = 1
-resolution = 0.1
-meshname = 'coverage'
-gmsh.initialize()
-gmsh.model.add("constriction")
-gmsh.option.setNumber("Mesh.MeshSizeMin", 0.5)
-gmsh.option.setNumber("Mesh.MeshSizeMax", 0.5)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='geometry with slices')
+    parser.add_argument('--Lx', help='length', required=True)
+    parser.add_argument('--Ly', help='width', required=True)
+    parser.add_argument("--w", help='slice width along x', nargs='?', const=1, default=10, type=float)
+    parser.add_argument("--h", help='slice position along y', nargs='?', const=1, default=0.5, type=float)
 
-channel = gmsh.model.occ.add_rectangle(0, 0, 0, Lx, Ly)
-# slit_1 = gmsh.model.occ.add_rectangle(0, 0.75 * Ly, 0, 0.25 * Lx, 0.01 * Ly)
-# gmsh.model.occ.cut([(2, channel)], [(2, slit_1)])
-gmsh.model.occ.synchronize()
-surfaces = gmsh.model.getEntities(dim=2)
-electrolyte_surface = gmsh.model.addPhysicalGroup(2, [surfaces[0][1]], 1)
+    args = parser.parse_args()
+    coverage = args.coverage
+    meshname = args.meshname
+    Lx = args.Lx
+    Ly = args.Ly
+    w = args.w / Lx
+    h = args.h / Ly
+    resolution = 0.1
+    meshname = 'current_constriction/{%0.3fh}-{%0.3fw}'
+    utils.make_dir_if_missing('current_constriction')
 
-gmsh.model.mesh.generate(2)
-gmsh.write(f"{meshname}.msh")
-gmsh.finalize()
+    gmsh.initialize()
+    gmsh.model.add("constriction")
+    gmsh.option.setNumber("Mesh.MeshSizeMin", 0.01)
+    gmsh.option.setNumber("Mesh.MeshSizeMax", 0.01)
 
-mesh_from_file = meshio.read(f"{meshname}.msh")
+    channel = gmsh.model.occ.add_rectangle(0, 0, 0, Lx, Ly)
+    slit_1 = gmsh.model.occ.add_rectangle(0.5 * (1 - w) * Lx, (h - 0.5 * w) * Ly, 0, w * Lx, w * Ly)
+    gmsh.model.occ.cut([(2, channel)], [(2, slit_1)])
+    gmsh.model.occ.synchronize()
+    surfaces = gmsh.model.getEntities(dim=2)
+    gmsh.model.addPhysicalGroup(2, [surfaces[0][1]], 1)
 
-triangle_mesh = geometry.create_mesh(mesh_from_file, "triangle")
-meshio.write(f"{meshname}.xdmf", triangle_mesh)
+    gmsh.model.mesh.generate(2)
+    gmsh.write(f"{meshname}.msh")
+    gmsh.finalize()
+
+    mesh_from_file = meshio.read(f"{meshname}.msh")
+
+    triangle_mesh = geometry.create_mesh(mesh_from_file, "triangle")
+    meshio.write(f"{meshname}.xdmf", triangle_mesh)
