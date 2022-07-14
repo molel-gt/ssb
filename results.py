74c98b1aa0bf4905ff81526fa4d6d8581d69e266
Leshinka Molel <emolel3@gatech.edu>
2022-07-14 05:08:06 -0400

    figure

--- results.py
+++ results.py
@@ -5,24 +5,23 @@ import numpy as np
 
 
 porosity = []
-kappa_eff = []
+model = []
 bruggeman = []
 
 with open("results.csv", "r") as fp:
     reader = csv.DictReader(fp)
     for row in reader:
-        porosity.append(float(row['porosity (avg)']))
-        kappa_eff.append(float(row['kappa_eff (avg)']))
+        porosity.append(float(row['porosity']))
+        model.append(float(row['model']))
         bruggeman.append(float(row['bruggeman']))
 fig, ax = plt.subplots()
-ax.plot(porosity, kappa_eff, 'o')
+ax.plot(porosity, model, 'o')
 ax.plot(porosity, bruggeman, '-')
 ax.set_xlim(0, 1)
-ax.set_ylim(0, 1)
+ax.set_ylim(0, 0.1)
 ax.set_box_aspect(1)
 ax.set_xlabel(r"$\varepsilon$")
 ax.set_ylabel(r"$\kappa_{eff}$")
-ax.set_title("Effective Conductivity")
-ax.axis('equal')
+ax.set_title(r"$\kappa_0$ = 0.1 S/m")
 ax.legend(['model', 'bruggeman'])
 plt.show()
