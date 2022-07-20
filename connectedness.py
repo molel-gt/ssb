a69e68a413a1fecc17f66f89bbc1c76aa86c46db
Leshinka Molel <emolel3@gatech.edu>
2022-07-20 12:29:45 -0400

    add connectedness figures

--- connectedness.py
+++ connectedness.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import geometry
 
@@ -21,8 +22,17 @@ voxels = geometry.load_images_to_voxel(im_files, x_lims=(0, Nx),
 av_x = np.mean(voxels, axis=0)
 av_z = np.mean(voxels, axis=2)
 fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.imshow(av_x)
+im1 = ax1.imshow(av_x)
 ax1.set_title("Average along x")
-ax2.imshow(av_z)
+im2 = ax2.imshow(av_z)
 ax2.set_title("Average along z")
+
+divider1 = make_axes_locatable(ax1)
+cax1 = divider1.append_axes('right', size='5%', pad=0.05)
+fig.colorbar(im1, cax=cax1, orientation='vertical')
+
+divider2 = make_axes_locatable(ax2)
+cax2 = divider2.append_axes('right', size='5%', pad=0.05)
+fig.colorbar(im2, cax=cax2, orientation='vertical')
+
 plt.show()
