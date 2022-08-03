e51ab45ed009844b76549b8543cada9b66295162
Leshinka Molel <emolel3@gatech.edu>
2022-08-03 14:54:24 -0400

    update

--- summary.py
+++ summary.py
@@ -12,10 +12,10 @@ import utils
 
 
 # current constriction
-def compute_resistance(coverage, w, h, voltages=(1, 2), Lx=100, Ly=1):
+def compute_resistance(coverage, w, h, voltages=(1, 2), Lx=100, Ly=1, pos='mid', n_pieces=0):
     currents = []
     for v in voltages:
-        f = results_filename(coverage, w, h, v)
+        f = results_filename(coverage, w, h, v, pos, n_pieces)
         data = h5py.File(f, "r")
         geom = data['/Mesh/Grid/geometry']
         values = data['/Function/f/0']
@@ -25,26 +25,29 @@ def compute_resistance(coverage, w, h, voltages=(1, 2), Lx=100, Ly=1):
         for idx, v in enumerate(values):
             x, y, _ = geom[idx]
             if np.isclose(y, 0):
-                if 0.5 * (1 - coverage) * Lx <= x <= (0.5 + 0.5 * coverage) * Lx:
-                    current_0.append(v)
-            elif np.isclose(y, Ly):
+                # if 0.5 * (1 - coverage) * Lx <= x <= (0.5 + 0.5 * coverage) * Lx:
+                #     current_0.append(v)
+                pass
+            if np.isclose(y, Ly):
                 current_1.append(v)
-        i_avg_1 = np.around(np.nanmean(current_0), 6)
+        i_avg_1 = np.nan # np.around(np.nanmean(current_0), 6)
         i_avg_2 = np.around(np.nanmean(current_1), 6)
         currents.append((i_avg_1, i_avg_2))
     return (voltages[1] - voltages[0]) / (currents[1][1] - currents[0][1]), currents
 
 
-def results_filename(coverage, w, h, voltage: int):
-    return f'current_constriction/{h:.3}_{w:.3}_{coverage:.2}_{voltage}_current.h5'
+def results_filename(coverage, w, h, voltage: int, pos, n_pieces):
+    return f'current_constriction/{h:.3}_{w:.3}_{coverage:.2}_{voltage}_pos-{pos}_pieces-{n_pieces}_current.h5'
 
 
 if __name__ == '__main__':
-    heights = [0.25, 0.50, 0.75]
-    widths = [0.1, 0.5, 0.9]
-    coverages = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    heights = [0.0]
+    widths = [0.0]
+    coverages = [0.25]
     voltages = (1, 2)
-    columns = ['coverage', 'voltage', 'Lx', 'Ly', 'slice_width', 'slice_y_position', 'current (y = 0)', 'current (y = Ly)', 'resistance']
+    n_pieces = [1, 2, 5, 10, 15, 50, 75, 100, 250, 350, 500]
+    pos = ['mid']
+    columns = ['coverage', 'voltage', 'Lx', 'Ly', 'slice_width', 'slice_y_position', 'current (y = 0)', 'current (y = Ly)', 'resistance', 'pos', 'n_pieces']
     utils.make_dir_if_missing("current_constriction")
     with open("current_constriction/current-constriction.csv", "w") as f:
         writer = csv.DictWriter(f, fieldnames=columns)
@@ -52,23 +55,25 @@ if __name__ == '__main__':
         for w in widths:
             for h in heights:
                 for cov in coverages:
-                    try:
-                        resistance, currents = compute_resistance(cov, w, h, voltages=voltages)
-                    except Exception as e:
-                        print(e)
-                        continue
-                    print("processing coverage %0.2f, with slice width %d  at y = %0.2f" % (cov, int(w * 100), h))
-                    for idx, current in enumerate(currents):
-                        writer.writerow(
-                        {
-                            'coverage': cov, 'voltage': voltages[idx], 'Lx': 100, 'Ly': 1, 'slice_width': w * 100,
-                            'slice_y_position': h, 'current (y = 0)': currents[idx][0], 'current (y = Ly)': currents[idx][1],
-                            'resistance': "-"
-                        })
-                    writer.writerow(
-                        {
-                            'coverage': cov, 'voltage': "-", 'Lx': 100, 'Ly': 1, 'slice_width': w * 100,
-                            'slice_y_position': h, 'current (y = 0)': "-", 'current (y = Ly)': "-",
-                            'resistance': np.around(resistance, 2)
-                        }
-                    )
+                    for p in pos:
+                        for num_piece in n_pieces:
+                            try:
+                                resistance, currents = compute_resistance(cov, w, h, voltages=voltages, pos=p, n_pieces=num_piece)
+                            except Exception as e:
+                                print(e)
+                                continue
+                            print("processing coverage %0.2f, with slice width %d  at y = %0.2f" % (cov, int(w * 100), h))
+                            for idx, current in enumerate(currents):
+                                writer.writerow(
+                                {
+                                    'coverage': cov, 'voltage': voltages[idx], 'Lx': 100, 'Ly': 1, 'slice_width': w * 100,
+                                    'slice_y_position': h, 'current (y = 0)': currents[idx][0], 'current (y = Ly)': currents[idx][1],
+                                    'resistance': "-", "pos": p, "n_pieces": num_piece,
+                                })
+                            writer.writerow(
+                                {
+                                    'coverage': cov, 'voltage': "-", 'Lx': 100, 'Ly': 1, 'slice_width': w * 100,
+                                    'slice_y_position': h, 'current (y = 0)': "-", 'current (y = Ly)': "-",
+                                    'resistance': np.around(resistance, 2), "pos": p, "n_pieces": num_piece,
+                                }
+                            )
