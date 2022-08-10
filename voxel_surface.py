896227bd88ff71b7e4a1beaf0c045e51d32e3e7b
Leshinka Molel <emolel3@gatech.edu>
2022-08-10 16:59:15 -0400

    update

--- voxel_surface.py
+++ voxel_surface.py
@@ -25,16 +25,29 @@ logging.basicConfig(format=FORMAT)
 logger = logging.getLogger(__file__)
 logger.setLevel('INFO')
 
+
 if __name__ == '__main__':
     img_dir = sys.argv[1]
     grid_info = sys.argv[2]
     Nx, Ny, Nz = [int(v) for v in grid_info.split("-")]
     nodefile = os.path.join(img_dir, "porous.1.node")
+    tetsfile = os.path.join(img_dir, "porous.1.ele")
     facefile = os.path.join(img_dir, "porous.1.face")
     stlfile = os.path.join(img_dir, "porous.stl")
     mshfile = os.path.join(img_dir, "porous_tria.msh")
-    
-
+    tets_points = set()
+    with open(tetsfile, "r") as fp:
+        line = 0
+        for row in fp.readlines():
+            line += 1
+            if row.startswith("#"):
+                continue
+            if line == 1:
+                num_vertices = int(row.split()[0])
+                vertices = np.zeros((num_vertices, 3), dtype=int)
+                continue
+            tet_id, a, b, c, d = [float(v) for v in row.split()]
+            tets_points.update(set([a, b, c, d]))
     with open(nodefile, 'r') as fp:
         line = 0
         for row in fp.readlines():
@@ -65,9 +78,14 @@ if __name__ == '__main__':
         for face_idx in range(num_faces):
             points = faces[face_idx]
             triangles = []
+            all_in_tet = True
             for point in points:
+                if point not in tets_points:
+                    all_in_tet = False
                 coord = vertices[point]
                 triangles.append(np.array(coord))
+            if not all_in_tet:
+                continue
             p1, p2, p3 = triangles
             n = np.cross(p3 - p2, p2 - p1)
             n = n / n.sum()
@@ -82,18 +100,20 @@ if __name__ == '__main__':
 
     gmsh.initialize()
     gmsh.merge(stlfile)
-    gmsh.model.occ.synchronize()
+    model = gmsh.model
+    factory = model.occ
+    factory.synchronize()
 
-    surfaces = gmsh.model.getEntities(dim=2)
+    surfaces = model.getEntities(dim=2)
 
     insulated = []
     left_cc = []
     right_cc = []
     for surface in surfaces:
-        surf = gmsh.model.addPhysicalGroup(2, [surface[1]], int(time.time()))
-        gmsh.model.setPhysicalName(2, surf, f"S{surf}")
-    gmsh.model.geo.synchronize()
-    gmsh.model.mesh.generate(2)
+        surf = model.addPhysicalGroup(2, [surface[1]], int(time.time()))
+        model.setPhysicalName(2, surf, f"S{surf}")
+    model.geo.synchronize()
+    model.mesh.generate(2)
     gmsh.write(mshfile)
     gmsh.finalize()
     tria_msh = meshio.read(mshfile)
