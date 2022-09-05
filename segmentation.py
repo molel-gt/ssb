f5622bae21ed7cea80af8aad8333baf9cdb3e4c8
Leshinka Molel <emolel3@gatech.edu>
2022-09-05 02:13:49 -0400

    update

--- segmentation.py
+++ segmentation.py
@@ -20,50 +20,54 @@ utils.make_dir_if_missing("unsegmented")
 
 img_dir = "SEM Image/"
 image_files = sorted([os.path.join(img_dir, f) for f in os.listdir(img_dir) if f.endswith(".tif")])
-
-thresholds = [35, 65]
-thresholds2 = [35, 85]
-sigma = 0.5
+# 35 to 65
+thresholds = [40, 65]
+thresholds2 = [47, 65]
+thresholds2 = [35, 65]
+sigma = 1
 
 data = np.zeros((501, 501, 202))
+all_thresholds = []
 
-for idx, img_file in enumerate(image_files):
+for img_file in image_files:
+    idx = int(img_file.split("-")[-1].strip().split(".")[0]) - 1
+    if idx != 201:
+        continue
     fname = str(idx).zfill(3) + ".tif"
-    image = plt.imread(img_file)[1000-idx:1501-idx, 750:1251]
-    img = np.zeros((501, 501))
-    img[:, :] = image
-    data[:, :, idx] = img
+    image = plt.imread(img_file)
+
+    Nx = 641
+    Ny = 961
+    img = np.zeros((Nx, Ny))
+    # img[:, :] = image[1000-idx:1501-idx, 750:1251]
+    # image2 = image[360:1001, 560:1521]
+    image2 = image[340:981, 740-idx:1701-idx]
+    img = image2
 
     img_raw = Image.fromarray(img.astype(np.uint8))
-    # img_raw.save(os.path.join("unsegmented", fname), format="TIFF")
-    # img_laplace = filters.laplace(img)
-    # img_meijering = filters.meijering(img)
-    # img_sobel = filters.sobel(img)
-    # img_hysteresis = filters.apply_hysteresis_threshold(img, low=35, high=55)
-    # img_sato = filters.sato(img)
-    img_gauss_diff = filters.difference_of_gaussians(img, 0.5, 2.5)
-    img_gauss = filters.gaussian(img, sigma=sigma)
-    img_segmented = np.digitize(img_gauss, bins=thresholds)
-    img_segmented2 = np.digitize(img_gauss, bins=thresholds2)
+    img_raw.save(os.path.join("unsegmented", fname), format="TIFF")
+    img_gauss = filters.gaussian(img)
+    thresholds_auto = filters.threshold_multiotsu(img_gauss)
+    all_thresholds.append(thresholds_auto)
+    img_segmented = filters.meijering(img_gauss)
 
-    img_out = Image.fromarray(img_segmented.astype(np.uint8))
-    img_out.save(os.path.join("segmented", fname), format="TIFF")
+    # img_out = Image.fromarray(img_segmented.astype(np.uint8))
+    # img_out.save(os.path.join("segmented", fname), format="TIFF")
 
-    # fig, ax = plt.subplots(1, 5)
-    # ax[0].imshow(img, cmap='gray')
-    # ax[0].set_title("Original")
+    fig, ax = plt.subplots(1, 4)
+    ax[0].imshow(image, cmap='gray')
+    ax[0].set_title("Original")
 
-    # ax[1].hist(img.ravel(), bins=255, density=True)
-    # ax[1].set_title('Image Histogram')
+    ax[1].imshow(img, cmap="gray")
+    ax[1].set_title('Selected Section')
 
-    # ax[2].imshow(img_gauss_diff, cmap='gray')
-    # ax[2].set_title("Gaussian Blur")
+    ax[2].imshow(img_gauss, cmap='gray')
+    ax[2].set_title("Gaussian Blur")
 
-    # ax[3].imshow(img_segmented, cmap='gray')
-    # ax[3].set_title(f"Segmented Gaussian Blur {thresholds.__repr__()}")
+    ax[3].imshow(img_segmented, cmap='gray')
+    ax[3].set_title("Edges Post Gaussian Blur")
 
-    # ax[4].imshow(img_segmented2, cmap='gray')
-    # ax[4].set_title(f"Segmented Gaussian Blur {thresholds2.__repr__()}")
-
-    # plt.show()
-    # break
+    plt.show()
+    break
+all_thresholds = np.array(all_thresholds)
+print(np.average(all_thresholds[:, 0]), np.average(all_thresholds[:, 1]))
