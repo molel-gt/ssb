feaf04381f4841fa79d596554beb0100b220b62e
Leshinka Molel <emolel3@gatech.edu>
2022-10-13 15:48:49 -0400

    update

--- effective_active_material_area.py
+++ effective_active_material_area.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import pickle
 import timeit
 
 import argparse
@@ -40,10 +41,21 @@ if __name__ == '__main__':
     tetr_mesh_path = os.path.join(data_dir, 'tetr.xdmf')
     tria_mesh_path = os.path.join(data_dir, 'tria.xdmf')
 
+    logger.debug("Loading tetrahedra (dim = 3) mesh..")
+
+    with dolfinx.io.XDMFFile(comm, tetr_mesh_path, "r") as infile3:
+        mesh3d = infile3.read_mesh(dolfinx.cpp.mesh.GhostMode.none, 'Grid')
+        ct = infile3.read_meshtags(mesh3d, name="Grid")
+    mesh3d.topology.create_connectivity(mesh3d.topology.dim, mesh3d.topology.dim - 1)
+
     logger.debug("Loading triangles (dim = 2) mesh..")
     with dolfinx.io.XDMFFile(comm, tria_mesh_path, "r") as infile3:
-        mesh = infile3.read_mesh(dolfinx.cpp.mesh.GhostMode.none, 'Grid')
-        facets_ct = infile3.read_meshtags(mesh, name="Grid")
+        mesh2d = infile3.read_mesh(dolfinx.cpp.mesh.GhostMode.none, 'Grid')
+        facets_ct = infile3.read_meshtags(mesh2d, name="Grid")
+
+    logger.debug("Loading effective electrolyte..")
+    with open(os.path.join(data_dir, 'effective_electrolyte.pickle'), 'rb') as handle:
+        effective_electrolyte = list(pickle.load(handle))
 
     left_cc_marker = constants.surface_tags["left_cc"]
     right_cc_marker = constants.surface_tags["right_cc"]
@@ -51,23 +63,55 @@ if __name__ == '__main__':
     active_marker = constants.surface_tags["active_area"]
     inactive_marker = constants.surface_tags["inactive_area"]
 
-    # x0facet = np.array(facets_ct.indices[facets_ct.values == left_cc_marker])
-    # x1facet = np.array(facets_ct.indices[facets_ct.values == right_cc_marker])
-    # active_facet = np.array(facets_ct.indices[facets_ct.values == active_marker])
-    # inactive_facet = np.array(facets_ct.indices[facets_ct.values == inactive_marker])
+    x0facet = np.array(facets_ct.indices[facets_ct.values == left_cc_marker])
+    x1facet = np.array(facets_ct.indices[facets_ct.values == right_cc_marker])
+    insulated_facet = np.array(facets_ct.indices[facets_ct.values == insulated_marker])
+    active_facet = np.array(facets_ct.indices[facets_ct.values == active_marker])
+    inactive_facet = np.array(facets_ct.indices[facets_ct.values == inactive_marker])
 
-    surf_meshtags = dolfinx.mesh.meshtags(mesh, 2, facets_ct.indices, facets_ct.values)
-    n = -ufl.FacetNormal(mesh)
-    ds_left_cc = ufl.Measure('dx', domain=mesh, subdomain_data=surf_meshtags, subdomain_id=left_cc_marker)
-    ds_right_cc = ufl.Measure('dx', domain=mesh, subdomain_data=surf_meshtags, subdomain_id=right_cc_marker)
-    ds_insulated = ufl.Measure('dx', domain=mesh, subdomain_data=surf_meshtags, subdomain_id=insulated_marker)
-    ds_active = ufl.Measure('dx', domain=mesh, subdomain_data=surf_meshtags, subdomain_id=active_marker)
-    ds_inactive = ufl.Measure('dx', domain=mesh, subdomain_data=surf_meshtags, subdomain_id=inactive_marker)
+    # surf_meshtags = dolfinx.mesh.meshtags(mesh2d, 2, facets_ct.indices, facets_ct.values)
+    # n = -ufl.FacetNormal(mesh2d)
+    # ds_left_cc = ufl.Measure('dx', domain=mesh2d, subdomain_data=surf_meshtags, subdomain_id=left_cc_marker)
+    # ds_right_cc = ufl.Measure('dx', domain=mesh2d, subdomain_data=surf_meshtags, subdomain_id=right_cc_marker)
+    # ds_insulated = ufl.Measure('dx', domain=mesh2d, subdomain_data=surf_meshtags, subdomain_id=insulated_marker)
+    # ds_active = ufl.Measure('dx', domain=mesh2d, subdomain_data=surf_meshtags, subdomain_id=active_marker)
+    # ds_inactive = ufl.Measure('dx', domain=mesh2d, subdomain_data=surf_meshtags, subdomain_id=inactive_marker)
+    # x = ufl.SpatialCoordinate(mesh2d)
 
-    # Define variational problem
-    x = ufl.SpatialCoordinate(mesh)
+    x = ufl.SpatialCoordinate(mesh3d)
 
-    logger.debug('Solving problem..')
+    x0facet = dolfinx.mesh.locate_entities_boundary(mesh3d, 2,
+                                    lambda x: np.isclose(x[1], 0.0))
+    x1facet = dolfinx.mesh.locate_entities_boundary(mesh3d, 2,
+                                    lambda x: np.isclose(x[1], Ly))
+    insulated_facet = dolfinx.mesh.locate_entities_boundary(mesh3d, 2, lambda x: np.logical_and(np.logical_not(np.isclose(x[1], 0)), np.logical_not(np.isclose(x[1], Ly))))
+
+    def is_active_area(x, effective_electrolyte, dp=1):
+        ret_val = np.zeros(x.shape[1])
+        for idx in range(x.shape[1]):
+            c = tuple(x[:, idx])
+            coord = (round(c[0]/scale_x, dp), round(c[1]/scale_y, dp), round(c[2]/scale_z, dp))
+            ret_val[idx] = coord in effective_electrolyte
+        ret_val.reshape(-1, 1)
+        return ret_val
+
+    active_facet = dolfinx.mesh.locate_entities_boundary(mesh3d, 2, lambda x: is_active_area(x, effective_electrolyte))
+    inactive_facet = dolfinx.mesh.locate_entities_boundary(mesh3d, 2, lambda x: np.logical_not(is_active_area(x, effective_electrolyte)))
+
+    facets_ct_indices = np.hstack((x0facet, x1facet, insulated_facet))
+    facets_ct_values = np.hstack((np.ones(x0facet.shape[0], dtype=np.int32), right_cc_marker * np.ones(x1facet.shape[0], dtype=np.int32),
+                                insulated_marker * np.ones(insulated_facet.shape[0], dtype=np.int32)))
+    surf_meshtags = dolfinx.mesh.meshtags(mesh3d, 2, facets_ct_indices, facets_ct_values)
+    facets_ct_indices2 = np.hstack((active_facet, inactive_facet))
+    facets_ct_values2 = np.hstack((active_marker * np.ones(active_facet.shape[0], dtype=np.int32), inactive_marker * np.ones(inactive_facet.shape[0], dtype=np.int32)))
+    surf_meshtags2 = dolfinx.mesh.meshtags(mesh3d, 2, facets_ct_indices2, facets_ct_values2)
+    ds_insulated = ufl.Measure("ds", domain=mesh3d, subdomain_data=surf_meshtags, subdomain_id=insulated_marker)
+    ds_left_cc = ufl.Measure('ds', domain=mesh3d, subdomain_data=surf_meshtags, subdomain_id=left_cc_marker)
+    ds_right_cc = ufl.Measure('ds', domain=mesh3d, subdomain_data=surf_meshtags, subdomain_id=right_cc_marker)
+    ds_active = ufl.Measure('ds', domain=mesh3d, subdomain_data=surf_meshtags2, subdomain_id=active_marker)
+    ds_inactive = ufl.Measure('ds', domain=mesh3d, subdomain_data=surf_meshtags2, subdomain_id=inactive_marker)
+
+    logger.debug('Computing areas..')
 
     active_area = dolfinx.fem.assemble_scalar(dolfinx.fem.form(1 * ds_active))
     inactive_area = dolfinx.fem.assemble_scalar(dolfinx.fem.form(1 * ds_inactive))
