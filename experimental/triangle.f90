8d94a5fa83ea9d8e83cf132bfdc8c824d0fe7fd4
Leshinka Molel <emolel3@gatech.edu>
2022-10-14 16:06:47 -0400

    update

--- experimental/triangle.f90
+++ experimental/triangle.f90
@@ -29,40 +29,40 @@ PROGRAM  HeronFormula
  
  END PROGRAM  HeronFormula
 
-program plotfunction
-   implicit none
-   integer :: i
-   real :: x
-   real, parameter :: xmin = 0.,xmax=10., a=-2.
-   open(10, file='myplot.dat')
-      do i = 1,100
-         x = xmin + xmax*(i-1.0)/(100.0-1.0)
-         write(10,*) x, f(x)
-      enddo
-   close(10)
-end program plotfunction
+! program plotfunction
+!    implicit none
+!    integer :: i
+!    real :: x
+!    real, parameter :: xmin = 0.,xmax=10., a=-2.
+!    open(10, file='myplot.dat')
+!       do i = 1,100
+!          x = xmin + xmax*(i-1.0)/(100.0-1.0)
+!          write(10,*) x, f(x)
+!       enddo
+!    close(10)
+! end program plotfunction
 
-function f(x)
-   implicit none
-   real :: f,x
-   f = cos(x+a)
-end function f
+! function f(x)
+!    implicit none
+!    real :: f,x
+!    f = cos(x+a)
+! end function f
 
-program addmats
-   implicit none
-   integer, parameter :: dimmat = 3
-   real, dimension(dimmat, dimmat) :: a, b, c
-   integer :: i, j
-   a(1, 2) = 2.0
-   do i=2, dimmat-1
-      a(i, i+1) = 2.0
-      b(i, i-1) = 1.0
-   enddo
-   b(dimmat, dimmat-1) = 1.0
-   do i=1, dimmat
-      do j=1, dimmat
-         c(i, j) = a(i, j) + b(i, j)
-      enddo
-   enddo
-   write(*,*) c
-end program addmats
+! program addmats
+!    implicit none
+!    integer, parameter :: dimmat = 3
+!    real, dimension(dimmat, dimmat) :: a, b, c
+!    integer :: i, j
+!    a(1, 2) = 2.0
+!    do i=2, dimmat-1
+!       a(i, i+1) = 2.0
+!       b(i, i-1) = 1.0
+!    enddo
+!    b(dimmat, dimmat-1) = 1.0
+!    do i=1, dimmat
+!       do j=1, dimmat
+!          c(i, j) = a(i, j) + b(i, j)
+!       enddo
+!    enddo
+!    write(*,*) c
+! end program addmats
