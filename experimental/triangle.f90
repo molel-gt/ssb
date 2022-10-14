2b0d4533135d1b8db928c7afa68f3a40bad2f41a
Leshinka Molel <emolel3@gatech.edu>
2022-10-14 16:32:35 -0400

    update

--- experimental/triangle.f90
+++ experimental/triangle.f90
@@ -2,32 +2,32 @@
 ! Compute the area of a triangle using Heron's formula
 ! ------------------------------------------------------
 
-PROGRAM  HeronFormula
-    IMPLICIT  NONE
+! PROGRAM  HeronFormula
+!     IMPLICIT  NONE
  
-    REAL     :: a, b, c             ! three sides
-    REAL     :: s                   ! half of perimeter
-    REAL     :: Area                ! triangle area
-    LOGICAL  :: Cond_1, Cond_2      ! two logical conditions
+!     REAL     :: a, b, c             ! three sides
+!     REAL     :: s                   ! half of perimeter
+!     REAL     :: Area                ! triangle area
+!     LOGICAL  :: Cond_1, Cond_2      ! two logical conditions
  
-    READ(*,*)  a, b, c
+!     READ(*,*)  a, b, c
  
-    WRITE(*,*)  "a = ", a
-    WRITE(*,*)  "b = ", b
-    WRITE(*,*)  "c = ", c
-    WRITE(*,*)
+!     WRITE(*,*)  "a = ", a
+!     WRITE(*,*)  "b = ", b
+!     WRITE(*,*)  "c = ", c
+!     WRITE(*,*)
  
-    Cond_1 = (a > 0.) .AND. (b > 0.) .AND. (c > 0.0)
-    Cond_2 = (a + b > c) .AND. (a + c > b) .AND. (b + c > a)
-    IF (Cond_1 .AND. Cond_2) THEN
-       s    = (a + b + c) / 2.0
-       Area = SQRT(s * (s - a) * (s - b) * (s - c))
-       WRITE(*,*) "Triangle area = ", Area
-    ELSE
-       WRITE(*,*) "ERROR: this is not a triangle!"
-    END IF
+!     Cond_1 = (a > 0.) .AND. (b > 0.) .AND. (c > 0.0)
+!     Cond_2 = (a + b > c) .AND. (a + c > b) .AND. (b + c > a)
+!     IF (Cond_1 .AND. Cond_2) THEN
+!        s    = (a + b + c) / 2.0
+!        Area = SQRT(s * (s - a) * (s - b) * (s - c))
+!        WRITE(*,*) "Triangle area = ", Area
+!     ELSE
+!        WRITE(*,*) "ERROR: this is not a triangle!"
+!     END IF
  
- END PROGRAM  HeronFormula
+!  END PROGRAM  HeronFormula
 
 ! program plotfunction
 !    implicit none
@@ -48,21 +48,21 @@ PROGRAM  HeronFormula
 !    f = cos(x+a)
 ! end function f
 
-! program addmats
-!    implicit none
-!    integer, parameter :: dimmat = 3
-!    real, dimension(dimmat, dimmat) :: a, b, c
-!    integer :: i, j
-!    a(1, 2) = 2.0
-!    do i=2, dimmat-1
-!       a(i, i+1) = 2.0
-!       b(i, i-1) = 1.0
-!    enddo
-!    b(dimmat, dimmat-1) = 1.0
-!    do i=1, dimmat
-!       do j=1, dimmat
-!          c(i, j) = a(i, j) + b(i, j)
-!       enddo
-!    enddo
-!    write(*,*) c
-! end program addmats
+program addmats
+   implicit none
+   integer, parameter :: dimmat = 3
+   real, dimension(dimmat, dimmat) :: a, b, c
+   integer :: i, j
+   a(1, 2) = 2.0
+   do i = 2, dimmat - 1
+      a(i, i + 1) = 2.0
+      b(i, i - 1) = 1.0
+   enddo
+   b(dimmat, dimmat - 1) = 1.0
+   do i=1, dimmat
+      do j = 1, dimmat
+         c(i, j) = a(i, j) + b(i, j)
+      enddo
+   enddo
+   write(*,*) c
+end program addmats
