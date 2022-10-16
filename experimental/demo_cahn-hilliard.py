e8f636666625aa5171505859444e7a3a939cec21
Leshinka Molel <emolel3@gatech.edu>
2022-10-16 08:07:11 -0400

    add demo file

---
+++ experimental/demo_cahn-hilliard.py
@@ -0,0 +1,135 @@
+import os
+
+import numpy as np
+
+import ufl
+from dolfinx import log, plot
+from dolfinx.fem import Function, FunctionSpace
+from dolfinx.fem.petsc import NonlinearProblem
+from dolfinx.io import XDMFFile
+from dolfinx.mesh import CellType, create_unit_square
+from dolfinx.nls.petsc import NewtonSolver
+from ufl import dx, grad, inner
+
+from mpi4py import MPI
+from petsc4py import PETSc
+
+try:
+    import pyvista as pv
+    import pyvistaqt as pvqt
+    have_pyvista = True
+    if pv.OFF_SCREEN:
+        pv.start_xvfb(wait=0.5)
+except ModuleNotFoundError:
+    print("pyvista and pyvistaqt are required to visualise the solution")
+    have_pyvista = False
+
+
+# model parameters
+lmbda = 1.0e-02
+dt = 5.0e-06
+theta = 0.5  # time stepping family, e.g. theta=1 -> backward Euler, theta=0.5 -> Crank-Nicholson
+
+msh = create_unit_square(MPI.COMM_WORLD, 96, 96, CellType.triangle)
+P1 = ufl.FiniteElement("Lagrange", msh.ufl_cell(), 1)
+ME = FunctionSpace(msh, P1 * P1)
+
+# Trial and test functions of the space `ME` are now defined:
+q, v = ufl.TestFunctions(ME)
+
+u = Function(ME)  # current solution
+u0 = Function(ME)  # solution from previous converged step
+
+# Split mixed functions
+c, mu = ufl.split(u)
+c0, mu0 = ufl.split(u0)
+
+# The initial conditions are interpolated into a finite element space
+# Zero u
+u.x.array[:] = 0.0
+
+# Interpolate initial condition
+u.sub(0).interpolate(lambda x: 0.63 + 0.02 * (0.5 - np.random.rand(x.shape[1])))
+u.x.scatter_forward()
+
+# Compute the chemical potential df/dc
+c = ufl.variable(c)
+f = 100 * c ** 2 * (1 - c) ** 2
+dfdc = ufl.diff(f, c)
+
+# mu_(n+theta)
+mu_mid = (1.0 - theta) * mu0 + theta * mu
+
+# Weak statement of the equations
+F0 = inner(c, q) * dx - inner(c0, q) * dx + dt * inner(grad(mu_mid), grad(q)) * dx
+F1 = inner(mu, v) * dx - inner(dfdc, v) * dx - lmbda * inner(grad(c), grad(v)) * dx
+F = F0 + F1
+
+# Create nonlinear problem and Newton solver
+problem = NonlinearProblem(F, u)
+solver = NewtonSolver(MPI.COMM_WORLD, problem)
+solver.convergence_criterion = "incremental"
+solver.rtol = 1e-6
+
+# customize the linear solver used inside the NewtonSolver
+ksp = solver.krylov_solver
+opts = PETSc.Options()
+option_prefix = ksp.getOptionsPrefix()
+opts[f"{option_prefix}ksp_type"] = "preonly"
+opts[f"{option_prefix}pc_type"] = "lu"
+opts[f"{option_prefix}pc_factor_mat_solver_type"] = "mumps"
+ksp.setFromOptions()
+
+# Output file
+file = XDMFFile(MPI.COMM_WORLD, "demo_ch/output.xdmf", "w")
+file.write_mesh(msh)
+
+# Step in time
+t = 0.0
+
+T = 50 * dt
+
+# Get the sub-space for c and the corresponding dofs in the mixed space
+# vector
+V0, dofs = ME.sub(0).collapse()
+
+# Prepare viewer for plotting the solution during the computation
+if have_pyvista:
+    # Create a VTK 'mesh' with 'nodes' at the function dofs
+    topology, cell_types, x = plot.create_vtk_mesh(V0)
+    grid = pv.UnstructuredGrid(topology, cell_types, x)
+
+    # Set output data
+    grid.point_data["c"] = u.x.array[dofs].real
+    grid.set_active_scalars("c")
+
+    p = pvqt.BackgroundPlotter(title="concentration", auto_update=True)
+    p.add_mesh(grid, clim=[0, 1])
+    p.view_xy(True)
+    p.add_text(f"time: {t}", font_size=12, name="timelabel")
+
+c = u.sub(0)
+u0.x.array[:] = u.x.array
+while (t < T):
+    t += dt
+    r = solver.solve(u)
+    print(f"Step {int(t/dt)}: num iterations: {r[0]}")
+    u0.x.array[:] = u.x.array
+    file.write_function(c, t)
+
+    # Update the plot window
+    if have_pyvista:
+        p.add_text(f"time: {t:.2e}", font_size=12, name="timelabel")
+        grid.point_data["c"] = u.x.array[dofs].real
+        p.app.processEvents()
+
+file.close()
+
+# Update ghost entries and plot
+if have_pyvista:
+    u.x.scatter_forward()
+    grid.point_data["c"] = u.x.array[dofs].real
+    screenshot = None
+    if pv.OFF_SCREEN:
+        screenshot = "c.png"
+    pv.plot(grid, show_edges=True, screenshot=screenshot)
