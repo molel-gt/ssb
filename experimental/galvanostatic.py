983a58bec5b115c283b87c5d12b618d47f0cc2e4
Leshinka Molel <emolel3@gatech.edu>
2022-10-28 19:19:35 -0400

    update

--- experimental/galvanostatic.py
+++ experimental/galvanostatic.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
+import sys
+
 import dolfinx
 import numpy as np
 import pyvista
 import ufl
 
 from dolfinx import fem, io, mesh, plot
-from ufl import ds, dx, grad, inner
+# from ufl import ds, dx, grad, inner
 from mpi4py import MPI
 from petsc4py import PETSc
 
+sys.path.append("../")
 
+from ssb import commons
+
+markers = commons.SurfaceMarkers()
 Lx = 2
 Ly = 2
 K0 = 0.1  # siemens per meter --> bulk conductivity
@@ -38,22 +44,43 @@ v = ufl.TestFunction(V)
 
 f = dolfinx.fem.Constant(mesh2d, PETSc.ScalarType(0.0))
 g = dolfinx.fem.Constant(mesh2d, PETSc.ScalarType(0.0))
-a = inner(K0 * grad(u), grad(v)) * dx
-L = inner(f, v) * dx + inner(g, v) * ds
+g_cc = dolfinx.fem.Constant(mesh2d, PETSc.ScalarType(1e-4))
 # left_cc_value = (-i0 / K0) * (ufl.exp(alpha_a * F * (u0 - u_am) / (R * T)) - ufl.exp(-alpha_c * F * (u - u_am) / (R * T)))
 # right_cc_value = (-i0 / K0) * (ufl.exp(alpha_a * F * (u0 - u_am) / (R * T)) - ufl.exp(-alpha_c * F * (u - u_am) / (R * T)))
 
 # boundary conditions
 left_cc_facets = mesh.locate_entities_boundary(mesh2d, dim=1, marker=lambda x: np.isclose(x[0], 0.0))
 right_cc_facets = mesh.locate_entities_boundary(mesh2d, dim=1, marker=lambda x: np.isclose(x[0], Lx))
+insulated_facet = dolfinx.mesh.locate_entities_boundary(mesh2d, 1, lambda x: np.logical_and(np.logical_not(np.isclose(x[0], 0)), np.logical_not(np.isclose(x[0], Lx))))
+
 left_cc_dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=left_cc_facets)
 right_cc_dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=right_cc_facets)
 
-# set bcs
-left_cc = fem.dirichletbc(value=PETSc.ScalarType(1), dofs=left_cc_dofs, V=V)
-right_cc = fem.dirichletbc(value=PETSc.ScalarType(0), dofs=right_cc_dofs, V=V)
+# facets_ct_indices = np.hstack((left_cc_facets, right_cc_facets, insulated_facet))
+facets_ct_indices = np.hstack((left_cc_facets, insulated_facet))
+# facets_ct_values = np.hstack((markers.left_cc * np.ones(left_cc_facets.shape[0], dtype=np.int32), markers.right_cc * np.ones(right_cc_facets.shape[0], dtype=np.int32),
+#                             markers.insulated * np.ones(insulated_facet.shape[0], dtype=np.int32)))
+facets_ct_values = np.hstack((markers.left_cc * np.ones(left_cc_facets.shape[0], dtype=np.int32), markers.insulated * np.ones(insulated_facet.shape[0], dtype=np.int32)))
+facets_ct = commons.Facet(facets_ct_indices, facets_ct_values)
+surf_meshtags = dolfinx.mesh.meshtags(mesh2d, 1, facets_ct.indices, facets_ct.values)
 
-problem = fem.petsc.LinearProblem(a, L, bcs=[left_cc, right_cc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+ds = ufl.Measure("ds", domain=mesh2d, subdomain_data=surf_meshtags, subdomain_id=markers.insulated)
+ds_left_cc = ufl.Measure('ds', domain=mesh2d, subdomain_data=surf_meshtags, subdomain_id=markers.left_cc)
+ds_right_cc = ufl.Measure('ds', domain=mesh2d, subdomain_data=surf_meshtags, subdomain_id=markers.right_cc)
+
+a = ufl.inner(K0 * ufl.grad(u), ufl.grad(v)) * ufl.dx
+L = ufl.inner(f, v) * ufl.dx + ufl.inner(g, v) * ds + ufl.inner(g_cc, v) * ds_left_cc #+ ufl.inner(g_cc, v) * ds_right_cc
+
+# set bcs
+# left_cc = fem.dirichletbc(value=PETSc.ScalarType(1), dofs=left_cc_dofs, V=V)
+right_cc = fem.dirichletbc(value=PETSc.ScalarType(0), dofs=right_cc_dofs, V=V)
+# bcs = [left_cc, right_cc]
+options = {
+               "ksp_type": "gmres",
+               "pc_type": "hypre",
+               "ksp_rtol": 1.0e-12
+               }
+problem = fem.petsc.LinearProblem(a, L, bcs=[right_cc], petsc_options=options)
 uh = problem.solve()
 
 with dolfinx.io.XDMFFile(mesh2d.comm, "out_poisson/poisson.xdmf", "w") as file:
@@ -61,12 +88,12 @@ with dolfinx.io.XDMFFile(mesh2d.comm, "out_poisson/poisson.xdmf", "w") as file:
     file.write_function(uh)
 
 # Visualization
-cells, types, x = plot.create_vtk_mesh(V)
-grid = pyvista.UnstructuredGrid(cells, types, x)
-grid.point_data["u"] = uh.x.array.real
-grid.set_active_scalars("u")
-plotter = pyvista.Plotter()
-plotter.add_mesh(grid, show_edges=True)
-warped = grid.warp_by_scalar()
-plotter.add_mesh(warped)
-plotter.show()
+# cells, types, x = plot.create_vtk_mesh(V)
+# grid = pyvista.UnstructuredGrid(cells, types, x)
+# grid.point_data["u"] = uh.x.array.real
+# grid.set_active_scalars("u")
+# plotter = pyvista.Plotter()
+# plotter.add_mesh(grid, show_edges=True)
+# warped = grid.warp_by_scalar()
+# plotter.add_mesh(warped)
+# plotter.show()
