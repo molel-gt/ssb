cc3455257ba23a0f98d89741490deee7667b8994
Leshinka Molel <emolel3@gatech.edu>
2022-10-31 20:07:26 -0400

    update

--- laminate.py
+++ laminate.py
@@ -9,6 +9,7 @@ import logging
 import numpy as np
 import ufl
 
+from dolfinx import mesh, fem, io, nls, log
 from mpi4py import MPI
 from petsc4py import PETSc
 
@@ -16,7 +17,7 @@ import commons, configs, constants
 
 
 markers = commons.SurfaceMarkers()
-
+phases = commons.Phases()
 # Some constants
 D_am = 5e-15
 D_se = 0
@@ -26,6 +27,14 @@ sigma_se = 0
 # ionic conductivity
 kappa_am = 0
 kappa_se = 0.1
+
+i0 = 10  # exchange current density
+phi2 = 0.5
+F_c = 96485  # Faraday constant
+R = 8.314
+T = 298
+alpha_a = 0.5
+alpha_c = 0.5
 
 
 if __name__ == '__main__':
@@ -64,12 +73,12 @@ if __name__ == '__main__':
     right_cc_marker = markers.right_cc
     insulated_marker = markers.insulated
 
-    with dolfinx.io.XDMFFile(MPI.COMM_WORLD, "mesh/laminate/tria.xdmf", "r") as xdmf:
+    with io.XDMFFile(MPI.COMM_WORLD, "mesh/laminate/tria.xdmf", "r") as xdmf:
         mesh2d = xdmf.read_mesh(name="Grid")
         ct = xdmf.read_meshtags(mesh2d, name="Grid")
 
     mesh2d.topology.create_connectivity(dolfinx.mesh.topology.dim, dolfinx.mesh.topology.dim - 1)
-    with dolfinx.io.XDMFFile(MPI.COMM_WORLD, "mesh/laminate/line.xdmf", "r") as xdmf:
+    with io.XDMFFile(MPI.COMM_WORLD, "mesh/laminate/line.xdmf", "r") as xdmf:
         mesh1d = xdmf.read_mesh(name="Grid")
         ft = xdmf.read_meshtags(mesh1d, name="Grid")
 
@@ -77,8 +86,8 @@ if __name__ == '__main__':
     kappa = dolfinx.Function(Q)
     sigma = dolfinx.Function(Q)
     d_eff = dolfinx.Function(Q)
-    se_cells = ct.find(1)
-    am_cells = ct.find(3)
+    se_cells = ct.find(phases.electrolyte)
+    am_cells = ct.find(phases.active_material)
     kappa.x.array[am_cells] = np.full_like(am_cells, kappa_am, dtype=PETSc.ScalarType)
     kappa.x.array[se_cells]  = np.full_like(se_cells, kappa_se, dtype=PETSc.ScalarType)
     sigma.x.array[am_cells] = np.full_like(am_cells, sigma_am, dtype=PETSc.ScalarType)
@@ -87,3 +96,71 @@ if __name__ == '__main__':
     d_eff.x.array[se_cells]  = np.full_like(se_cells, D_se, dtype=PETSc.ScalarType)
 
     x = ufl.SpatialCoordinate(mesh2d)
+    # additions
+    f = fem.Constant(mesh2d, PETSc.ScalarType(0.0))
+    n = ufl.FacetNormal(mesh2d)
+    g = fem.Constant(mesh2d, PETSc.ScalarType(0.0))
+
+    V = fem.FunctionSpace(mesh2d, ("CG", 1))
+    u, v = fem.Function(V), ufl.TestFunction(V)
+
+    def i_butler_volmer(phi1=u, phi2=phi2):
+        return i0  * (ufl.exp(alpha_a * F_c * (u - phi2) / R / T) - ufl.exp(-alpha_c * F_c * (u - phi2) / R / T))
+
+    g_curr = -i_butler_volmer() / kappa
+
+    fdim = mesh2d.topology.dim - 1
+    facet_tag = mesh.meshtags(mesh2d, fdim, ft.indices, ft.values)
+
+    ds = ufl.Measure("ds", domain=mesh2d, subdomain_data=facet_tag)
+    u0 = dolfinx.fem.Function(V)
+    with u0.vector.localForm() as u0_loc:
+        u0_loc.set(0)
+
+    x0facet = dolfinx.mesh.locate_entities_boundary(mesh2d, 1, lambda x: np.isclose(x[0], 0.0))
+    x0bc = dolfinx.fem.dirichletbc(u0, dolfinx.fem.locate_dofs_topological(V, 1, x0facet))
+
+    F = ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx - f * v * ufl.dx - ufl.inner(g_curr, v) * ds(markers.right_cc) - ufl.inner(g, v) * ds(markers.insulated) - ufl.inner(g, v) * ds(markers.insulated)
+
+    problem = fem.petsc.NonlinearProblem(F, u, bcs=[x0bc])
+    solver = nls.petsc.NewtonSolver(comm, problem)
+    solver.convergence_criterion = "incremental"
+    solver.rtol = 1e-6
+    solver.maximum_iterations = 100
+    solver.report = True
+
+    ksp = solver.krylov_solver
+    opts = PETSc.Options()
+    option_prefix = ksp.getOptionsPrefix()
+    opts[f"{option_prefix}ksp_type"] = "cg"
+    opts[f"{option_prefix}pc_type"] = "gamg"
+    opts[f"{option_prefix}pc_factor_mat_solver_type"] = "mumps"
+    opts[f"{option_prefix}maximum_iterations"] = 100
+    ksp.setFromOptions()
+
+    log.set_log_level(log.LogLevel.WARNING)
+    n, converged = solver.solve(u)
+    assert(converged)
+    print(f"Number of interations: {n:d}")
+
+    with dolfinx.io.XDMFFile(comm, "mesh/laminate/potential.xdmf", "w") as file:
+        file.write_mesh(mesh2d)
+        file.write_function(u)
+
+    grad_u = ufl.grad(u)
+    area_left_cc = dolfinx.fem.assemble_scalar(dolfinx.fem.form(1 * ds(1)))
+    area_right_cc = dolfinx.fem.assemble_scalar(dolfinx.fem.form(1 * ds(2)))
+    i_left_cc = (1/area_left_cc) * dolfinx.fem.assemble_scalar(dolfinx.fem.form(kappa * ufl.sqrt(ufl.inner(grad_u, grad_u)) * ds(1)))
+    i_right_cc = (1/area_right_cc) * dolfinx.fem.assemble_scalar(dolfinx.fem.form(kappa * ufl.sqrt(ufl.inner(grad_u, grad_u)) * ds(2)))
+
+    W = dolfinx.fem.FunctionSpace(mesh2d, ("Lagrange", 1))
+    current_expr = dolfinx.fem.Expression(kappa * ufl.sqrt(ufl.inner(grad_u, grad_u)), W.element.interpolation_points())
+    current_h = dolfinx.fem.Function(W)
+    current_h.interpolate(current_expr)
+
+    with dolfinx.io.XDMFFile(comm, "mesh/laminate/current.xdmf", "w") as file:
+        file.write_mesh(mesh2d)
+        file.write_function(current_h)
+
+    print("Current density @ left cc                       : {:.4f}".format(i_left_cc))
+    print("Current density @ right cc                      : {:.4f}".format(i_right_cc))
