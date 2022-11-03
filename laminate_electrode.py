1109c1bd92c33c741013fffd6eb0cfe4bb0f467c
Leshinka Molel <emolel3@gatech.edu>
2022-11-03 12:14:04 -0400

    update

--- laminate_electrode.py
+++ laminate_electrode.py
@@ -20,7 +20,7 @@ eps_se = 0.3
 eps_am = 0.7
 
 # meshing
-resolution = 0.05
+resolution = 0.1
 L_coating = L - 5 #* resolution
 
 points = [
@@ -44,22 +44,28 @@ for p in points:
     tag = gmsh.model.occ.addPoint(*p, meshSize=resolution)
     gmsh_points.append(tag)
 gmsh.model.occ.synchronize()
-se_lines = []
-points_se = gmsh_points[:8]
-for i in range(-1, len(points_se) - 1):
-    line = gmsh.model.occ.addLine(points_se[i], points_se[i + 1])
-    se_lines.append(line)
-se_loop = gmsh.model.occ.addCurveLoop(se_lines)
-se_channel = gmsh.model.occ.addPlaneSurface((1, se_loop))
+
+bottom_lines = []
+bottom_lines.append(gmsh.model.occ.addLine(gmsh_points[0], gmsh_points[7]))
+bottom_lines.append(gmsh.model.occ.addLine(gmsh_points[7], gmsh_points[8]))
+top_lines = []
+top_lines.append(gmsh.model.occ.addLine(gmsh_points[1], gmsh_points[2]))
+top_lines.append(gmsh.model.occ.addLine(gmsh_points[2], gmsh_points[9]))
+am_se_interface_lines = []
+for i in range(2, 7):
+    am_se_interface_lines.append(gmsh.model.occ.addLine(gmsh_points[i], gmsh_points[i+1]))
 gmsh.model.occ.synchronize()
-top_coating_line = gmsh.model.occ.addLine(gmsh_points[2], gmsh_points[-1])
-right_cc_line = gmsh.model.occ.addLine(gmsh_points[-2], gmsh_points[-1])
-bottom_coating_line = gmsh.model.occ.addLine(gmsh_points[-3], gmsh_points[-2])
-am_loop = gmsh.model.occ.addCurveLoop(se_lines[3:] + [bottom_coating_line, right_cc_line, top_coating_line])
+left_cc_line = gmsh.model.occ.addLine(gmsh_points[0], gmsh_points[1])
+right_cc_line = gmsh.model.occ.addLine(gmsh_points[8], gmsh_points[9])
+gmsh.model.occ.synchronize()
+se_loop = gmsh.model.occ.addCurveLoop([left_cc_line, top_lines[0]] + am_se_interface_lines + [bottom_lines[0]])
+se_channel = gmsh.model.occ.addPlaneSurface((1, se_loop))
+am_loop = gmsh.model.occ.addCurveLoop(am_se_interface_lines + [top_lines[1], right_cc_line, bottom_lines[1]])
 am_channel = gmsh.model.occ.addPlaneSurface((2, am_loop))
 gmsh.model.occ.synchronize()
 gmsh.model.occ.fragment([(2, se_channel)], [(2, am_channel)])
 gmsh.model.occ.synchronize()
+
 # surfaces = gmsh.model.getEntities(dim=2)
 # lines = gmsh.model.getEntities(dim=1)
 s1 = gmsh.model.addPhysicalGroup(2, [se_channel], phases.electrolyte)
@@ -68,14 +74,17 @@ gmsh.model.occ.synchronize()
 s2 = gmsh.model.addPhysicalGroup(2, [am_channel], phases.active_material)
 gmsh.model.setPhysicalName(2, s2, "AM")
 gmsh.model.occ.synchronize()
-grp1 = gmsh.model.addPhysicalGroup(1, [se_lines[1]], markers.left_cc)
+grp1 = gmsh.model.addPhysicalGroup(1, [left_cc_line], markers.left_cc)
 gmsh.model.setPhysicalName(1, grp1, "left_cc")
 gmsh.model.occ.synchronize()
 grp2 = gmsh.model.addPhysicalGroup(1, [right_cc_line], markers.right_cc)
 gmsh.model.setPhysicalName(1, grp2, "right_cc")
 gmsh.model.occ.synchronize()
-grp3 = gmsh.model.addPhysicalGroup(1, se_lines[3:], markers.am_se_interface)
+grp3 = gmsh.model.addPhysicalGroup(1, am_se_interface_lines, markers.am_se_interface)
 gmsh.model.setPhysicalName(1, grp3, "am_se_interface")
+gmsh.model.occ.synchronize()
+grp4 = gmsh.model.addPhysicalGroup(1, top_lines + bottom_lines, markers.insulated)
+gmsh.model.setPhysicalName(1, grp4, "insulated")
 gmsh.model.occ.synchronize()
 gmsh.model.mesh.generate(2)
 gmsh.write("mesh/laminate/mesh.msh")
