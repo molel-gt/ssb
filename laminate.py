de6ab15a201c6ca2ed23862ac3af3d0002c35222
Leshinka Molel <emolel3@gatech.edu>
2022-11-03 14:10:59 -0400

    update

--- laminate.py
+++ laminate.py
@@ -22,14 +22,14 @@ phases = commons.Phases()
 D_am = 5e-15
 D_se = 0
 # electronic conductivity
-sigma_am = 5e3
-sigma_se = 0
+sigma_am = 1e-1
+sigma_se = 1e-3
 # ionic conductivity
 kappa_am = 0
 kappa_se = 0.1
 
 i0 = 1e-2  # exchange current density
-phi2 = 0.5
+phi2 = 0.495
 F_c = 96485  # Faraday constant
 R = 8.314
 T = 298
@@ -89,21 +89,13 @@ if __name__ == '__main__':
     n = ufl.FacetNormal(mesh2d)
     g = fem.Constant(mesh2d, PETSc.ScalarType(0.0))
 
-    V = fem.FunctionSpace(mesh2d, ("CG", 1))
-    # u_se, u_am = fem.Function(V), fem.Function(V)
-    # q, v = ufl.TestFunction(V), ufl.TestFunction(V)
+    V = fem.FunctionSpace(mesh2d, ("Lagrange", 2))
     v = ufl.TestFunction(V)
-
-    # V1 = ufl.VectorElement("CG", mesh2d.ufl_cell(), 1)
-    # V = fem.FunctionSpace(mesh2d, V1 * V1)
-    # (q, v) = ufl.TestFunctions(V)
     u = fem.Function(V)
-    # u_se, u_am = ufl.split(u)
+    # def i_butler_volmer(phi1=u, phi2=phi2):
+    #     return i0  * (ufl.exp(alpha_a * F_c * (phi1 - phi2) / R / T) - ufl.exp(-alpha_c * F_c * (phi1 - phi2) / R / T))
 
-    def i_butler_volmer(phi1=u, phi2=phi2):
-        return i0  * (ufl.exp(alpha_a * F_c * (phi1 - phi2) / R / T) - ufl.exp(-alpha_c * F_c * (phi1 - phi2) / R / T))
-
-    left_cc_curr = -i_butler_volmer() / kappa
+    # left_cc_curr = -i_butler_volmer() / kappa
 
     fdim = mesh2d.topology.dim - 1
     facet_tag = mesh.meshtags(mesh2d, fdim, ft.indices, ft.values)
@@ -111,7 +103,7 @@ if __name__ == '__main__':
     ds = ufl.Measure("ds", domain=mesh2d, subdomain_data=facet_tag)
     u_left_cc = fem.Function(V)
     with u_left_cc.vector.localForm() as u0_loc:
-        u0_loc.set(0)
+        u0_loc.set(0.5)
     
     u_right_cc = fem.Function(V)
     with u_right_cc.vector.localForm() as u0_loc:
@@ -124,27 +116,7 @@ if __name__ == '__main__':
     right_cc_dofs = fem.locate_dofs_topological(V, 1, right_cc_facet)
     right_cc = fem.dirichletbc(u_right_cc, fem.locate_dofs_topological(V, 1, right_cc_facet))
 
-    # Trial 1
-    F = (kappa + sigma) * ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx - f * v * ufl.dx - ufl.inner(left_cc_curr, v) * ds(markers.left_cc) - ufl.inner(g, v) * ds(markers.insulated) - ufl.inner(g, v) * ds(markers.insulated)
-    
-    # Trial 2
-    # F_elec = sigma * ufl.inner(ufl.grad(u_am), ufl.grad(v)) * ufl.dx \
-    #          - f * v * ufl.dx \
-    #          - ufl.inner(g, v) * ds(markers.insulated)\
-    #          + ufl.inner(ufl.grad(u_am), ufl.grad(v)) * ds(markers.am_se_interface)
-            #  + ufl.inner(left_cc_curr, v) * ds(markers.am_se_interface)\
-
-    # F_ion = kappa * ufl.inner(ufl.grad(u_se), ufl.grad(q)) * ufl.dx \
-    #         - f * q * ufl.dx \
-    #         - ufl.inner(g, q) * ds(markers.insulated)\
-    #         + ufl.inner(left_cc_curr, q) * ds(markers.am_se_interface)\
-    #         - ufl.inner(ufl.grad(u_se), ufl.grad(q)) * ds(markers.am_se_interface)
-    # F = F_ion + F_elec
-    # Trial 3
-    # F_elec = ufl.inner(sigma * ufl.grad(u), ufl.grad(v)) * ufl.dx(2) - ufl.inner(f, v) * ufl.dx(2) - ufl.inner(g, v) * ds (markers.insulated)
-    # F_ion = ufl.inner(kappa * ufl.grad(u), ufl.grad(v)) * ufl.dx(1) - ufl.inner(f, v) * ufl.dx(1) - ufl.inner(g, v) * ds (markers.insulated)
-    # F = F_ion + F_elec
-
+    F = sigma * ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx - f * v * ufl.dx - ufl.inner(g, v) * ds(markers.insulated)
     problem = fem.petsc.NonlinearProblem(F, u, bcs=[left_cc, right_cc])
     solver = nls.petsc.NewtonSolver(comm, problem)
     solver.convergence_criterion = "incremental"
@@ -163,8 +135,6 @@ if __name__ == '__main__':
 
     log.set_log_level(log.LogLevel.WARNING)
     n, converged = solver.solve(u)
-    # n, converged = solver.solve(u_am)
-    assert(converged)
     print(f"Number of interations: {n:d}")
 
     with io.XDMFFile(comm, output_potential_path, "w") as file:
@@ -172,10 +142,10 @@ if __name__ == '__main__':
         file.write_function(u)
 
     grad_u = ufl.grad(u)
-    area_left_cc = fem.assemble_scalar(fem.form(1 * ds(1)))
-    area_right_cc = fem.assemble_scalar(fem.form(1 * ds(2)))
-    i_left_cc = (1/area_left_cc) * fem.assemble_scalar(fem.form(kappa * ufl.sqrt(ufl.inner(grad_u, grad_u)) * ds(1)))
-    i_right_cc = (1/area_right_cc) * fem.assemble_scalar(fem.form(kappa * ufl.sqrt(ufl.inner(grad_u, grad_u)) * ds(2)))
+    area_left_cc = fem.assemble_scalar(fem.form(1 * ds(markers.left_cc)))
+    area_right_cc = fem.assemble_scalar(fem.form(1 * ds(markers.right_cc)))
+    i_left_cc = (1/area_left_cc) * fem.assemble_scalar(fem.form(kappa * ufl.sqrt(ufl.inner(grad_u, grad_u)) * ds(markers.left_cc)))
+    i_right_cc = (1/area_right_cc) * fem.assemble_scalar(fem.form(sigma * ufl.sqrt(ufl.inner(grad_u, grad_u)) * ds(markers.right_cc)))
 
     W = fem.FunctionSpace(mesh2d, ("Lagrange", 1))
     current_expr = fem.Expression(kappa * ufl.sqrt(ufl.inner(grad_u, grad_u)), W.element.interpolation_points())
