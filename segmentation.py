5a8c3ad7f1326e8b910c32a2e59ca54a1ffcb872
Leshinka Molel <emolel3@gatech.edu>
2023-02-14 14:09:59 -0500

    add codes

---
+++ segmentation.py
@@ -0,0 +1,148 @@
+import itertools
+import os
+import sys
+import time
+
+import cv2
+import hdbscan
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+# %matplotlib ipympl
+import mpl_interactions.ipyplot as iplt
+import networkx as nx
+import numpy as np
+import pandas as pd
+import pickle
+import seaborn as sns
+import warnings
+
+from collections import defaultdict
+from mpl_toolkits import mplot3d
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
+from PIL import Image
+from scipy.cluster.hierarchy import fcluster, linkage, dendrogram
+from scipy import ndimage, signal, stats
+from skimage import io, filters, segmentation
+from skimage.segmentation import felzenszwalb, mark_boundaries, quickshift, slic, watershed
+from sklearn import metrics
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.decomposition import PCA
+from sklearn.ensemble import AdaBoostClassifier, ExtraTreesClassifier, HistGradientBoostingClassifier, RandomForestClassifier
+from sklearn.mixture import BayesianGaussianMixture, GaussianMixture
+from sklearn.naive_bayes import GaussianNB, ComplementNB, BernoulliNB, MultinomialNB
+from sklearn.neighbors import KernelDensity, NearestNeighbors, KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+
+# %matplotlib notebook
+warnings.simplefilter("ignore")
+hdbscan_kwargs = {
+    "min_cluster_size": 25,
+    "cluster_selection_epsilon": 5,
+    "gen_min_span_tree": True
+    }
+image_ids = np.array(range(202)).reshape(-1, 1)
+thresholds = np.linspace(0, 0.1, num=41).reshape(-1, 1)
+actions = ['labeling', 'predicting']
+
+
+def load_clusters(X_2d, y_predict, shape=(501, 501)):
+    img_seg = -2 * np.ones(shape)
+    for v in np.unique(y_predict):
+        X_v = np.argwhere(y_predict == v)
+        coords = list(itertools.chain.from_iterable(X_v))
+        for coord in coords:
+            xx, yy = X_2d[int(coord), :2]
+            img_seg[int(xx), int(yy)] = v
+    return img_seg
+
+
+def enhance_clusters(img_seg):
+    new_img = -2 * np.ones(img_seg.shape)
+    for i in range(img_seg.shape[0]):
+        for j in range(img_seg.shape[1]):
+            v = img_seg[i, j]
+            if v != -2:
+                new_img[i, j] = v
+                continue
+            neighs = [
+                (i, j + 1),
+                (i + 1, j),
+                (i, j - 1),
+                (i - 1, j),
+                (i + 1, j + 1),
+                (i - 1, j - 1),
+                (i - 1, j + 1),
+                (i + 1, j - 1),
+                ]
+            vals = []
+            for neigh in neighs:
+                try:
+                    vals.append(img_seg[neigh])
+                except IndexError:
+                    continue
+            set_vals = set(vals)
+            if -2 in set_vals:
+                set_vals.remove(-2)
+            if len(set_vals) == 1:
+                new_img[i, j] = set_vals.pop()
+    
+    return new_img
+
+
+def neighborhood_average(arr, d=(1, 1), n_min=(0, 0), n_max=(501, 501)):
+    out = np.zeros(arr.shape)
+    dx, dy = d
+    for i in range(n_max[0]):
+        for j in range(n_max[1]):
+            val = arr[i, j]
+            neighbors = arr[max(i - dx, 0):min(i + dx, n_max[0] - 1), max(j - dy, 0):min(j + dy, n_max[1] - 1)]
+            out[i, j] = np.mean(neighbors)
+    return out
+
+
+def neighborhood_mode(arr, d=(1, 1), n_min=(0, 0), n_max=(501, 501)):
+    out = np.zeros(arr.shape)
+    dx, dy = d
+    for i in range(n_max[0]):
+        for j in range(n_max[1]):
+            val = arr[i, j]
+            neighbors = arr[max(i - dx, 0):min(i + dx, n_max[0] - 1), max(j - dy, 0):min(j + dy, n_max[1] - 1)]
+            out[i, j] = np.mode(neighbors)[0][0]
+
+    return out
+
+
+def center_of_mass(arr):
+    xcom = np.average(arr[:, 0])
+    ycom = np.average(arr[:, 1])
+
+    return xcom, ycom
+
+
+def build_features_matrix(img, img_1, threshold=0.035):
+    """"""
+    coords = np.asarray(np.where(np.logical_and(np.greater_equal(img, 0), np.less_equal(img, threshold)))).T
+    y = np.array([img_1[ix, iy] for (ix, iy) in coords]).reshape(-1, 1) / 255
+    y_ = np.array([img[ix, iy] for (ix, iy) in coords]).reshape(-1, 1)
+    X_2d = np.hstack((coords, y, y_))
+
+    return X_2d
+
+
+def get_clustering_results(X_2d, **hdbscan_kwargs):
+    # hdbscan
+    clusterer = hdbscan.HDBSCAN(**hdbscan_kwargs)
+    y_predict = clusterer.fit_predict(X_2d).reshape(-1, 1)
+
+    return y_predict
+
+
+def run_segmentation(image_id, threshold, action='labeling'):
+    return
+
+fig, ax = plt.subplots(2, 2)
+controls = iplt.plot(run_segmentation, image_id=image_ids, threshold=thresholds)
