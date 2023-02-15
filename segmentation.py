49f4d04d4f1fc2608790828ad4db6d4c37ed8020
Leshinka Molel <emolel3@gatech.edu>
2023-02-15 03:26:04 -0500

    label

--- segmentation.py
+++ segmentation.py
@@ -17,7 +17,9 @@ import pickle
 import warnings
 
 # from collections import defaultdict
-from matplotlib.widgets import Slider, Button#, RadioButtons
+from matplotlib.image import AxesImage
+from matplotlib.widgets import Slider, Button, PolygonSelector, RadioButtons, LassoSelector
+from matplotlib.path import Path
 # from matplotlib.ticker import (MultipleLocator)
 
 # from PIL import Image
@@ -33,16 +35,135 @@ hdbscan_kwargs = {
     "cluster_selection_epsilon": 5,
     "gen_min_span_tree": True
     }
+phases = {
+    "Void": 0,
+    "SE": 1,
+    "AM": 2,
+    }
+
 # defaults
+phase = 1
 image_id = 0
 threshold = 0.0325
+NZ = 202
 # options
-image_ids = np.array(range(202))
+image_ids = np.array(range(NZ))
 thresholds = np.linspace(0, 0.1, num=41)
 actions = ['label', 'predict']
-# plotter = pyvista.Plotter()
 
-fig, ax = plt.subplots(3, 2, figsize=(12, 12))
+
+class SelectFromCollection1:
+    """
+    Select indices from a matplotlib collection using `PolygonSelector`.
+
+    Selected indices are saved in the `ind` attribute. This tool fades out the
+    points that are not part of the selection (i.e., reduces their alpha
+    values). If your collection has alpha < 1, this tool will permanently
+    alter the alpha values.
+
+    Note that this tool selects collection objects based on their *origins*
+    (i.e., `offsets`).
+
+    Parameters
+    ----------
+    ax : `~matplotlib.axes.Axes`
+        Axes to interact with.
+    collection : `matplotlib.collections.Collection` subclass
+        Collection you want to select from.
+    alpha_other : 0 <= float <= 1
+        To highlight a selection, this tool sets all selected points to an
+        alpha value of 1 and non-selected points to *alpha_other*.
+    """
+
+    def __init__(self, ax, collection, alpha_other=0.3):
+        self.canvas = ax.figure.canvas
+        self.collection = collection
+        self.alpha_other = alpha_other
+
+        self.xys = collection.get_offsets()
+        self.Npts = len(self.xys)
+
+        # Ensure that we have separate colors for each object
+        self.fc = collection.get_facecolors()
+        if len(self.fc) == 0:
+            raise ValueError('Collection must have a facecolor')
+        elif len(self.fc) == 1:
+            self.fc = np.tile(self.fc, (self.Npts, 1))
+
+        self.poly = PolygonSelector(ax, self.onselect, draw_bounding_box=True)
+        self.ind = []
+
+    def onselect(self, verts):
+        path = Path(verts)
+        self.ind = np.nonzero(path.contains_points(self.xys))[0]
+        self.fc[:, -1] = self.alpha_other
+        self.fc[self.ind, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
+
+    def disconnect(self):
+        self.poly.disconnect_events()
+        self.fc[:, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
+
+
+class SelectFromCollection:
+    """
+    Select indices from a matplotlib collection using `LassoSelector`.
+
+    Selected indices are saved in the `ind` attribute. This tool fades out the
+    points that are not part of the selection (i.e., reduces their alpha
+    values). If your collection has alpha < 1, this tool will permanently
+    alter the alpha values.
+
+    Note that this tool selects collection objects based on their *origins*
+    (i.e., `offsets`).
+
+    Parameters
+    ----------
+    ax : `~matplotlib.axes.Axes`
+        Axes to interact with.
+    collection : `matplotlib.collections.Collection` subclass
+        Collection you want to select from.
+    alpha_other : 0 <= float <= 1
+        To highlight a selection, this tool sets all selected points to an
+        alpha value of 1 and non-selected points to *alpha_other*.
+    """
+
+    def __init__(self, ax, collection, alpha_other=0.3):
+        self.canvas = ax.figure.canvas
+        self.collection = collection
+        self.alpha_other = alpha_other
+
+        self.xys = collection.get_offsets()
+        self.Npts = len(self.xys)
+
+        # Ensure that we have separate colors for each object
+        self.fc = collection.get_facecolors()
+        if len(self.fc) == 0:
+            raise ValueError('Collection must have a facecolor')
+        elif len(self.fc) == 1:
+            self.fc = np.tile(self.fc, (self.Npts, 1))
+
+        self.lasso = LassoSelector(ax, onselect=self.onselect)
+        self.ind = []
+
+    def onselect(self, verts):
+        path = Path(verts)
+        self.ind = np.nonzero(path.contains_points(self.xys))[0]
+        self.fc[:, -1] = self.alpha_other
+        self.fc[self.ind, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
+
+    def disconnect(self):
+        self.lasso.disconnect_events()
+        self.fc[:, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
+
+fig, ax = plt.subplots(2, 2, figsize=(12, 12))
 plt.subplots_adjust(left=0.15, bottom=0.25)
 
 
@@ -141,7 +262,7 @@ def get_clustering_results(X_2d, **hdbscan_kwargs):
     return y_predict
 
 
-def run_clustering(image_id=image_id, threshold=threshold, rerun=False, action='label'):
+def run_clustering(image_id=image_id, threshold=threshold, rerun=False, rethreshold=False, action='label'):
     img_id = str(image_id).zfill(3)
     make_dir_if_missing('segmentation')
     make_dir_if_missing('segmentation/edges')
@@ -165,7 +286,11 @@ def run_clustering(image_id=image_id, threshold=threshold, rerun=False, action='
         with open(f'segmentation/edges/{img_id}', 'rb') as fp:
             img_2 = pickle.load(fp)
 
-    if rerun or not os.path.exists(f'segmentation/clusters/{img_id}'):
+    if rerun or rethreshold or not os.path.exists(f'segmentation/clusters/{img_id}'):
+        img_3 = neighborhood_average(img_2)
+        for i in range(5):
+            img_3 = neighborhood_average(img_3)
+        img = img_3 / np.max(img_3)
         X_2d = build_features_matrix(img, img_1, threshold)
         y_predict = get_clustering_results(X_2d, **hdbscan_kwargs)
         img_cluster_raw = -2 * np.ones(img.shape)
@@ -188,15 +313,48 @@ def run_clustering(image_id=image_id, threshold=threshold, rerun=False, action='
     return img_1, img_2, img_cluster_enhanced
 
 
-def run_segmentation(img_1, img_cluster_enhanced):
-    img_seg = np.random.randint(0, 2, size=img_1.shape)
+def run_segmentation(image_id, img_1, img_cluster_enhanced):
+    img_id = str(image_id).zfill(3)
+    if not os.path.exists(f"segmentation/phases/{img_id}"):
+        img_seg = np.random.randint(0, 2, size=img_1.shape, dtype=np.uint8)
+        with open(f'segmentation/phases/{img_id}', 'wb') as fp:
+            pickle.dump(img_seg, fp)
+    else:
+        with open(f'segmentation/phases/{img_id}', 'rb') as fp:
+            img_seg = pickle.load(fp)
     return img_seg
+
+
+def line_picker(line, mouseevent):
+    """
+    Find the points within a certain distance from the mouseclick in
+    data coords and attach some extra attributes, pickx and picky
+    which are the data points that were picked.
+    """
+    if mouseevent.xdata is None:
+        return False, dict()
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+    maxd = 0.05
+    d = np.sqrt(
+        (xdata - mouseevent.xdata)**2 + (ydata - mouseevent.ydata)**2)
+
+    ind, = np.nonzero(d <= maxd)
+    if len(ind):
+        pickx = xdata[ind]
+        picky = ydata[ind]
+        props = dict(ind=ind, pickx=pickx, picky=picky)
+        return True, props
+    else:
+        return False, dict()
 
 
 def update_image_id(val):
     image_id = image_id_slider.val
     img_1, img_2, img_cluster_enhanced = run_clustering(image_id=image_id, threshold=threshold)
-    img_seg = run_segmentation(img_1, img_cluster_enhanced)
+    img_seg = run_segmentation(image_id, img_1, img_cluster_enhanced)
+
+
     ax[0, 0].imshow(img_1, cmap='gray')
     ax[0, 0].set_title('Original')
     ax[0, 0].invert_yaxis()
@@ -205,21 +363,25 @@ def update_image_id(val):
     ax[0, 1].set_xlim([0, 500])
     ax[0, 1].set_ylim([0, 500])
 
-    ax[1, 0].imshow(img_cluster_enhanced, cmap='magma')
-    ax[1, 0].set_title("Enhanced Clusters")
+    coords = np.asarray(np.where(img_cluster_enhanced > -1)).T
+    y = np.array([img_cluster_enhanced[ix, iy] for (ix, iy) in coords]).reshape(-1, 1)
+    X = np.hstack((coords, y))
+    ax[1, 0].scatter(X[:, 1], X[:, 0], cmap=X[:, 2])
+    ax[1, 0].set_title("Clusters")
     ax[1, 0].set_xlim([0, 500])
     ax[1, 0].set_ylim([0, 500])
 
     ax[1, 1].imshow(img_seg, cmap='gray')
     ax[1, 1].set_xlim([0, 500])
     ax[1, 1].set_ylim([0, 500])
-    ax[1, 1].set_title("Training Data")
+    ax[1, 1].set_title("Segmented")
 
 
 def update_threshold(val):
+    image_id = image_id_slider.val
     threshold = threshold_slider.val
-    img_1, img_2, img_cluster_enhanced = run_clustering(image_id=image_id, threshold=threshold)
-    img_seg = run_segmentation(img_1, img_cluster_enhanced)
+    img_1, img_2, img_cluster_enhanced = run_clustering(image_id=image_id, threshold=threshold, rethreshold=True)
+    img_seg = run_segmentation(image_id, img_1, img_cluster_enhanced)
 
     ax[0, 0].imshow(img_1, cmap='gray')
     ax[0, 0].set_title('Original')
@@ -229,30 +391,90 @@ def update_threshold(val):
     ax[0, 1].set_xlim([0, 500])
     ax[0, 1].set_ylim([0, 500])
 
-
-    ax[1, 0].imshow(img_cluster_enhanced, cmap='magma')
-    ax[1, 0].set_title("Enhanced Clusters")
+    coords = np.asarray(np.where(img_cluster_enhanced > -1)).T
+    y = np.array([img_cluster_enhanced[ix, iy] for (ix, iy) in coords]).reshape(-1, 1)
+    X = np.hstack((coords, y))
+    ax[1, 0].scatter(X[:, 1], X[:, 0], cmap=X[:, 2])
+    ax[1, 0].set_title("Clusters")
     ax[1, 0].set_xlim([0, 500])
     ax[1, 0].set_ylim([0, 500])
     ax[1, 1].imshow(img_seg, cmap='gray')
     ax[1, 1].set_xlim([0, 500])
     ax[1, 1].set_ylim([0, 500])
-    ax[1, 1].set_title("Training Data")
+    ax[1, 1].set_title("Segmented")
+
+
+def label_clusters(val):
+    image_id = image_id_slider.val
+    img_id = str(image_id).zfill(3)
+    selected_pts = np.array(selector.xys[selector.ind], dtype=int)
+    cluster_vals = list(np.unique([img_cluster_enhanced[iy, ix] for ix, iy in selected_pts]))
+    print(cluster_vals)
+    if len(cluster_vals) == 1:
+        coords = np.where(img_cluster_enhanced == cluster_vals[0])
+        img_seg[coords] = phase
+    with open(f'segmentation/phases/{img_id}', 'wb') as fp:
+        pickle.dump(img_seg, fp)
+    ax[1, 1].imshow(img_seg, cmap='gray')
+    ax[1, 1].set_xlim([0, 500])
+    ax[1, 1].set_ylim([0, 500])
+    ax[1, 1].set_title("Segmented")
+    fig.canvas.draw_idle()
     
+    return True
+
+
+def select_phase(val):
+    phase = phases[radio2.value_selected]
+
 
 def reset(event):
     image_id_slider.reset()
     threshold_slider.reset()
 
 
+def onpick(event):
+    print('onpick2 line:', event.pickx, event.picky)
 
-resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='0.975')
+
+def accept(event):
+        if event.key == "enter":
+            print("Selected points:")
+            print(selector.xys[selector.ind])
+            
+            image_id = image_id_slider.val
+            img_id = str(image_id).zfill(3)
+            selected_pts = np.array(selector.xys[selector.ind], dtype=int)
+            cluster_vals = list(np.unique([img_cluster_enhanced[iy, ix] for ix, iy in selected_pts]))
+            print(cluster_vals)
+            if len(cluster_vals) == 1:
+                coords = np.where(img_cluster_enhanced == int(cluster_vals[0]))
+                img_seg[coords] = phase
+            else:
+                for v in cluster_vals:
+                    coords = np.where(img_cluster_enhanced == int(v))
+                    img_seg[coords] = phase
+            with open(f'segmentation/phases/{img_id}', 'wb') as fp:
+                pickle.dump(img_seg, fp)
+            ax[1, 1].imshow(img_seg, cmap='brg')
+            ax[1, 1].set_xlim([0, 500])
+            ax[1, 1].set_ylim([0, 500])
+            ax[1, 1].set_title("Segmented")
+            fig.canvas.draw_idle()
+            # selector.disconnect()
+            # ax.set_title("")
+            # fig.canvas.draw()
+
+resetax = fig.add_axes([0.935, 0.035, 0.05, 0.025])
+image_id_ax = fig.add_axes([0.01, 0.6, 0.025, 0.35])
+threshold_ax = fig.add_axes([0.95, 0.6, 0.025, 0.35])
+button = Button(resetax, 'Reset', hovercolor='red')
 button.on_clicked(reset)
 
 image_id_slider = Slider(
-    ax=ax[2, 0],
+    ax=image_id_ax,
     label='Image',
+    orientation='vertical',
     valmin=0,
     valmax=202,
     valinit=image_id,
@@ -260,18 +482,27 @@ image_id_slider = Slider(
 )
 
 threshold_slider = Slider(
-    ax=ax[2, 1],
+    ax=threshold_ax,
     label='Threshold',
+    orientation='vertical',
     valmin=0,
     valmax=0.1,
     valinit=threshold,
     valstep=thresholds,
 )
+##
+axcolor = 'lightgoldenrodyellow'
+rax = fig.add_axes([0.475, 0.475, 0.05, 0.05], facecolor=axcolor)
+radio2 = RadioButtons(rax, ('Void', 'SE', 'AM'), active=2)
+
+radio2.on_clicked(select_phase)
+
+##
 image_id = image_id_slider.val
 threshold = threshold_slider.val
 
 img_1, img_2, img_cluster_enhanced = run_clustering()
-
+img_seg = run_segmentation(image_id, img_1, img_cluster_enhanced)
 ax[0, 0].imshow(img_1, cmap='gray')
 ax[0, 0].set_title('Original')
 ax[0, 0].invert_yaxis()
@@ -279,20 +510,28 @@ ax[0, 1].imshow(img_2, cmap='magma')
 ax[0, 1].set_title('Edges')
 ax[0, 1].set_xlim([0, 500])
 ax[0, 1].set_ylim([0, 500])
+ax[1, 0].set_box_aspect(1)
 
-
-ax[1, 0].imshow(img_cluster_enhanced, cmap='magma')
-ax[1, 0].set_title("Enhanced Clusters")
+coords = np.asarray(np.where(img_cluster_enhanced > -1)).T
+y = np.array([img_cluster_enhanced[ix, iy] for (ix, iy) in coords]).reshape(-1, 1)
+X = np.hstack((coords, y))
+pts = ax[1, 0].scatter(X[:, 1], X[:, 0], cmap=X[:, 2])
+ax[1, 0].set_title("Clusters")
 ax[1, 0].set_xlim([0, 500])
 ax[1, 0].set_ylim([0, 500])
-
+ax[1, 1].imshow(img_seg, cmap='brg')
 ax[1, 1].set_xlim([0, 500])
 ax[1, 1].set_ylim([0, 500])
-ax[1, 1].set_title("Training Data")
+ax[1, 1].set_title("Segmented")
 
+selector = SelectFromCollection(ax[1, 0], pts)
+
+# selected_pts = selector.xys[selector.ind]
+# selector.onselect(selected_pts)
+fig.canvas.mpl_connect("key_press_event", accept)
 image_id_slider.on_changed(update_image_id)
 threshold_slider.on_changed(update_threshold)
-
-fig.canvas.draw()
+# selector.disconnect()
+fig.canvas.draw_idle()
 plt.tight_layout()
 plt.show()
