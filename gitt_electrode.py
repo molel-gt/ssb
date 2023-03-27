21b31f40f616041c15390526a185fee2a05a7286
Leshinka Molel <emolel3@gatech.edu>
2023-03-27 16:25:23 -0400

    update

--- gitt_electrode.py
+++ gitt_electrode.py
@@ -10,55 +10,40 @@ import commons, geometry
 markers = commons.SurfaceMarkers()
 phases = commons.Phases()
 
-W = 20
 N_pieces = 1000
 dy = 20 / N_pieces
 
-La = 25  # anode
-Ls = 25  # separator
-Lc = 75  # cathode
-L = Ls + Lc
-eps_se = 0.3
-eps_am = 0.7
+L = 20
+W = 20
 
 # meshing
 resolution = 0.05
 L_coating = L - 5 * resolution
-
-
-se_points = [
-    (0, 0, 0),  # bottom left
-    (0, W, 0),  # top left
-]
-am_points = [
-    (L, W, 0), # top right
-]
-
-points1 = np.arange(eps_se, W, 1)
-points2 = np.arange(0, W, 1)
-serrated = sorted(list(points1) + list(points2) + [W], reverse=True)
 
 gmsh.initialize()
 gmsh.model.add("AM/SE")
 # gmsh.option.setNumber("Mesh.MeshSizeMax", resolution)
 
 points = []
-for i in range(0, 1000, 3):
+for i in range(0, 1000):
     if i % 2 == 0:
+        count = 0
         for p in [
-                (L_coating, i * dy, 0),
-                (L_coating, (i + 1) * dy, 0),
-                (0, (i + 1) * dy, 0),
-                (0, (i + 2) * dy, 0)
+                (L_coating, np.round(i * dy, 2), 0),
+                (L_coating, np.round((i + 1) * dy, 2), 0),
+                (0, np.round((i + 1) * dy, 2), 0),
+                (0, np.round((i + 2) * dy, 2), 0)
                 ]:
             tag = gmsh.model.occ.addPoint(*p, meshSize=resolution)
             points.append(tag)
+            print(p)
 
 points.append(gmsh.model.occ.addPoint(L_coating, W - dy, 0, meshSize=resolution))
 points.append(gmsh.model.occ.addPoint(L_coating, W, 0, meshSize=resolution))
 for p in [(L, W, 0), (L, 0, 0)]:
     tag = gmsh.model.occ.addPoint(*p)
     points.append(tag)
+    print(p)
 am_lines = []
 
 for i in range(-1, len(points) - 1):
