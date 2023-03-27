ec0d352c0d3c13a26319ddcc2b30e67cf5de1083
Leshinka Molel <emolel3@gatech.edu>
2023-03-27 19:16:10 -0400

    update

--- diffusivity.py
+++ diffusivity.py
@@ -26,7 +26,7 @@ D = 1e-10  # m^2/s
 F_c = 96485  # C/mol
 i0 = 100  # A/m^2
 dt = 1.0e-03
-t_iter = 100
+t_iter = 200
 theta = 0.5  # time stepping family, e.g. theta=1 -> backward Euler, theta=0.5 -> Crank-Nicholson
 c_init = 0.01
 R = 8.314
