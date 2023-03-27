6bb154927d8d8d2fa17ae293cf9f6e383f8c2f04
Leshinka Molel <emolel3@gatech.edu>
2023-03-27 14:16:34 -0400

    update

--- diffusivity.py
+++ diffusivity.py
@@ -19,22 +19,23 @@ import commons, configs, constants
 
 
 markers = commons.SurfaceMarkers()
+
 # model parameters
-kappa = 1e3 # S/m
-D = 1e-15  # m^2/s
+kappa = 1e-1 # S/m
+D = 1e-10  # m^2/s
 F_c = 96485  # C/mol
 i0 = 100  # A/m^2
-dt = 1.0e-03
+dt = 1.0e-02
+t_iter = 250
 theta = 0.5  # time stepping family, e.g. theta=1 -> backward Euler, theta=0.5 -> Crank-Nicholson
 c_init = 0.01
 R = 8.314
 T = 298
-# Lx = 20
+# Lx = 1
+# Ly = 5
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Estimates Effective Conductivity.')
-    # parser.add_argument('--grid_size', help='Lx-Ly-Lz', required=True)
-    # parser.add_argument('--data_dir', help='Directory with tria.xdmf and tetr.xmf mesh files. Output files potential.xdmf and current.xdmf will be saved here', required=True, type=str)
     parser.add_argument("--voltage", help="Potential to set at the left current collector. Right current collector is set to a potential of 0", nargs='?', const=1, default=1)
 
     args = parser.parse_args()
@@ -51,20 +52,13 @@ if __name__ == '__main__':
     tags = mesh.meshtags(domain, domain.topology.dim - 1, ft.indices, ft.values)
     V = dolfinx.fem.FunctionSpace(domain, ("Lagrange", 2))
 
-    # Define variational problem
-    u = ufl.TrialFunction(V)
-    v = ufl.TestFunction(V)
-    x = ufl.SpatialCoordinate(domain)
-    
     f = dolfinx.fem.Constant(domain, PETSc.ScalarType(0.0))
     g = dolfinx.fem.Constant(domain, PETSc.ScalarType(0.0))
     kappa = dolfinx.fem.Constant(domain, PETSc.ScalarType(constants.KAPPA0))
     ds = ufl.Measure("ds", domain=domain, subdomain_data=tags)
     dS = ufl.Measure("dS", domain=domain, subdomain_data=tags)
 
-    a = ufl.inner(kappa * ufl.grad(u), ufl.grad(v)) * ufl.dx
-    L = ufl.inner(f, v) * ufl.dx + ufl.inner(g, v) * ds
-
+    # x = ufl.SpatialCoordinate(domain)
     options = {
                "ksp_type": "gmres",
                "pc_type": "hypre",
@@ -79,18 +73,13 @@ if __name__ == '__main__':
     tags = mesh.meshtags(domain, domain.topology.dim - 1, ft.indices, ft.values)
 
     # Trial and test functions of the space `ME` are now defined:
-    q, v = ufl.TestFunctions(ME)
+    q = ufl.TestFunction(V)
 
-    u = fem.Function(ME)  # current solution
-    u0 = fem.Function(ME)  # solution from previous converged step
-
-    # Split mixed functions
-    c, mu = ufl.split(u)
-    c0, mu0 = ufl.split(u0)
+    u = fem.Function(V)  # current solution
+    u0 = fem.Function(V)  # solution from previous converged step
 
     # The initial conditions are interpolated into a finite element space
     def set_initial_bc(x):
-        print(x.shape)
         new_x = x.T
         values = np.ones(new_x.shape[0])
         for i in range(x.shape[1]):
@@ -107,50 +96,24 @@ if __name__ == '__main__':
             values[i] = val
         return values
 
-    mu = ufl.variable(mu)
-    flux = 0 * ( ufl.exp(0.5 * F_c * (mu - 0.0) / (R * T)) - ufl.exp(-0.5 * F_c * (mu - 0.0) / (R * T)))
-    j_n = -D * ufl.dot(ufl.grad(c), n)
-    flux = j_n
-    flux2 = -kappa * ufl.dot(ufl.grad(mu), n)
-
     f = dolfinx.fem.Constant(domain, PETSc.ScalarType(0.0))
     g = dolfinx.fem.Constant(domain, PETSc.ScalarType(0.0))
+    g_exch = dolfinx.fem.Constant(domain, PETSc.ScalarType(1e-3))
 
-    left_cc_facet = ft.find(markers.left_cc)
-    right_cc_facet = ft.find(markers.right_cc)
-
-    V0, dofs = ME.sub(0).collapse()
-    V1, dofs = ME.sub(1).collapse()
-    u_left = fem.Function(V1)
-    u_right = fem.Function(V1)
-
-    with u_left.vector.localForm() as u0_loc:
-        u0_loc.set(0.0)
-
-    with u_right.vector.localForm() as u0_loc:
-        u0_loc.set(3.7)
-
-    left_cc_dofs = dolfinx.fem.locate_dofs_topological(V1, 1, left_cc_facet)
-    right_cc_dofs = dolfinx.fem.locate_dofs_topological(V1, 1, right_cc_facet)
-    left_cc_bc = dolfinx.fem.dirichletbc(u_left, left_cc_dofs)
-    right_cc_bc = dolfinx.fem.dirichletbc(u_right, right_cc_dofs)
-
-    print(u.sub(0).x.array[:].shape)
-    u.sub(0).interpolate(lambda x: set_initial_bc(x))
+    u.interpolate(lambda x: set_initial_bc(x))
     u.x.scatter_forward()
 
-    # mu_mid = (1.0 - theta) * mu0 + theta * mu
-    # Weak statement of the equations
-    c = ufl.variable(c)
-    current = -D * ufl.diff(c, x)
-    F0 = ufl.inner(c, q) * ufl.dx - ufl.inner(c0, q) * ufl.dx + dt * ufl.inner(D * ufl.grad(c), ufl.grad(q)) * ufl.dx - dt * ufl.inner(f, q) * ufl.dx - dt * ufl.inner(g, q) * ds(markers.insulated)
-    F1 = ufl.inner(kappa * ufl.grad(mu), ufl.grad(v)) * ufl.dx - ufl.inner(f, v) * ufl.dx - ufl.inner(g, v) * ds(markers.insulated) - ufl.inner(flux, q) * ds(markers.left_cc)
-    F = F0 + F1
+    F = ufl.inner(u, q) * ufl.dx - ufl.inner(u0, q) * ufl.dx 
+    F += dt * ufl.inner(D * ufl.grad(u), ufl.grad(q)) * ufl.dx 
+    F -= dt * ufl.inner(f, q) * ufl.dx
+    F -= dt * ufl.inner(g, q) * ds(markers.insulated)
+    F -= dt * ufl.inner(g, q) * ds(markers.right_cc)
+    F -= g_exch * ufl.inner(u, q) * ds(markers.left_cc)
 
     problem = fem.petsc.NonlinearProblem(F, u)
     solver = nls.petsc.NewtonSolver(comm, problem)
     solver.convergence_criterion = "incremental"
-    solver.maximum_iterations = 250
+    solver.maximum_iterations = 50
     solver.rtol = 1e-12
     ksp = solver.krylov_solver
     opts = PETSc.Options()
@@ -161,24 +124,20 @@ if __name__ == '__main__':
     ksp.setFromOptions()
 
     # Output file
-    file = io.XDMFFile(comm, "demo_ch/output.xdmf", "w")
+    file = io.XDMFFile(comm, "concentration.xdmf", "w")
     file.write_mesh(domain)
 
     # Step in time
     t = 0.0
 
-    SIM_TIME = 12000 * dt
-
-    # Get the sub-space for c and the corresponding dofs in the mixed space
-    # vector
-    V0, dofs = ME.sub(0).collapse()
+    SIM_TIME = t_iter * dt
 
     # Create a VTK 'mesh' with 'nodes' at the function dofs
-    topology, cell_types, x = plot.create_vtk_mesh(V0)
+    topology, cell_types, x = plot.create_vtk_mesh(V)
     grid = pv.UnstructuredGrid(topology, cell_types, x)
 
     # Set output data
-    grid.point_data["c"] = u.x.array[dofs].real
+    grid.point_data["c"] = u.x.array
     grid.set_active_scalars("c")
 
     p = pvqt.BackgroundPlotter(title="concentration", auto_update=True)
@@ -186,7 +145,6 @@ if __name__ == '__main__':
     p.view_xy(True)
     p.add_text(f"time: {t}", font_size=12, name="timelabel")
 
-    c = u.sub(0)
     u0.x.array[:] = u.x.array
     solution = fem.petsc.create_vector(problem.L)
 
@@ -195,15 +153,15 @@ if __name__ == '__main__':
         r = solver.solve(u)
         print(f"Step {int(t/dt)}: num iterations: {r[0]}")
         u0.x.array[:] = u.x.array
-        file.write_function(c, t)
+        file.write_function(u, t)
 
         # Update the plot window
         p.add_text(f"time: {t:.2e}", font_size=12, name="timelabel")
-        grid.point_data["c"] = u.x.array[dofs].real
+        grid.point_data["c"] = u.x.array
         p.app.processEvents()
 
     file.close()
 
     # Update ghost entries and plot
     u.x.scatter_forward()
-    grid.point_data["c"] = u.x.array[dofs].real
+    grid.point_data["c"] = u.x.array
