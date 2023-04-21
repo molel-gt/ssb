9aa1db0624c1bc5cba22b5632c42e8ced53b2dd4
Leshinka Molel <emolel3@gatech.edu>
2023-04-21 12:51:26 -0400

    overall script

--- run-segmentation.py
+++ run-segmentation.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
+import os
 import time
 
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 import seaborn as sns
 
 import segmentation
 
+from PIL import Image 
+from scipy import stats
 from sklearn.metrics import confusion_matrix
 
 
-def resample(img_id):
-    pass
+def resample(img_id, output_dir):
+    phases = plt.imread(os.path.join("segmented", str(img_id).zfill(3) + '.tif'))
+    with open(os.path.join(output_dir, 'clusters', str(img_id).zfill(3)), 'rb') as fp:
+        clusters = pickle.load(fp)
+    new_img = phases.copy()
+    for v in np.unique(clusters):
+        if v < 0:
+            continue
+        coords = np.where(np.isclose(clusters, v))
+        p = phases[coords]
+        mode = stats.mode(p)
+        count = coords[0].shape[0]
+        print(v, mode.mode[0], mode.count[0]/count)#, np.mean(p), np.std(p))
+        if mode.count[0]/count > 0.7:
+            new_img[coords] = mode.mode[0]
+    img_2 = Image.fromarray(new_img)
+    img_2.save(f'segmented/resampled/{str(img_id).zfill(3)}.tif', format='TIFF')
 
 
 if __name__ == '__main__':
@@ -32,6 +51,7 @@ if __name__ == '__main__':
     # ob.create_output()
 
     # resample
+    resample(0, args.output_dir)
     
     stop = time.time()
     print("Took: %s minutes" % str(int((stop - start)/60)))
