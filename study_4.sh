095ea25922e4766034ceb3919e93cfb6a6eed757
Leshinka Molel <emolel3@gatech.edu>
2023-05-04 15:33:43 -0400

    add study 4

---
+++ study_4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.05
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.10
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.20
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.30
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.40
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.50
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.60
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.70
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.80
+python3 contact_loss.py --grid_info 26-26-26 --contact_map data/current_constriction/test11.tif --eps 0.85
