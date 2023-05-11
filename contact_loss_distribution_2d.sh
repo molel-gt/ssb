b0d5d4b0691a95cfda8ff15a65051cf84e8a1f3b
Leshinka Molel <emolel3@gatech.edu>
2023-05-11 11:01:38 -0400

    contact loss dist

--- contact_loss_distribution_2d.sh
+++ contact_loss_distribution_2d.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 python3 contact_loss_2d.py --eps 1 --Lx 10 --Ly 10 --w 0 --h 0 --pos 0 --n_pieces 1
+
+python3 current_constriction_2d.py --eps 1 --Lx 10 --Ly 10 --w 0 --h 0 --voltage 1 --pos 0 --n_pieces 1
+python3 current_constriction_2d.py --eps 1 --Lx 10 --Ly 10 --w 0 --h 0 --voltage 2 --pos 0 --n_pieces 1
