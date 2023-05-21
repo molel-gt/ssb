867c5c36926c3a88e4c95d9e11053ca5e2d418c7
Leshinka Molel <emolel3@gatech.edu>
2023-05-21 10:27:03 -0400

    update

--- study_5.sh
+++ study_5.sh
@@ -4,7 +4,7 @@ for eps in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9;
 do
   for pos in 0.1 0.5 0.9;
   do
-    python3 contact_loss_2d.py --eps $eps --Lx 1 --Ly 1 --w 0.1 --h 0.1 --pos $pos --n_pieces 3 && python3 current_constriction_2d.py --eps $eps --Lx 1 --Ly 1 --w 0.1 --h 0.1 --pos $pos --n_pieces 3
+    python3 contact_loss_2d.py --eps $eps --Lx 1 --Ly 1 --w 0.1 --h 0.1 --pos $pos --n_pieces 3 && python3 current_constriction_2d.py --eps $eps --Lx 1 --Ly 1 --w 0.1 --h 0.1 --pos $pos --n_pieces 3 &
   done
 done
 
