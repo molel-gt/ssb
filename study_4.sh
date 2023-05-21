7633828487b4a91ce785f87bb912778d9021783a
Leshinka Molel <emolel3@gatech.edu>
2023-05-21 11:57:56 -0400

    update

--- study_4.sh
+++ study_4.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 echo 'Creating Geometries..'
-python3 study_4_geo.py --grid_info 26-26-7.25 --contact_map data/current_constriction/test11.tif --eps 0.10
-python3 study_4_geo.py --grid_info 26-26-7.25 --contact_map data/current_constriction/test11.tif --eps 0.20
-python3 study_4_geo.py --grid_info 26-26-7.25 --contact_map data/current_constriction/test11.tif --eps 0.30
-python3 study_4_geo.py --grid_info 26-26-7.25 --contact_map data/current_constriction/test11.tif --eps 0.40
-python3 study_4_geo.py --grid_info 26-26-7.25 --contact_map data/current_constriction/test11.tif --eps 0.50
-python3 study_4_geo.py --grid_info 26-26-7.25 --contact_map data/current_constriction/test11.tif --eps 0.60
-python3 study_4_geo.py --grid_info 26-26-7.25 --contact_map data/current_constriction/test11.tif --eps 0.70
-python3 study_4_geo.py --grid_info 26-26-7.25 --contact_map data/current_constriction/test11.tif --eps 0.75
-
-echo 'Running Models..'
-python3 study_4.py --grid_size 25-25-25 --data_dir mesh/study_4/26-26-7.25/0.1 --eps 0.10
-python3 study_4.py --grid_size 25-25-25 --data_dir mesh/study_4/26-26-7.25/0.2 --eps 0.20
-python3 study_4.py --grid_size 25-25-25 --data_dir mesh/study_4/26-26-7.25/0.3 --eps 0.30
-python3 study_4.py --grid_size 25-25-25 --data_dir mesh/study_4/26-26-7.25/0.4 --eps 0.40
-python3 study_4.py --grid_size 25-25-25 --data_dir mesh/study_4/26-26-7.25/0.5 --eps 0.50
-python3 study_4.py --grid_size 25-25-25 --data_dir mesh/study_4/26-26-7.25/0.6 --eps 0.60
-python3 study_4.py --grid_size 25-25-25 --data_dir mesh/study_4/26-26-7.25/0.7 --eps 0.70
-python3 study_4.py --grid_size 25-25-25 --data_dir mesh/study_4/26-26-7.25/0.75 --eps 0.75
+for size in 7.25 26 101;
+do
+  for eps in 0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75;
+  do
+    sbatch study_4.sh $size $eps
+  done
+done
