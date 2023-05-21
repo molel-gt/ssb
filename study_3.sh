5a564f5e26e7663e0d9cff8ab795e4d8a22ae977
Leshinka Molel <emolel3@gatech.edu>
2023-05-21 11:34:30 -0400

    update

--- study_3.sh
+++ study_3.sh
@@ -3,6 +3,6 @@ for relative_radius in 0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.
 do
   for Wa in 0.00001 0.0001 0.001 0.01 0.1 1 10 100 1000;
   do
-    python3 study_3_geo.py --relative_radius $relative_radius --outdir mesh/study_3/$Wa/$relative_radius && python3 study_3.py --outdir mesh/study_3/$Wa/$relative_radius --Wa $Wa
+    sbatch study_3.sbatch $relative_radius mesh/study_3/$Wa/$relative_radius
   done
 done
