f9b0444ae864e31b23f50a9c7a6d9c9011b889c5
Leshinka Molel <emolel3@gatech.edu>
2023-05-23 12:18:13 -0400

    update

--- study_5.sh
+++ study_5.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-for eps in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9;
+#for eps in 0.01 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9;
+for eps in 0.01 0.05 0.1 0.2 0.3;
 do
-  for pos in 0.1 0.5 0.9;
+  for pos in 0.1 0.9;
   do
-    sbatch study_5.sbatch $eps $pos
+#    python3 study_5_geo.py --eps $eps --Lx 1 --Ly 1 --w 0.1 --h 0.2 --pos $pos --n_pieces 2 &
+#    python3 study_5.py --eps $eps --Lx 1 --Ly 1 --w 0.1 --h 0.2 --pos $pos --n_pieces 2 &
+  sbatch study_5.sbatch $eps $pos
   done
 done
