e24be5939e75521ce4f2a52954270d3a55f75193
Leshinka Molel <emolel3@gatech.edu>
2023-05-26 16:59:43 -0400

    renv

--- study_4.sh
+++ study_4.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-source /storage/home/hcoda1/0/emolel3/.bashrc && conda activate fenicsx-env
+module load anaconda3
+conda activate fenicsx-env
+
 echo 'Creating Geometries..'
+cd /storage/coda1/p-tf74/0/shared/leshinka/ssb
+
 for Lz in 7.25 26;
 do
-#  for eps in 0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75;
-  for eps in 0.01 0.05 0.1 0.15; # 0.2 0.25 0.3;# 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75;
+for eps in 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75;
   do
-    python3 study_4_geo.py --eps $eps --grid_extents 26-26-$Lz &
+    sbatch study_4.sbatch $eps 26-26-$Lz
   done
 done
