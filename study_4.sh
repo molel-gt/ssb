3e68d6440789573dde0394007d3c1156b5343f59
Leshinka Molel <emolel3@gatech.edu>
2023-05-26 17:18:28 -0400

    Update for scheduling

---
+++ study_4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+module anaconda3
+conda activate fenicsx-env
+
+cd /storage/coda1/p-tf74/0/shared/leshinka/ssb
+
+for Lz in 7.25 26;
+  do
+  for eps in 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75;
+    do
+      sbatch study_4.sbatch $eps 26-26-$Lz
+    done
+ done
