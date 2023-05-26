ac927519254f93b6a31cc82398ac03c861a5ccd4
Leshinka Molel <emolel3@gatech.edu>
2023-05-26 19:06:47 -0400

    Update for scheduling

--- study_3.sh
+++ study_3.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-cd /storage/coda1/p-tf74/0/shared/leshinka/ && . ./spack/share/spack/setup-env.sh
-spack env activate fenicsx-env
-spack load gcc@10.1.0
-
+module load anaconda3
+conda activate fenicsx-env
+export PYTHONPATH=$PYTHONPATH:/storage/coda1/p-tf74/0/shared/leshinka/opt/
+export PATH=$PATH:/storage/coda1/p-tf74/0/shared/leshinka/opt/:/storage/home/hcoda1/0/emolel3/.local/
 for relative_radius in 0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9;
 do
   for Wa in 1000; #0.00001 0.0001 0.001 0.01 0.1 1 10 100 1000;
   do
     sbatch study_3.sbatch $relative_radius mesh/study_3/$Wa/$relative_radius
-    # python3 study_3_geo.py --relative_radius $relative_radius --outdir mesh/study_3/$Wa/$relative_radius &&
-#    python3 study_3.py --outdir mesh/study_3/$Wa/$relative_radius --Wa $Wa &
   done
 done
--- study_5.sh
+++ study_5.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 module load anaconda3
 conda activate fenicsx-env
+export PATH=/storage/coda1/p-tf74/0/shared/leshinka/opt/gmsh:$PATH
 
 for eps in 0.01 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9;
 do
