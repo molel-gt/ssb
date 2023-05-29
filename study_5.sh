11204d3c4251b880d5791093d79ee1453d1c3c31
Leshinka Molel <emolel3@gatech.edu>
2023-05-29 10:16:28 -0400

    Update for scheduling

--- study_5.sh
+++ study_5.sh
@@ -2,8 +2,8 @@
 module load anaconda3
 conda activate fenicsx-env
 export PATH=/storage/coda1/p-tf74/0/shared/leshinka/opt/gmsh:$PATH
-conda install --yes --file requirements.txt
-for eps in 0.01 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9;
+
+for eps in 0.01 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9;
 do
   for pos in 0.5 0.8;
   do
