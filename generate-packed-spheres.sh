e79fa311da7ee8f55b00950c7ddb761c1b27dd27
Leshinka Molel <emolel3@gatech.edu>
2023-06-11 19:44:07 -0400

    bug fix

--- generate-packed-spheres.sh
+++ generate-packed-spheres.sh
@@ -10,7 +10,7 @@ for packing_fraction in 0.01 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.75;
 do
   cd ~/spheres
   mkdir -p $1"/$packing_fraction"
-  sed -i "/double maxpf\ = */c\double maxpf = ${packing_fraction};"
+  sed -i "/double maxpf\ = */c\double maxpf = ${packing_fraction};" input
   ./spheres input
   cp centers.dat $1"/$packing_fraction"
   cd -
