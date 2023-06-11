6d9070163c1bd3c0b1a8b063997a32f65e563110
Leshinka Molel <emolel3@gatech.edu>
2023-06-11 14:59:59 -0400

    generate packed spheres centers

---
+++ generate-packed-spheres.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "requires output directory as parameter"
+    exit 1
+fi
+cd ~/spheres
+
+for packing_fraction in 0.01 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.75;
+do
+  mkdir -p $1
+  sed -i "/double maxpf\ = */c\double maxpf = ${packing_fraction};"
+  ./spheres input
+  cp centers.dat $1
+done
