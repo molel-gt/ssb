2648d3ddee3f15d8fea22b423b6af88af1f1112b
Leshinka Molel <emolel3@gatech.edu>
2023-06-11 15:44:46 -0400

    bug fix

--- packed-spheres-model.sh
+++ packed-spheres-model.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "requires output directory as parameter"
-    exit 1
-fi
-
-
 for packing_fraction in 0.01 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.75;
 do
   sbatch packed-spheres-model.sbatch $packing_fraction
