b0789292456f4ffbc291550c2602689a869e99d5
Leshinka Molel <emolel3@gatech.edu>
2023-06-11 20:05:51 -0400

    bug fix

--- generate-packed-spheres.sh
+++ generate-packed-spheres.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 
-for packing_fraction in 0.01 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.75;
+for packing_fraction in 0.01 0.05 0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.75 0.8 0.9 0.95 0.99;
 do
   cd ~/spheres
   mkdir -p $1"/$packing_fraction"
