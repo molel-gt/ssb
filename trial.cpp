fd45ba3378e5506ba4b7e5cf076e4af209c71ae3
Leshinka Molel <emolel3@gatech.edu>
2023-06-13 13:33:52 -0400

    trial size

---
+++ trial.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    std::vector<int> v;
+    cout << v.max_size() << std::endl;
+    return 0;
+}
