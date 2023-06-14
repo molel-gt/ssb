3caeda037e4b4785df5f050fb4e21f1a9c70408c
Leshinka Molel <emolel3@gatech.edu>
2023-06-14 16:54:52 -0400

    geometry scheduling

---
+++ study_2_geo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+module load anaconda3
+conda activate fenicsx-env
+export PATH=/storage/coda1/p-tf74/0/shared/leshinka/opt/gmsh:$PATH
+
+for img_id in 1 6 11 16 22;
+do
+  sbatch study_2_geo.sbatch $img_id
+done
