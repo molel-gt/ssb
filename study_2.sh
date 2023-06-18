a0854237339015339920b18fc61b6c258966d0e9
Leshinka Molel <emolel3@gatech.edu>
2023-06-18 14:12:19 -0400

    update

--- study_2.sh
+++ study_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-module load anaconda3
-conda activate fenicsx-env
-export PATH=/storage/coda1/p-tf74/0/shared/leshinka/opt/gmsh:$PATH
+#module load anaconda3
+#conda activate fenicsx-env
+#export PATH=/storage/coda1/p-tf74/0/shared/leshinka/opt/gmsh:$PATH
 
 for img_id in 1 6 11 16 22;
 do
