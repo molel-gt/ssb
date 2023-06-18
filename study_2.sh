bbf52107d07f03bd532eebdd75a1d0f9f23f92e6
Leshinka Molel <emolel3@gatech.edu>
2023-06-18 15:14:01 -0400

    add new images

--- study_2.sh
+++ study_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for img_id in 1 6 11 16 22;
+for img_id in 1 6 7 10 11 13 16 22;
 do
   sbatch study_2.sbatch $img_id
 done
--- study_2_geo.sh
+++ study_2_geo.sh
@@ -3,7 +3,7 @@ module load anaconda3
 conda activate fenicsx-env
 export PATH=/storage/coda1/p-tf74/0/shared/leshinka/opt/gmsh:$PATH
 
-for img_id in 1 6 11 16 22;
+for img_id in 1 6 7 10 11 13 16 22;
 do
   sbatch study_2_geo.sbatch $img_id
 done
