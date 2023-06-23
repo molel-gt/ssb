6ae2ca3f179ae140c3e407f8ebd28b5851239d2a
Leshinka Molel <emolel3@gatech.edu>
2023-06-23 19:29:49 -0400

    update

--- clusters.sh
+++ clusters.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo systemctl restart slurmctld && sudo systemctl restart slurmd && sudo systemctl restart slurmdbd && sudo systemctl restart munge && sudo systemctl restart mariadb && sudo slurmdbd -Dvv
+
 sudo apt update
 
 sudo apt install -y  slurmctld slurmdb slurmdbb
@@ -21,6 +23,8 @@ sudo sacctmgr add user slurm Account=gts-tf74 Partitions=High
 sudo sacctmgr modify cluster localcluster set QOS=inferno
 
 # banking
+sudo apt-get install libswitch-perl
+
 cd ~/dev
 git clone https://github.com/jcftang/slurm-bank.git
 cd slurm-bank
@@ -30,3 +34,14 @@ sudo sbank project create -c localcluster -a gts-tf74
 sudo sbank deposit -c localcluster -a gts-tf74 -t 1000
 sbank balance statement -A
 sbank cluster cpuhrs
+
+sudo sacctmgr modify user slurm set GrpTRES=cpu=1000 GrpTRESRunMin=cpu=2000000
+sudo sacctmgr update qos inferno set priority=10 MaxTRESPerUser=cpu=400
+sudo sacctmgr -i modify user where name=slurm set DefaultQOS=inferno
+
+sudo sacctmgr show stats
+sudo sacctmgr show problem
+sudo sacctmgr show RunawayJobs
+
+sreport cluster UserUtilizationByAccount
+sreport cluster AccountUtilizationByUser
