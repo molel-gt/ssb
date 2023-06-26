f0d1bb2cfc13753ef17267a3ecc12a7d9691ee60
Leshinka Molel <emolel3@gatech.edu>
2023-06-26 13:48:24 -0400

    update

--- transport.py
+++ transport.py
@@ -161,7 +161,8 @@ if __name__ == '__main__':
     logger.debug(current_h.vector)
     logger.debug(current_h.x.array[2])
     logger.debug(current_h.x.array[2] < new_fun.x.array[2])
-    logger.debug(ufl.conditional(ufl.le(current_h.x, 5), 1, 0))
+    logger.debug(ufl.conditional(ufl.le(current_h, 5), 1, 0))
+    logger.debug(ufl.conditional(ufl.le(ufl.as_vector(current_h.x), 5), 1, 0))
     def new_func(v1, v2):
         return ufl.conditional(ufl.le(v1, v2), v1, v2)
     # new_express = fem.Expression(ufl.conditional(ufl.le(current_h.x.array, 5), 1, 0), W.element.interpolation_points())
