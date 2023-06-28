b37874fee8e597f6002880911f667a262853b6a2
Leshinka Molel <emolel3@gatech.edu>
2023-06-28 12:00:39 -0400

    args

--- study_2_geo.sh
+++ study_2_geo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "requires L_z"
+if [ "$#" -ne 2 ]; then
+    echo "usage: sh study_2_geo.sh L_z resolution"
     exit 1
 fi
 
 for img_id in 1 6 11 16 22;
 do
-  sbatch study_2_geo.sbatch $img_id $1
+  sbatch study_2_geo.sbatch $img_id $1 $2
 done
