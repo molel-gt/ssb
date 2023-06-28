cabcccee10135d1493cbc7b658dbd73f3b708268
Leshinka Molel <emolel3@gatech.edu>
2023-06-28 11:56:45 -0400

    pass Lz

--- study_2.sh
+++ study_2.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+if [ "$#" -ne 1 ]; then
+    echo "requires L_z"
+    exit 1
+fi
 for img_id in 1 6 11 16 22;
 do
-  sbatch study_2.sbatch $img_id
+  sbatch study_2.sbatch  $img_id $1
 done
--- study_2_geo.sh
+++ study_2_geo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "requires L_z"
+    exit 1
+fi
 
 for img_id in 1 6 11 16 22;
 do
-  sbatch study_2_geo.sbatch $img_id
+  sbatch study_2_geo.sbatch $img_id $1
 done
