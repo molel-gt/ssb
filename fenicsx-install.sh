c8f113f7424f6a543b8cf42862c39d646f1db275
Leshinka Molel <emolel3@gatech.edu>
2023-07-01 13:25:14 -0400

    add more packages

--- fenicsx-install.sh
+++ fenicsx-install.sh
@@ -6,11 +6,13 @@ git clone https://github.com/spack/spack.git
 
 spack env create fenicsx-env
 spack env activate fenicsx-env
-
+spack add hypre~int64
+spack add metis~int64
 spack add ninja@1.10.0
 spack add openmpi schedulers=slurm
-spack add petsc@3.17.5
+spack add petsc@3.17.5~int64
 spack add py-fenics-dolfinx cflags="-O3" fflags="-O3"
+spack add superlu_dist~int64
 spack add tetgen
 
 spack install
