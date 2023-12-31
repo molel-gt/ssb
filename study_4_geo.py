a772ac1125b04d6c4412c5aa612f27cc57fb49c8
Leshinka Molel <emolel3@gatech.edu>
2023-12-30 19:46:30 -0500

    save geometry and simulation metadata

--- study_4_geo.py
+++ study_4_geo.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 
 import os
-import pickle
+import json
 import subprocess
+
+from utc import datetime
 
 import argparse
 import gmsh
 import meshio
 import numpy as np
 
-import commons, configs, geometry, utils
+import commons, configs, geometry, utils, study_4_utils
 
-markers = commons.SurfaceMarkers()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Generate Volume with Contact Loss")
-    parser.add_argument("--grid_extents", help="Nx-Ny-Nz that defines the grid bounds", required=True)
-    # parser.add_argument("--contact_map", help="Image to generate contact map", required=True)
-    parser.add_argument("--phase", help="0 -> void, 1 -> SE, 2 -> AM", nargs='?', const=1, default=1)
+    parser.add_argument("--dimensions", help="integer representation of Lx-Ly-Lz of the grid", required=True)
     parser.add_argument("--eps", help="coverage of area at left cc", nargs='?', const=1, default=0.05)
     parser.add_argument('--scaling', help='scaling key in `configs.cfg` to ensure geometry in meters', nargs='?',
                         const=1, default='STUDY4_VOXEL_SCALING', type=str)
     parser.add_argument("--name_of_study", help="name_of_study", nargs='?', const=1, default="study_4")
+    parser.add_argument("--max_resolution", help="maximum resolution", nargs='?', const=1, default=1)
     args = parser.parse_args()
-    grid_extents = args.grid_extents
+    markers = commons.SurfaceMarkers()
+    dimensions = args.dimensions
+    max_resolution = args.max_resolution
     # contact_img_file = args.contact_map
     phase = args.phase
     eps = float(args.eps)
@@ -36,13 +38,10 @@ if __name__ == '__main__':
     dp = int(configs.get_configs()['GEOMETRY']['dp'])
     h = float(configs.get_configs()['GEOMETRY']['h'])
     origin_str = args.name_of_study
-    mesh_dir = os.path.join(configs.get_configs()['LOCAL_PATHS']['data_dir'], args.name_of_study, grid_extents, str(eps))
-    Nx, Ny, Nz = [float(v) for v in grid_extents.split("-")]
-    Lx = Nx - 1
-    Ly = Ny - 1
-    Lz = Nz - 1
+    mesh_dir = study_4_utils.meshfile_subfolder_path(eps=args.eps, name_of_study=args.name_of_study, dimensions=args.dimensions, max_resolution=args.max_resolution)
+    Lx, Ly, Lz = [int(v) for v in dimensions.split("-")]
     utils.make_dir_if_missing(mesh_dir)
-    contact_points_filepath = os.path.join(mesh_dir, "contact_points.pickle")
+    geometry_metafile = os.path.join(data_dir, "geometry.json")
     nodefile = os.path.join(mesh_dir, "porous.node")
     tetfile = os.path.join(mesh_dir, "porous.ele")
     facesfile = os.path.join(mesh_dir, "porous.face")
@@ -56,21 +55,17 @@ if __name__ == '__main__':
     tria_xdmf_unscaled = os.path.join(mesh_dir, "tria_unscaled.xdmf")
     tria_xmf_unscaled = os.path.join(mesh_dir, "tria_unscaled.xmf")
 
-    # img = io.imread(contact_img_file)
-    # contact_points = set()
-    # for idx in np.argwhere(np.isclose(img, phase)):
-    #     contact_points.add(tuple([int(v) for v in idx] + [0]))
-
-    # with open(contact_points_filepath, "wb") as fp:
-    #     pickle.dump(contact_points, fp, protocol=pickle.HIGHEST_PROTOCOL)
     r = 2 * Lx * (eps/np.pi) ** 0.5
     xc, yc = 0.5 * Lx, 0.5 * Ly
-    res0 = subprocess.check_call(f'cp contact-loss.geo {mesh_dir}', shell=True)
-    res1 = subprocess.check_call(f'sed -i "/eps\ = */c\eps = {eps};" {mesh_dir}/contact-loss.geo', shell=True)
-    res2 = subprocess.check_call(f'sed -i "/Lx\ = */c\Lx = {Lx};" {mesh_dir}/contact-loss.geo', shell=True)
-    res3 = subprocess.check_call(f'sed -i "/Ly\ = */c\Ly = {Ly};" {mesh_dir}/contact-loss.geo', shell=True)
-    res4 = subprocess.check_call(f'sed -i "/Lz\ = */c\Lz = {Lz};" {mesh_dir}/contact-loss.geo', shell=True)
+    _ = subprocess.check_call(f'cp contact-loss.geo {mesh_dir}', shell=True)
+    _ = subprocess.check_call(f'sed -i "/eps\ = */c\eps = {eps};" {mesh_dir}/contact-loss.geo', shell=True)
+    _ = subprocess.check_call(f'sed -i "/Lx\ = */c\Lx = {Lx};" {mesh_dir}/contact-loss.geo', shell=True)
+    _ = subprocess.check_call(f'sed -i "/Ly\ = */c\Ly = {Ly};" {mesh_dir}/contact-loss.geo', shell=True)
+    _ = subprocess.check_call(f'sed -i "/Lz\ = */c\Lz = {Lz};" {mesh_dir}/contact-loss.geo', shell=True)
+    _ = subprocess.check_call(f'sed -i "/Lz\ = */c\Lz = {Lz};" {mesh_dir}/contact-loss.geo', shell=True)
+    _ = subprocess.check_call(f'sed -i "/max_resolution\ = */c\max_resolution = {max_resolution};" {mesh_dir}/contact-loss.geo', shell=True)
     res = subprocess.check_call(f"gmsh -3 {mesh_dir}/contact-loss.geo -o {tetr_mshfile}", shell=True)
+
     tet_msh = meshio.read(tetr_mshfile)
     tetr_mesh_unscaled = geometry.create_mesh(tet_msh, "tetra")
     tetr_mesh_unscaled.write(tetr_xdmf_unscaled)
@@ -81,4 +76,11 @@ if __name__ == '__main__':
     tria_mesh_unscaled.write(tria_xdmf_unscaled)
     tria_mesh_scaled = geometry.scale_mesh(tria_mesh_unscaled, "triangle", scale_factor=scale_factor)
     tria_mesh_scaled.write(tria_xdmf_scaled)
+    geometry_metadata = {
+        "max_resolution": args.max_resolution,
+        "dimensions": args.dimensions,
+        "scaling": args.scaling,
+    }
+    with open(geometry_metafile, "w", encoding='utf-8') as f:
+        json.dump(geometry_metadata, f, ensure_ascii=False, indent=4)
     print(f"Wrote meshfile '{tria_xdmf_scaled}'")
