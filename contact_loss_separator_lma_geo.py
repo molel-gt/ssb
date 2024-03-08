0b6c0998d50a96a6e174ade289dd2c87fdbc4ce7
Leshinka Molel <emolel3@gatech.edu>
2024-03-08 12:59:48 -0500

    update

--- contact_loss_separator_lma_geo.py
+++ contact_loss_separator_lma_geo.py
@@ -282,8 +282,8 @@ if __name__ == '__main__':
     gmsh.model.mesh.field.setNumber(2, "IField", 1)
     gmsh.model.mesh.field.setNumber(2, "LcMin", 0.1 * args.resolution * scale_x)
     gmsh.model.mesh.field.setNumber(2, "LcMax", args.resolution * scale_x)
-    gmsh.model.mesh.field.setNumber(2, "DistMin", 0.5 * scale_x)
-    gmsh.model.mesh.field.setNumber(2, "DistMax", 1 * scale_x)
+    gmsh.model.mesh.field.setNumber(2, "DistMin", 1 * scale_x)
+    gmsh.model.mesh.field.setNumber(2, "DistMax", 2 * scale_x)
 
     gmsh.model.mesh.field.add("Max", 5)
     gmsh.model.mesh.field.setNumbers(5, "FieldsList", [2])
