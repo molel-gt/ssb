e54c916836feaa710992cbfd964364b84e50fe43
Leshinka Molel <emolel3@gatech.edu>
2024-03-28 12:43:15 -0400

    update

--- lmb-3d-cc.sh
+++ lmb-3d-cc.sh
@@ -4,5 +4,5 @@ rm -r ~/.cache/fenics/
 
 python3 lmb_3d_cc_2d_geo.py --name_of_study lithium_metal_3d_cc_2d  --resolution 0.2
 
-python3 lmb_3d_cc_new.py --dimensions 150-40-0 --mesh_folder output/subdomains_dg --Wa 1e3
-python3 lmb_3d_cc_new.py --dimensions 150-40-0 --mesh_folder output/lithium_metal_3d_cc_2d/150-40-0/20-55-20/2e-07/ --Wa 1e3
+mpiexec python3 lmb_3d_cc_new.py --dimensions 150-40-0 --mesh_folder output/subdomains_dg --Wa 1e3
+mpiexec python3 lmb_3d_cc_new.py --dimensions 150-40-0 --mesh_folder output/lithium_metal_3d_cc_2d/150-40-0/20-55-20/2e-07/ --Wa 1e3
