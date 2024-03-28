dc68998da2bd8e33ff5e26545a4149d6addb7fae
Leshinka Molel <emolel3@gatech.edu>
2024-03-28 12:42:06 -0400

    resolution

--- lmb_3d_cc_2d_geo.py
+++ lmb_3d_cc_2d_geo.py
@@ -164,10 +164,10 @@ if __name__ == '__main__':
 
     gmsh.model.mesh.field.add("Threshold", 2)
     gmsh.model.mesh.field.setNumber(2, "IField", 1)
-    gmsh.model.mesh.field.setNumber(2, "LcMin", 0.5 * resolution)
+    gmsh.model.mesh.field.setNumber(2, "LcMin", 0.25 * resolution)
     gmsh.model.mesh.field.setNumber(2, "LcMax", resolution)
     gmsh.model.mesh.field.setNumber(2, "DistMin", 0)
-    gmsh.model.mesh.field.setNumber(2, "DistMax", micron)
+    gmsh.model.mesh.field.setNumber(2, "DistMax", 2 * micron)
 
     gmsh.model.mesh.field.add("Max", 5)
     gmsh.model.mesh.field.setNumbers(5, "FieldsList", [2])
