400c19b2fd64be358f15677f0c4bce73d4a59515
Leshinka Molel <emolel3@gatech.edu>
2024-04-18 12:38:02 -0400

    nmc capacity

--- utils.py
+++ utils.py
@@ -9,3 +9,21 @@ def make_dir_if_missing(f_path):
 def print_dict(dict, padding=20):
     for k, v in dict.items():
         print(k.ljust(padding, ' '), ": ", str(v))
+
+
+def nmc_capacity(density, volume, Ni=0.6, Mn=0.2, Co=0.2):
+    """
+    density of nmc [kg/m3]
+
+    return: capacity in [mA.h]
+    """
+    faraday_constant = 96485  # A.s/mol
+    mwt_li = 6.94e-3  # kg/mol
+    mwt_ni = 58.693  # kg/mol
+    mwt_mn = 54.938  # kg/mol
+    mwt_co = 58.933  # kg/mol
+    mwt_o = 15.999  # kg/mol
+    mass_frac_li = mwt_li / (mwt_li + Ni * mwt_ni + Mn * mwt_mn + Co * mwt_co + 2 * mwt_o)
+    moles_li = density * volume * mass_frac_li / mwt_li
+
+    return moles_li * faraday_constant * (1 / 3600) * (1/1e-3)
