561da54deb42a5f70546e8ee2361b7b45a8190cc
Leshinka Molel <emolel3@gatech.edu>
2024-06-12 10:43:16 -0400

    update

--- laminate_composite_positive_electrode_geo.py
+++ laminate_composite_positive_electrode_geo.py
@@ -245,12 +245,12 @@ if __name__ == '__main__':
     # refinement
     if args.refine:
         gmsh.model.mesh.field.add("Distance", 1)
-        gmsh.model.mesh.field.setNumbers(1, "FacesList", left_surfs + interface)
+        gmsh.model.mesh.field.setNumbers(1, "FacesList", left_surfs + interface + right + insulated_se + insulated_am)
 
         gmsh.model.mesh.field.add("Threshold", 2)
         gmsh.model.mesh.field.setNumber(2, "IField", 1)
         gmsh.model.mesh.field.setNumber(2, "LcMin", resolution / 2)
-        gmsh.model.mesh.field.setNumber(2, "LcMax", resolution)
+        gmsh.model.mesh.field.setNumber(2, "LcMax", 5 * resolution)
         gmsh.model.mesh.field.setNumber(2, "DistMin", resolution)
         gmsh.model.mesh.field.setNumber(2, "DistMax", 2 * resolution)
 
