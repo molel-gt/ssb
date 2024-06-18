a0a9cff38e8bac6585aea90939042aa84b447527
Leshinka Molel <emolel3@gatech.edu>
2024-06-18 17:27:07 -0400

    update

--- demo_hdg.py
+++ demo_hdg.py
@@ -2,7 +2,8 @@
 # - Solve Poisson's equation using an HDG scheme.
 
 # +
-import importlib.util
+import argparse
+import os
 import sys
 
 import dolfinx
@@ -13,23 +14,12 @@ from dolfinx import fem, mesh
 from dolfinx.cpp.mesh import cell_num_entities
 
 from dolfinx.fem.petsc import assemble_matrix_block, assemble_vector_block
-from dolfinx.io import VTXWriter
+from dolfinx.io import gmshio, VTXWriter
 from mpi4py import MPI
 from petsc4py import PETSc
 from ufl import div, dot, grad, inner
 
-
-
-def par_print(comm, string):
-    if comm.rank == 0:
-        print(string)
-        sys.stdout.flush()
-
-
-def norm_L2(comm, v, measure=ufl.dx):
-    return np.sqrt(
-        comm.allreduce(fem.assemble_scalar(fem.form(ufl.inner(v, v) * measure)), op=MPI.SUM)
-    )
+import commons, utils
 
 
 def compute_cell_boundary_facets(domain):
@@ -50,164 +40,186 @@ def compute_cell_boundary_facets(domain):
     return np.vstack((np.repeat(np.arange(n_c), n_f), np.tile(np.arange(n_f), n_c))).T.flatten()
 
 
-def u_e(x):
-    """Exact solution."""
-    u_e = 1
-    for i in range(tdim):
-        u_e *= ufl.sin(ufl.pi * x[i])
-    return u_e
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='3D Current Collector.')
+    # parser.add_argument("--name_of_study", help="name_of_study", nargs='?', const=1, default="lithium_metal_3d_cc_2d")
+    # parser.add_argument('--dimensions', help='integer representation of Lx-Ly-Lz of the grid', required=True)
+    parser.add_argument('--mesh_folder', help='parent folder containing mesh folder', required=True)
+    parser.add_argument("--voltage", help="applied voltage drop", nargs='?', const=1, default=1.0, type=float)
+    # parser.add_argument("--Wa_n", help="Wagna number for negative electrode: charge transfer resistance <over> ohmic resistance", nargs='?', const=1, default=1e-3, type=float)
+    # parser.add_argument("--Wa_p", help="Wagna number for positive electrode: charge transfer resistance <over> ohmic resistance", nargs='?', const=1, default=1e3, type=float)
+    # parser.add_argument("--kr", help="ratio of ionic to electronic conductivity", nargs='?', const=1, default=1, type=float)
+    # parser.add_argument("--gamma", help="interior penalty parameter", nargs='?', const=1, default=15, type=float)
+    # parser.add_argument("--atol", help="solver absolute tolerance", nargs='?', const=1, default=1e-12, type=float)
+    # parser.add_argument("--rtol", help="solver relative tolerance", nargs='?', const=1, default=1e-9, type=float)
+    # parser.add_argument('--scaling', help='scaling key in `configs.cfg` to ensure geometry in meters', nargs='?',
+    #                     const=1, default='MICRON_TO_METER', type=str)
 
+    args = parser.parse_args()
+    comm = MPI.COMM_WORLD
+    rank = comm.rank
+    dtype = PETSc.ScalarType
 
-def boundary(x):
-    """Boundary marker."""
-    lr = np.isclose(x[0], 0.0) | np.isclose(x[0], 1.0)
-    tb = np.isclose(x[1], 0.0) | np.isclose(x[1], 1.0)
-    lrtb = lr | tb
-    if tdim == 2:
-        return lrtb
-    else:
-        assert tdim == 3
-        fb = np.isclose(x[2], 0.0) | np.isclose(x[2], 1.0)
-        return lrtb | fb
+    workdir = os.path.join(args.mesh_folder, "results")
+    utils.make_dir_if_missing(workdir)
+    output_meshfile = os.path.join(args.mesh_folder, 'mesh.msh')
+    lines_h5file = os.path.join(args.mesh_folder, 'lines.h5')
+    potential_resultsfile = os.path.join(workdir, "potential.bp")
+    u_resultsfile = os.path.join(workdir, "u.bp")
+    ubar_resultsfile = os.path.join(workdir, "ubar.bp")
+    concentration_resultsfile = os.path.join(workdir, "concentration.bp")
+    current_resultsfile = os.path.join(workdir, "current.bp")
+    simulation_metafile = os.path.join(workdir, "simulation.json")
 
+    markers = commons.Markers()
 
-comm = MPI.COMM_WORLD
-rank = comm.rank
-dtype = PETSc.ScalarType
+    # ### Read input geometry
+    partitioner = mesh.create_cell_partitioner(mesh.GhostMode.shared_facet)
+    domain, ct, ft = gmshio.read_from_msh(output_meshfile, comm, partitioner=partitioner)
+    tdim = domain.topology.dim
+    fdim = tdim - 1
+    domain.topology.create_connectivity(tdim, fdim)
+    domain.topology.create_connectivity(tdim, tdim)
 
-# Number of elements in each direction
-n = 8
+    # tag internal facets as 0
+    ft_imap = domain.topology.index_map(fdim)
+    num_facets = ft_imap.size_local + ft_imap.num_ghosts
+    indices = np.arange(0, num_facets)
+    values = np.zeros(indices.shape, dtype=np.intc)
+    values[ft.indices] = ft.values
+    ft = mesh.meshtags(domain, fdim, indices, values)
+    ct = mesh.meshtags(domain, tdim, ct.indices, ct.values)
 
-# Create the mesh
-domain = mesh.create_unit_cube(comm, n, n, n, ghost_mode=mesh.GhostMode.none)
+    # Create the sub-mesh
+    # NOTE Despite all facets being present in the submesh, the entity map isn't
+    # necessarily the identity in parallel
+    facet_mesh, facet_mesh_to_mesh = mesh.create_submesh(domain, fdim, ft.indices)[:2]
 
-# We need to create a broken Lagrange space defined over the facets of the
-# mesh. To do so, we require a sub-mesh of the all facets. We begin by
-# creating a list of all of the facets in the mesh
-tdim = domain.topology.dim
-fdim = tdim - 1
-domain.topology.create_entities(fdim)
-facet_imap = domain.topology.index_map(fdim)
-num_facets = facet_imap.size_local + facet_imap.num_ghosts
-facets = np.arange(num_facets, dtype=np.int32)
+    # Define function spaces
+    k = 3  # Polynomial order
+    V = fem.functionspace(domain, ("Discontinuous Lagrange", k))
+    Vbar = fem.functionspace(facet_mesh, ("Discontinuous Lagrange", k))
 
-# Create the sub-mesh
-# NOTE Despite all facets being present in the submesh, the entity map isn't
-# necessarily the identity in parallel
-facet_mesh, facet_mesh_to_mesh = mesh.create_submesh(domain, fdim, facets)[:2]
+    # Trial and test functions
+    # Cell space
+    u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
+    # Facet space
+    ubar, vbar = ufl.TrialFunction(Vbar), ufl.TestFunction(Vbar)
 
-# Define function spaces
-k = 3  # Polynomial order
-V = fem.functionspace(domain, ("Discontinuous Lagrange", k))
-Vbar = fem.functionspace(facet_mesh, ("Discontinuous Lagrange", k))
+    # Define integration measures
+    # Cell
+    dx_c = ufl.Measure("dx", domain=domain)
+    # Cell boundaries
+    # We need to define an integration measure to integrate around the
+    # boundary of each cell. The integration entities can be computed
+    # using the following convenience function.
+    cell_boundary_facets = compute_cell_boundary_facets(domain)
+    cell_boundaries = 1  # A tag
+    # Create the measure
+    ds_c = ufl.Measure("ds", subdomain_data=[(cell_boundaries, cell_boundary_facets)], domain=domain)
+    # Create a cell integral measure over the facet mesh
+    dx_f = ufl.Measure("dx", domain=facet_mesh)
 
-# Trial and test functions
-# Cell space
-u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
-# Facet space
-ubar, vbar = ufl.TrialFunction(Vbar), ufl.TestFunction(Vbar)
+    # We write the mixed domain forms as integrals over domain. Hence, we must
+    # provide a map from facets in domain to cells in facet_mesh. This is the
+    # 'inverse' of facet_mesh_to_mesh, which we compute as follows:
+    mesh_to_facet_mesh = np.full(num_facets, -1)
+    mesh_to_facet_mesh[facet_mesh_to_mesh] = np.arange(len(facet_mesh_to_mesh))
+    entity_maps = {facet_mesh: mesh_to_facet_mesh}
 
-# Define integration measures
-# Cell
-dx_c = ufl.Measure("dx", domain=domain)
-# Cell boundaries
-# We need to define an integration measure to integrate around the
-# boundary of each cell. The integration entities can be computed
-# using the following convenience function.
-cell_boundary_facets = compute_cell_boundary_facets(domain)
-cell_boundaries = 1  # A tag
-# Create the measure
-ds_c = ufl.Measure("ds", subdomain_data=[(cell_boundaries, cell_boundary_facets)], domain=domain)
-# Create a cell integral measure over the facet mesh
-dx_f = ufl.Measure("dx", domain=facet_mesh)
+    # Define forms
+    h = ufl.CellDiameter(domain)
+    n = ufl.FacetNormal(domain)
+    gamma = 16.0 * k**2 / h  # Scaled penalty parameter
 
-# We write the mixed domain forms as integrals over domain. Hence, we must
-# provide a map from facets in domain to cells in facet_mesh. This is the
-# 'inverse' of facet_mesh_to_mesh, which we compute as follows:
-mesh_to_facet_mesh = np.full(num_facets, -1)
-mesh_to_facet_mesh[facet_mesh_to_mesh] = np.arange(len(facet_mesh_to_mesh))
-entity_maps = {facet_mesh: mesh_to_facet_mesh}
-
-# Define forms
-h = ufl.CellDiameter(domain)
-n = ufl.FacetNormal(domain)
-gamma = 16.0 * k**2 / h  # Scaled penalty parameter
-
-x = ufl.SpatialCoordinate(domain)
-c = 1.0 + 0.1 * ufl.sin(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1])
-a_00 = fem.form(
-    inner(c * grad(u), grad(v)) * dx_c
-    - (
-        inner(c * u, dot(grad(v), n)) * ds_c(cell_boundaries)
-        + inner(dot(grad(u), n), c * v) * ds_c(cell_boundaries)
+    x = ufl.SpatialCoordinate(domain)
+    c = 1.0 # lambda x: 1.0 + x[0] - x[0]  # 1.0 + 0.1 * ufl.sin(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1])
+    a_00 = fem.form(
+        inner(c * grad(u), grad(v)) * dx_c
+        - (
+            inner(c * u, dot(grad(v), n)) * ds_c(cell_boundaries)
+            + inner(dot(grad(u), n), c * v) * ds_c(cell_boundaries)
+        )
+        + gamma * inner(c * u, v) * ds_c(cell_boundaries)
     )
-    + gamma * inner(c * u, v) * ds_c(cell_boundaries)
-)
-a_10 = fem.form(
-    inner(dot(grad(u), n) - gamma * u, c * vbar) * ds_c(cell_boundaries), entity_maps=entity_maps
-)
-a_01 = fem.form(
-    inner(c * ubar, dot(grad(v), n) - gamma * v) * ds_c(cell_boundaries), entity_maps=entity_maps
-)
-a_11 = fem.form(gamma * inner(c * ubar, vbar) * ds_c(cell_boundaries), entity_maps=entity_maps)
+    a_10 = fem.form(
+        inner(dot(grad(u), n) - gamma * u, c * vbar) * ds_c(cell_boundaries), entity_maps=entity_maps
+    )
+    a_01 = fem.form(
+        inner(c * ubar, dot(grad(v), n) - gamma * v) * ds_c(cell_boundaries), entity_maps=entity_maps
+    )
+    a_11 = fem.form(gamma * inner(c * ubar, vbar) * ds_c(cell_boundaries), entity_maps=entity_maps)
 
-# Manufacture a source term
-f = -div(c * grad(u_e(x)))
+    # Manufacture a source term
+    f = fem.Constant(domain, dtype(0.0))  # -div(c * grad(u_e(x)))
 
-L_0 = fem.form(inner(f, v) * dx_c)
-L_1 = fem.form(inner(fem.Constant(facet_mesh, dtype(0.0)), vbar) * dx_f)
+    L_0 = fem.form(inner(f, v) * dx_c)
+    L_1 = fem.form(inner(fem.Constant(facet_mesh, dtype(0.0)), vbar) * dx_f)
 
-# Define block structure
-a = [[a_00, a_01], [a_10, a_11]]
-L = [L_0, L_1]
+    # Define block structure
+    a = [[a_00, a_01], [a_10, a_11]]
+    L = [L_0, L_1]
 
-# Apply Dirichlet boundary conditions
-# We begin by locating the boundary facets of domain
-domain_boundary_facets = mesh.locate_entities_boundary(domain, fdim, boundary)
-# Since the boundary condition is enforced in the facet space, we must
-# use the mesh_to_facet_mesh map to get the corresponding facets in
-# facet_mesh
-facet_mesh_boundary_facets = mesh_to_facet_mesh[domain_boundary_facets]
-# Get the dofs and apply the bondary condition
-facet_mesh.topology.create_connectivity(fdim, fdim)
-dofs = fem.locate_dofs_topological(Vbar, fdim, facet_mesh_boundary_facets)
-bc = fem.dirichletbc(dtype(0.0), dofs, Vbar)
+    # Apply Dirichlet boundary conditions
+    # We begin by locating the boundary facets of domain
+    left_boundary = ft.find(markers.left)
+    right_boundary = ft.find(markers.right)
 
-# Assemble the matrix and vector
-A = assemble_matrix_block(a, bcs=[bc])
-A.assemble()
-b = assemble_vector_block(L, a, bcs=[bc])
+    # left_domain_boundary_facets = mesh.locate_entities_boundary(domain, fdim, left_boundary)
+    # left_domain_boundary_facets = mesh.locate_entities_boundary(domain, fdim, right_boundary)
 
-# Setup the solver
-ksp = PETSc.KSP().create(domain.comm)
-ksp.setOperators(A)
-ksp.setType("preonly")
-ksp.getPC().setType("lu")
-ksp.getPC().setFactorSolverType("superlu_dist")
+    # Since the boundary condition is enforced in the facet space, we must
+    # use the mesh_to_facet_mesh map to get the corresponding facets in
+    # facet_mesh
+    left_facet_mesh_boundary_facets = mesh_to_facet_mesh[left_boundary]
+    right_facet_mesh_boundary_facets = mesh_to_facet_mesh[right_boundary]
+    # Get the dofs and apply the bondary condition
+    facet_mesh.topology.create_connectivity(fdim, fdim)
+    left_dofs = fem.locate_dofs_topological(Vbar, fdim, left_facet_mesh_boundary_facets)
+    right_dofs = fem.locate_dofs_topological(Vbar, fdim, right_facet_mesh_boundary_facets)
+    left_bc = fem.dirichletbc(dtype(0.0), left_dofs, Vbar)
+    right_bc = fem.dirichletbc(dtype(args.voltage), right_dofs, Vbar)
 
-# Compute solution
-x = A.createVecRight()
-ksp.solve(b, x)
+    # Assemble the matrix and vector
+    A = assemble_matrix_block(a, bcs=[left_bc, right_bc])
+    A.assemble()
+    b = assemble_vector_block(L, a, bcs=[left_bc, right_bc])
 
-# Create functions for the solution and update values
-u, ubar = fem.Function(V), fem.Function(Vbar)
-offset = V.dofmap.index_map.size_local * V.dofmap.index_map_bs
-u.x.array[:offset] = x.array_r[:offset]
-ubar.x.array[: (len(x.array_r) - offset)] = x.array_r[offset:]
-u.x.scatter_forward()
-ubar.x.scatter_forward()
+    # Setup the solver
+    ksp = PETSc.KSP().create(domain.comm)
+    ksp.setOperators(A)
+    ksp.setType("preonly")
+    ksp.getPC().setType("lu")
+    ksp.getPC().setFactorSolverType("superlu_dist")
 
-# Write to file
-with VTXWriter(domain.comm, "u.bp", u, "bp4") as f:
-    f.write(0.0)
-with VTXWriter(domain.comm, "ubar.bp", ubar, "bp4") as f:
-    f.write(0.0)
+    # Compute solution
+    x = A.createVecRight()
+    ksp.solve(b, x)
 
+    # Create functions for the solution and update values
+    u, ubar = fem.Function(V), fem.Function(Vbar)
+    offset = V.dofmap.index_map.size_local * V.dofmap.index_map_bs
+    u.x.array[:offset] = x.array_r[:offset]
+    ubar.x.array[: (len(x.array_r) - offset)] = x.array_r[offset:]
+    u.x.scatter_forward()
+    ubar.x.scatter_forward()
 
-# Compute errors
-x = ufl.SpatialCoordinate(domain)
-e_u = norm_L2(domain.comm, u - u_e(x))
-x_bar = ufl.SpatialCoordinate(facet_mesh)
-e_ubar = norm_L2(domain.comm, ubar - u_e(x_bar))
-par_print(comm, f"e_u = {e_u}")
-par_print(comm, f"e_ubar = {e_ubar}")
+    # Write to file
+    with VTXWriter(domain.comm, u_resultsfile, u, "bp5") as f:
+        f.write(0.0)
+
+    with VTXWriter(domain.comm, ubar_resultsfile, ubar, "bp5") as f:
+        f.write(0.0)
+
+    W_DG = fem.functionspace(domain, ('DG', 1))
+    u_dg = fem.Function(W_DG)
+    u_dg.interpolate(u)
+    W_CG = fem.functionspace(domain, ('CG', 1, (3,)))
+    current_cg = fem.Function(W_CG)
+    current_expr = fem.Expression(-grad(u_dg), W_CG.element.interpolation_points())
+    current_cg.interpolate(current_expr)
+    with VTXWriter(domain.comm, potential_resultsfile, u_dg, "bp5") as f:
+        f.write(0.0)
+
+    with VTXWriter(domain.comm, current_resultsfile, current_cg, "bp5") as f:
+        f.write(0.0)
