3f3fc672db54c432fdec234ab91e0d2974b26e09
Leshinka Molel <emolel3@gatech.edu>
2024-06-21 02:10:15 -0400

    use newton solver

--- tertiary_current_hdg.py
+++ tertiary_current_hdg.py
@@ -28,6 +28,118 @@ R = 8.3145
 T = 298
 
 
+class NewtonSolver:
+    max_iterations: int
+    bcs: list[fem.DirichletBC]
+    A: PETSc.Mat
+    b: PETSc.Vec
+    J: fem.Form
+    b: fem.Form
+    dx: PETSc.Vec
+
+    def __init__(
+        self,
+        F: list[fem.form],
+        J: list[list[fem.form]],
+        w: list[fem.Function],
+        bcs: list[fem.DirichletBC] | None = None,
+        max_iterations: int = 5,
+        petsc_options: dict[str, str | float | int | None] = None,
+        problem_prefix="newton",
+    ):
+        self.max_iterations = max_iterations
+        self.bcs = [] if bcs is None else bcs
+        self.b = fem.petsc.create_vector_block(F)
+        self.F = F
+        self.J = J
+        self.A = fem.petsc.create_matrix_block(J)
+        self.dx = self.A.createVecLeft()
+        self.w = w
+        self.x = fem.petsc.create_vector_block(F)
+
+        # Set PETSc options
+        opts = PETSc.Options()
+        if petsc_options is not None:
+            for k, v in petsc_options.items():
+                opts[k] = v
+
+        # Define KSP solver
+        self._solver = PETSc.KSP().create(self.b.getComm().tompi4py())
+        self._solver.setOperators(self.A)
+        self._solver.setFromOptions()
+
+        # Set matrix and vector PETSc options
+        self.A.setFromOptions()
+        self.b.setFromOptions()
+
+    def solve(self, tol=1e-6, beta=1.0):
+        i = 0
+
+        while i < self.max_iterations:
+            dolfinx.cpp.la.petsc.scatter_local_vectors(
+                self.x,
+                [si.x.petsc_vec.array_r for si in self.w],
+                [
+                    (
+                        si.function_space.dofmap.index_map,
+                        si.function_space.dofmap.index_map_bs,
+                    )
+                    for si in self.w
+                ],
+            )
+            self.x.ghostUpdate(
+                addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD
+            )
+
+            # Assemble F(u_{i-1}) - J(u_D - u_{i-1}) and set du|_bc= u_D - u_{i-1}
+            with self.b.localForm() as b_local:
+                b_local.set(0.0)
+            fem.petsc.assemble_vector_block(
+                self.b, self.F, self.J, bcs=self.bcs, x0=self.x, scale=-1.0
+            )
+            self.b.ghostUpdate(
+                PETSc.InsertMode.INSERT_VALUES, PETSc.ScatterMode.FORWARD
+            )
+
+
+            # Assemble Jacobian
+            self.A.zeroEntries()
+            fem.petsc.assemble_matrix_block(self.A, self.J, bcs=self.bcs)
+            self.A.assemble()
+
+            self._solver.solve(self.b, self.dx)
+            # self._solver.view()
+            assert (
+                self._solver.getConvergedReason() > 0
+            ), "Linear solver did not converge"
+            offset_start = 0
+            for s in self.w:
+                num_sub_dofs = (
+                    s.function_space.dofmap.index_map.size_local
+                    * s.function_space.dofmap.index_map_bs
+                )
+                s.x.petsc_vec.array_w[:num_sub_dofs] -= (
+                    beta * self.dx.array_r[offset_start : offset_start + num_sub_dofs]
+                )
+                s.x.petsc_vec.ghostUpdate(
+                    addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD
+                )
+                offset_start += num_sub_dofs
+            # Compute norm of update
+
+            correction_norm = self.dx.norm(0)
+            print(f"Iteration {i}: Correction norm {correction_norm}")
+            if correction_norm < tol:
+                break
+            i += 1
+
+    def __del__(self):
+        self.A.destroy()
+        self.b.destroy()
+        self.dx.destroy()
+        self._solver.destroy()
+        self.x.destroy()
+
 def compute_cell_boundary_facets_new(domain, ct, marker):
     """Compute the integration entities for integrals around the
     boundaries of all cells in domain.
@@ -146,11 +258,11 @@ if __name__ == '__main__':
 
     # Trial and test functions
     # Cell space
-    u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
-    c, q = ufl.TrialFunction(V), ufl.TestFunction(V)
+    u, v = fem.Function(V), ufl.TestFunction(V)
+    # c, q = fem.Function(V), ufl.TestFunction(V)
     # Facet space
-    ubar, vbar = ufl.TrialFunction(Vbar), ufl.TestFunction(Vbar)
-    cbar, qbar = ufl.TrialFunction(Vbar), ufl.TestFunction(Vbar)
+    ubar, vbar = fem.Function(Vbar), ufl.TestFunction(Vbar)
+    # cbar, qbar = ufl.TrialFunction(Vbar), ufl.TestFunction(Vbar)
 
     # Define integration measures
     # Cell
@@ -170,7 +282,6 @@ if __name__ == '__main__':
     ds_c = ufl.Measure("ds", subdomain_data=[(phase1, phase1_facets), (phase2, phase2_facets)], domain=domain)
     dS = ufl.Measure("dS", domain=domain, subdomain_data=ft)
     ds = ufl.Measure("ds", domain=domain, subdomain_data=ft)
-    # ds_c = ufl.Measure("ds", subdomain_data=[(cell_boundaries, cell_boundary_facets)], domain=domain)
     # Create a cell integral measure over the facet mesh
     dx_f = ufl.Measure("dx", domain=facet_mesh, subdomain_data=ft)
 
@@ -188,31 +299,12 @@ if __name__ == '__main__':
 
     x = ufl.SpatialCoordinate(domain)
 
-    a_00 = fem.form(
-        inner(kappa * grad(u), grad(v)) * dx_c
-        - (
-            inner(kappa * u, dot(grad(v), n)) * (ds_c(phase1) + ds_c(phase2))
-            + inner(dot(grad(u), n), kappa * v) * (ds_c(phase1) + ds_c(phase2))
-        )
-        + gamma * inner(kappa * u, v) * (ds_c(phase1) + ds_c(phase2))
-    )
-    a_10 = fem.form(
-        inner(dot(grad(u), n) - gamma * u, kappa * vbar) * (ds_c(phase1) + ds_c(phase2)), entity_maps=entity_maps
-    )
-    a_01 = fem.form(
-        inner(kappa * ubar, dot(grad(v), n) - gamma * v) * (ds_c(phase1) + ds_c(phase2)), entity_maps=entity_maps
-    )
-    a_11 = fem.form(gamma * inner(kappa * ubar, vbar) * (ds_c(phase1) + ds_c(phase2)), entity_maps=entity_maps)
-
-    # Manufacture a source term
-    f = fem.Constant(domain, dtype(0.0))
-
-    L_0 = fem.form(inner(f, v) * dx_c)
-    L_1 = fem.form(inner(fem.Constant(facet_mesh, dtype(0.0)), vbar) * dx_f)
-
+    # source term
+    # f = fem.Constant(domain, dtype(0.0))
+    # L_0 = fem.form(inner(f, v) * dx_c)
+    # L_1 = fem.form(inner(fem.Constant(facet_mesh, dtype(0.0)), vbar) * dx_f)
     # Define block structure
-    a = [[a_00, a_01], [a_10, a_11]]
-    L = [L_0, L_1]
+    # L = [L_0, L_1]
 
     # Apply Dirichlet boundary conditions
     # We begin by locating the boundary facets of domain
@@ -230,30 +322,49 @@ if __name__ == '__main__':
     right_dofs = fem.locate_dofs_topological(Vbar, fdim, right_facet_mesh_boundary_facets)
     left_bc = fem.dirichletbc(dtype(0.0), left_dofs, Vbar)
     right_bc = fem.dirichletbc(dtype(args.voltage), right_dofs, Vbar)
+    bcs = [left_bc, right_bc]
+    a00 = (inner(kappa * grad(u), grad(v)) * dx_c
+        - (
+            inner(kappa * u, dot(grad(v), n)) * (ds_c(phase1) + ds_c(phase2))
+            + inner(dot(grad(u), n), kappa * v) * (ds_c(phase1) + ds_c(phase2))
+        )
+        + gamma * inner(kappa * u, v) * (ds_c(phase1) + ds_c(phase2)))
 
-    # Assemble the matrix and vector
-    A = assemble_matrix_block(a, bcs=[left_bc, right_bc])
-    A.assemble()
-    b = assemble_vector_block(L, a, bcs=[left_bc, right_bc])
+    a10 = inner(dot(grad(u), n) - gamma * u, kappa * vbar) * (ds_c(phase1) + ds_c(phase2))
+    a01 = inner(kappa * ubar, dot(grad(v), n) - gamma * v) * (ds_c(phase1) + ds_c(phase2))
+    a11 = gamma * inner(kappa * ubar, vbar) * (ds_c(phase1) + ds_c(phase2))
 
-    # Setup the solver
-    ksp = PETSc.KSP().create(domain.comm)
-    ksp.setOperators(A)
-    ksp.setType("preonly")
-    ksp.getPC().setType("lu")
-    ksp.getPC().setFactorSolverType("superlu_dist")
+    F0 = a00 + a01
+    F1 = a11 + a10
 
-    # Compute solution
-    x = A.createVecRight()
-    ksp.solve(b, x)
+    jac00 = ufl.derivative(F0, u)
+    jac01 = ufl.derivative(F0, ubar)
+    jac10 = ufl.derivative(F1, u)
+    jac11 = ufl.derivative(F1, ubar)
 
-    # Create functions for the solution and update values
-    u, ubar = fem.Function(V), fem.Function(Vbar)
-    offset = V.dofmap.index_map.size_local * V.dofmap.index_map_bs
-    u.x.array[:offset] = x.array_r[:offset]
-    ubar.x.array[: (len(x.array_r) - offset)] = x.array_r[offset:]
-    u.x.scatter_forward()
-    ubar.x.scatter_forward()
+    J00 = fem.form(jac00, entity_maps=entity_maps)
+    J01 = fem.form(jac01, entity_maps=entity_maps)
+    J10 = fem.form(jac10, entity_maps=entity_maps)
+    J11 = fem.form(jac11, entity_maps=entity_maps)
+
+    J = [[J00, J01], [J10, J11]]
+    F = [
+        fem.form(F0, entity_maps=entity_maps),
+        fem.form(F1, entity_maps=entity_maps),
+        ]
+    solver = NewtonSolver(
+        F,
+        J,
+        [u, ubar],
+        bcs=bcs,
+        max_iterations=2,
+        petsc_options={
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "superlu_dist",
+        },
+        )
+    solver.solve(1e-5)
 
     # Write to file
     with VTXWriter(domain.comm, u_resultsfile, u, "bp5") as f:
