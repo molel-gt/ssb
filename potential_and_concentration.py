e4102979c2922e8d9374c06f2d3623873e7a956c
Leshinka Molel <emolel3@gatech.edu>
2024-06-22 23:55:16 -0400

    remove solvers

--- potential_and_concentration.py
+++ potential_and_concentration.py
@@ -22,7 +22,7 @@ from petsc4py import PETSc
 from ufl import (Circumradius, FacetNormal, SpatialCoordinate, TrialFunction, TestFunction,
                  dot, div, dx, ds, dS, grad, inner, grad, avg, jump)
 
-import commons, utils
+import commons, solvers, utils
 
 warnings.simplefilter('ignore')
 
@@ -32,121 +32,6 @@ faraday_const = 96485
 R = 8.3145
 T = 298
 D = 1e-15
-
-
-class NewtonSolver:
-    max_iterations: int
-    bcs: list[fem.DirichletBC]
-    A: PETSc.Mat
-    b: PETSc.Vec
-    J: fem.Form
-    b: fem.Form
-    dx: PETSc.Vec
-
-    def __init__(
-        self,
-        F: list[fem.form],
-        J: list[list[fem.form]],
-        w: list[fem.Function],
-        bcs: list[fem.DirichletBC] | None = None,
-        max_iterations: int = 5,
-        petsc_options: dict[str, str | float | int | None] = None,
-        problem_prefix="newton",
-    ):
-        self.max_iterations = max_iterations
-        self.bcs = [] if bcs is None else bcs
-        self.b = fem.petsc.create_vector_block(F)
-        self.F = F
-        self.J = J
-        self.A = fem.petsc.create_matrix_block(J)
-        self.dx = self.A.createVecLeft()
-        self.w = w
-        self.x = fem.petsc.create_vector_block(F)
-
-        # Set PETSc options
-        opts = PETSc.Options()
-        if petsc_options is not None:
-            for k, v in petsc_options.items():
-                opts[k] = v
-
-        # Define KSP solver
-        self._solver = PETSc.KSP().create(self.b.getComm().tompi4py())
-        self._solver.setOperators(self.A)
-        self._solver.setFromOptions()
-
-        # Set matrix and vector PETSc options
-        self.A.setFromOptions()
-        self.b.setFromOptions()
-
-    def solve(self, tol=1e-6, beta=1.0):
-        i = 0
-
-        while i < self.max_iterations:
-            dolfinx.cpp.la.petsc.scatter_local_vectors(
-                self.x,
-                [si.x.petsc_vec.array_r for si in self.w],
-                [
-                    (
-                        si.function_space.dofmap.index_map,
-                        si.function_space.dofmap.index_map_bs,
-                    )
-                    for si in self.w
-                ],
-            )
-            self.x.ghostUpdate(
-                addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD
-            )
-
-            # Assemble F(u_{i-1}) - J(u_D - u_{i-1}) and set du|_bc= u_D - u_{i-1}
-            with self.b.localForm() as b_local:
-                b_local.set(0.0)
-            fem.petsc.assemble_vector_block(
-                self.b, self.F, self.J, bcs=self.bcs, x0=self.x, scale=-1.0
-            )
-            self.b.ghostUpdate(
-                PETSc.InsertMode.INSERT_VALUES, PETSc.ScatterMode.FORWARD
-            )
-
-
-            # Assemble Jacobian
-            self.A.zeroEntries()
-            fem.petsc.assemble_matrix_block(self.A, self.J, bcs=self.bcs)
-            self.A.assemble()
-
-            self._solver.solve(self.b, self.dx)
-            # self._solver.view()
-            assert (
-                self._solver.getConvergedReason() > 0
-            ), "Linear solver did not converge"
-            offset_start = 0
-            for s in self.w:
-                num_sub_dofs = (
-                    s.function_space.dofmap.index_map.size_local
-                    * s.function_space.dofmap.index_map_bs
-                )
-                s.x.petsc_vec.array_w[:num_sub_dofs] -= (
-                    beta * self.dx.array_r[offset_start : offset_start + num_sub_dofs]
-                )
-                s.x.petsc_vec.ghostUpdate(
-                    addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD
-                )
-                offset_start += num_sub_dofs
-            # Compute norm of update
-
-            correction_norm = self.dx.norm(0)
-            print(f"Iteration {i}: Correction norm {correction_norm}")
-            if correction_norm < tol:
-                break
-            if np.isnan(self.dx.norm(0)):
-                break
-            i += 1
-
-    def __del__(self):
-        self.A.destroy()
-        self.b.destroy()
-        self.dx.destroy()
-        self._solver.destroy()
-        self.x.destroy()
 
 
 def arctanh(y):
