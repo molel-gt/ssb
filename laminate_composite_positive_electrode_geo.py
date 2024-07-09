f27940ff47fd0b97b942c8b540c34806093ec38e
Leshinka Molel <emolel3@gatech.edu>
2024-07-09 17:32:47 -0400

    update

--- laminate_composite_positive_electrode_geo.py
+++ laminate_composite_positive_electrode_geo.py
@@ -243,7 +243,7 @@ if __name__ == '__main__':
         gmsh.model.mesh.field.setNumber(2, "LcMin", resolution / 10)
         gmsh.model.mesh.field.setNumber(2, "LcMax", resolution)
         gmsh.model.mesh.field.setNumber(2, "DistMin", resolution)
-        gmsh.model.mesh.field.setNumber(2, "DistMax", 5 * resolution/)
+        gmsh.model.mesh.field.setNumber(2, "DistMax", 5 * resolution)
 
         gmsh.model.mesh.field.add("Max", 5)
         gmsh.model.mesh.field.setNumbers(5, "FieldsList", [2])
