b0b7f01c9015f22741aac381991cd4d28fe6de91
Leshinka Molel <emolel3@gatech.edu>
2024-07-17 14:10:38 -0400

    update

--- rxn-dist-geo.sh
+++ rxn-dist-geo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-python3 laminate_composite_positive_electrode_geo.py --dimensions 470-470-45 --lsep 15 --name_of_study reaction_distribution --img_id 11 --radius 6 --eps_am 0.5 --scaling CONTACT_LOSS_SCALING --resolution 3
-mpiexec -np 8 python3 lmb_3d_cc_new.py --mesh_folder output/reaction_distribution/470-470-45/11/0.5/3.0/ --dimensions 470-470-45 --name_of_study reaction_distribution  --Wa_p $Wa --gamma $gamma --kr $kr  #--atol 1e-15 --rtol 1e-14
+for imgId in 6 11 16 22
+do
+python3 laminate_composite_positive_electrode_geo.py --dimensions 470-470-45 --lsep 15 --name_of_study reaction_distribution --img_id $imgId --radius 6 --eps_am 0.5 --scaling CONTACT_LOSS_SCALING --resolution 10 --refine
+# mpiexec -n 8 python3 dg_charge_xfer.py --mesh_folder output/reaction_dist/470-470-45/$imgId/0.5/10.0/ --dimensions 470-470-45 --name_of_study reaction_dist  --Wa_p $Wa --gamma $gamma --kr $kr  #--atol 1e-15 --rtol 1e-14
+done
