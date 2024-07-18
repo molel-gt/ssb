f16fc131f53ecea40cfd437bcb8effebcd75e029
Leshinka Molel <emolel3@gatech.edu>
2024-07-17 20:42:04 -0400

    update script

--- contact-loss.sh
+++ contact-loss.sh
@@ -6,9 +6,9 @@ fi
 
 for img_id in 6 11 16 22
 do
-    for Lz in 1 15
+    for Lz in 1 30 50
     do
-        python3 contact_loss_separator_lma_geo.py --img_id $img_id --dimensions 470-470-$Lz --resolution $1
+        python3 contact_loss_separator_lma_geo.py --img_id $img_id --dimensions 470-470-$Lz --resolution $1 --refine
         mpiexec -n 24 python3 contact_loss_separator.py --mesh_folder output/contact_loss_lma/470-470-$Lz/$img_id/$1/
     done
 done
