13c6798b024b60ccbb924e9c3036d8e9c5979b30
Leshinka Molel <emolel3@gatech.edu>
2024-07-18 01:08:49 -0400

    sphere centers

---
+++ process-centers.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for eps in 0.1 0.2 0.3 0.4 0.5 0.6 0.7
+do
+    echo 'Processing file 'centers/$eps.csv
+    sed -i '1,6d' centers/$eps.csv
+    sed -i 's/[ \t]*$//' centers/$eps.csv
+    sed -i 's/\ /,/g' centers/$eps.csv
+    sed -i '1i x,y' centers/$eps.csv
+done
