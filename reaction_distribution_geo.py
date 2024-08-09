37a41ca488d02ba5ed3ce84a5a949e9bbce1ff5c
Leshinka Molel <emolel3@gatech.edu>
2024-08-09 16:05:29 -0400

    set meshing algorithm

--- reaction_distribution_geo.py
+++ reaction_distribution_geo.py
@@ -104,8 +104,11 @@ if __name__ == '__main__':
     # gmsh.option.setNumber('Mesh.MeshSizeExtendFromBoundary', 0)
     # gmsh.option.setNumber('Mesh.MeshSizeFromCurvature', 0)
     # gmsh.option.setNumber('Mesh.MeshSizeFromPoints', 0)
-    # if not args.refine:
-    #     gmsh.option.setNumber('Mesh.CharacteristicLengthMax', resolution)
+    gmsh.option.setNumber("Mesh.Algorithm3D", 10)
+    gmsh.option.setNumber("Mesh.MaxNumThreads2D", 12)
+    gmsh.option.setNumber("Mesh.MaxNumThreads3D", 12)
+    if not args.refine:
+        gmsh.option.setNumber('Mesh.CharacteristicLengthMax', resolution)
     z0_points = [
         (0, 0, 0),
         (LX, 0, 0),
