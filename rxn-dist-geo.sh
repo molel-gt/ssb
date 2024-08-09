0ec72a4570737bb6790492ac00c1108c8c1d1f8a
Leshinka Molel <emolel3@gatech.edu>
2024-08-09 01:42:05 -0400

    update script

--- rxn-dist-geo.sh
+++ rxn-dist-geo.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-for imgId in 6 11 16 22
+Wa=0.1
+gamma=15
+kr=1
+for imgId in 1 #6 11 16 22
 do
-    for eps in 0.7 #0.1 0.2 0.3 0.4 0.5 0.6 0.7
+    for eps in 0.1 #0.1 0.2 0.3 0.4 0.5 0.6 0.7
     do
-        python3 laminate_composite_positive_electrode_geo.py --name_of_study reaction_distribution --img_id $imgId --eps_am $eps --resolution 10 --refine
-        # mpiexec -n 8 python3 dg_charge_xfer.py --mesh_folder output/reaction_distribution/470-470-45/$imgId/15-30/0.5/10.0/ --dimensions 470-470-45 --name_of_study reaction_dist  --Wa_p $Wa --gamma $gamma --kr $kr  #--atol 1e-15 --rtol 1e-14
+    for se_pos_am_area_frac in 1.0 # 0.25 0.5 0.75 1.0
+        do
+            # python3 reaction_distribution_geo.py --name_of_study reaction_distribution --img_id $imgId --eps_am $eps --resolution 10 --se_pos_am_area_frac $se_pos_am_area_frac #--refine
+            mpiexec -n 2 python3 dg_charge_xfer.py --mesh_folder output/reaction_distribution/470-470-45/15-30/$imgId/$eps/$se_pos_am_area_frac/unrefined/10.0/  --Wa_p $Wa --gamma $gamma --kr $kr  #--atol 1e-15 --rtol 1e-14
+        done
     done
 done
