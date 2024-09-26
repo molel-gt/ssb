eaed92b57ef6d13195de4b745ce3983f0ee179e0
Leshinka Molel <emolel3@gatech.edu>
2024-09-26 15:10:42 -0400

    update

--- experimental/conduit_flow.cpp
+++ experimental/conduit_flow.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <gsl/gsl_math.h>
-#include <gsl/gsl_sf_hyperg.h>
+#include <gsl/gsl_sf.h>
+// #include <gsl/gsl_sf_hyperg.h>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -77,10 +78,35 @@ double get_k(double h, double L){
     if (error > tol) { throw std::invalid_argument("Could not converge\n"); } else { return k; }
 }
 
+double get_a(double w_over_L, double k){
+    mode_t prec_mode = GSL_PREC_DOUBLE;
+    double phi = 0.5 * M_PI_2; // pi/4
+    double phi_new;
+    double tol = 1e-3;
+    double error = tol + 1.0;
+    int max_iters = 1000;
+    double eps = std::numeric_limits<double>::epsilon();
+    double iters = 0;
+    double gamma = 0.25;
+    double ratio = 1 - 2 * w_over_L;
+    double Kk = gsl_sf_ellint_Kcomp(k, prec_mode);
+    while (error > tol && iters < max_iters){
+        phi_new = phi - gamma * Kk * sqrt(1 - pow(k, 2)*pow(sin(phi), 2)) *(-ratio + (1.0/Kk) * gsl_sf_ellint_F(phi, k, prec_mode));
+        // reset phi when exceeds bounds
+        if (fabs(phi_new) > M_PI_2) {phi_new = phi - eps; gamma *= 0.5;}
+        error = fabs(gsl_sf_ellint_F(phi_new, k, prec_mode)/Kk - ratio)/ratio;
+        phi = phi_new;
+        iters++;
+    }
+    if (error > tol) { throw std::invalid_argument("Could not converge\n"); } else { double a = sin(phi); return a; }
+}
+
 int main(int argc, char **argv){
-    double k, b;
+    std::vector<double> w_over_L = {0.49, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0.01};
+    double k, b, h_L, w_L;
     fstream fin;
     fin.open("aspect-ratios.txt", ios::in);
+
     vector<double> aspectratios;
     if (!fin.is_open()) {
         std::cerr << "Error opening the file!" << std::endl;
@@ -92,20 +118,37 @@ int main(int argc, char **argv){
     }
 
     fin.close();
-    ofstream fout;
+    ofstream fout, fout2;
     fout.open("geometric-ratios.csv");
+    fout2.open("geometric-factors.csv");
+    fout2 << "k,b,a,h_over_L,w_over_L\n";
     fout  << "aspect_ratio,b\n";
     for (auto & element : aspectratios){
         try {
             k = get_k(element, 1.0);
             b = 1.0/k;
-            fout << element << "," << std::setprecision(16) << b << "\n";}
+            fout << element << "," << std::setprecision(16) << b << "\n";
+            h_L = element;
+        }
         catch (const std::invalid_argument& e) {
             fout << element << "," << "-\n";
             std::cout << "Could not converge for h/L = " << element << "\n" ;
         }
+
+        for (auto & element : w_over_L)
+            {
+                try
+                {
+                    w_L = element;
+                    double a = get_a(w_L, k);
+                    printf("w/L: %lf, a: %lf, b: %lf\n", w_L, a, b);
+                    fout2 << std::setprecision(16) << k << "," << std::setprecision(16) << b << "," << std::setprecision(16) << a << "," << std::setprecision(16) << h_L << "," << std::setprecision(16) << w_L << "\n";
+            }
+            catch (const std::invalid_argument& e) {std::cout << "Could not converge for w/L = " << w_L << " and for k = " << k << "\n" ;} 
+        }
     }
     fout.close();
+    fout2.close();
 
     return 0;
 }
