f782c920ba35f15859031981e2a5ded94387d352
Leshinka Molel <emolel3@gatech.edu>
2024-10-01 17:11:11 -0400

    update

--- conduit_flow.py
+++ conduit_flow.py
@@ -60,70 +60,6 @@ class Labels:
         return 7
 
 
-
-def create_mesh(output_meshfile, markers, L=1, w_over_L=0.1, h_over_L=0.1, resolution=0.5e-3, refine=True, **kwargs):
-    """
-    """
-    points_bottom = [
-        (0, 0, 0),
-        (w_over_L * L, 0, 0),
-        (L * (1 - w_over_L), 0, 0),
-        (L, 0, 0),
-    ]
-    points_top = [
-        (0, h_over_L * L, 0),
-        (L, h_over_L * L, 0)
-    ]
-    gpoints_top = []
-    gpoints_bottom = []
-    lines = []
-    gmsh.initialize()
-    gmsh.model.add('conduit')
-    gpoints_top.append(gmsh.model.occ.addPoint(*points_top[0]))
-    gpoints_top.append(gmsh.model.occ.addPoint(*points_top[1]))
-    gpoints_bottom.extend([gmsh.model.occ.addPoint(*p) for p in points_bottom])
-
-    lines.append(gmsh.model.occ.addLine(gpoints_bottom[0], gpoints_top[0]))
-    lines.append(gmsh.model.occ.addLine(gpoints_top[0], gpoints_top[1]))
-    lines.append(gmsh.model.occ.addLine(gpoints_top[1], gpoints_bottom[-1]))
-    lines.append(gmsh.model.occ.addLine(gpoints_bottom[-1], gpoints_bottom[-2]))
-    lines.append(gmsh.model.occ.addLine(gpoints_bottom[-2], gpoints_bottom[-3]))
-    lines.append(gmsh.model.occ.addLine(gpoints_bottom[-3], gpoints_bottom[-4]))
-
-    curve_loop = gmsh.model.occ.addCurveLoop(lines)
-    surf = gmsh.model.occ.addPlaneSurface([curve_loop])
-    gmsh.model.occ.synchronize()
-    gmsh.model.addPhysicalGroup(2, [surf], markers.domain, "domain")
-
-    gmsh.model.addPhysicalGroup(1, [lines[0]], markers.left, "left")
-    gmsh.model.addPhysicalGroup(1, [lines[1]], markers.top, "top")
-    gmsh.model.addPhysicalGroup(1, [lines[2]], markers.right, "right")
-    gmsh.model.addPhysicalGroup(1, [lines[3]], markers.outlet, "outlet")
-    gmsh.model.addPhysicalGroup(1, [lines[4]], markers.inlet_outlet_separation, "inlet-outlet separation")
-    gmsh.model.addPhysicalGroup(1, [lines[-1]], markers.inlet, "inlet")
-    gmsh.model.addPhysicalGroup(1, lines[:-3] + [lines[-2]], markers.insulated, "insulated")
-
-    if refine:
-        gmsh.model.mesh.field.add("Distance", 1)
-        gmsh.model.mesh.field.setNumbers(1, "CurvesList", lines)
-
-        gmsh.model.mesh.field.add("Threshold", 2)
-        gmsh.model.mesh.field.setNumber(2, "IField", 1)
-        gmsh.model.mesh.field.setNumber(2, "LcMin", resolution/5)
-        gmsh.model.mesh.field.setNumber(2, "LcMax", resolution)
-        gmsh.model.mesh.field.setNumber(2, "DistMin", resolution)
-        gmsh.model.mesh.field.setNumber(2, "DistMax", 5 * resolution)
-
-        gmsh.model.mesh.field.add("Max", 5)
-        gmsh.model.mesh.field.setNumbers(5, "FieldsList", [2])
-        gmsh.model.mesh.field.setAsBackgroundMesh(5)
-        gmsh.model.occ.synchronize()
-
-    gmsh.model.mesh.generate(2)
-    gmsh.write(output_meshfile)
-    gmsh.finalize()
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='secondary current distribution')
     parser.add_argument('--mesh_folder', help='parent folder containing mesh folder', required=True)
@@ -131,9 +67,9 @@ if __name__ == '__main__':
     parser.add_argument("--mu", help="viscosity", nargs='?', const=1, default=1.0, type=float)
     parser.add_argument("--p_in", help="inlet gauge pressure", nargs='?', const=1, default=1.0, type=float)
     parser.add_argument("--p_out", help="outlet gauge pressure", nargs='?', const=1, default=0, type=float)
-    parser.add_argument("--Lc", help="characteristic length", nargs='?', const=1, default=1.0, type=np.float16)
-    parser.add_argument("--h_over_L", help="aspect ratio", nargs='?', const=1, default=0.1, type=np.float16)
-    parser.add_argument("--w_over_L", help="aspect ratio of inlet/outlet", nargs='?', const=1, default=0.1, type=np.float16)
+    parser.add_argument("--Lc", help="characteristic length", nargs='?', const=1, default=1.0, type=float)
+    parser.add_argument("--h_over_L", help="aspect ratio", nargs='?', const=1, default=0.1, type=float)
+    parser.add_argument("--w_over_L", help="aspect ratio of inlet/outlet", nargs='?', const=1, default=0.1, type=float)
     args = parser.parse_args()
     mesh_folder = os.path.join("output", "conduit_flow")
     workdir = os.path.join(args.mesh_folder, str(args.Lc), str(args.h_over_L), str(args.w_over_L))
@@ -141,16 +77,11 @@ if __name__ == '__main__':
     output_meshfile_path = os.path.join(workdir, "mesh.msh")
     markers = Labels()
     comm = MPI.COMM_WORLD
-    params = {"a": 5e-3, "w": 200e-3, "t": 20e-3}
-    create_mesh(output_meshfile_path, markers, L=args.Lc, h_over_L=args.h_over_L, w_over_L=args.w_over_L)
-
-    results_dir = os.path.join(mesh_folder, "")
-    utils.make_dir_if_missing(results_dir)
 
     output_current_path = os.path.join(workdir, 'current.bp')
     output_potential_path = os.path.join(workdir, 'potential.bp')
     frequency_path = os.path.join(workdir, 'frequency.csv')
-    simulation_metadata_file = os.path.join(workdir, 'simulation.json')
+    simulation_metafile = os.path.join(workdir, 'simulation.json')
     left_values_path = os.path.join(workdir, 'left_values')
     right_values_path = os.path.join(workdir, 'right_values')
 
@@ -216,8 +147,8 @@ if __name__ == '__main__':
     q_h = fem.Function(W, name='current_density')
     q_h.interpolate(q_expr)
     norm_factor = (args.mu/args.k)*(args.Lc/np.abs(args.p_in - args.p_out))
-    q_tilde_in =  comm.allreduce(fem.assemble_scalar(fem.form(inner(q_h, n) * ds(markers.left), entity_maps=entity_maps)), op=MPI.SUM) * norm_factor
-    q_tilde_in =  comm.allreduce(fem.assemble_scalar(fem.form(inner(q_h, n) * ds(markers.right), entity_maps=entity_maps)), op=MPI.SUM) * norm_factor
+    q_tilde_in =  comm.allreduce(fem.assemble_scalar(fem.form(inner(q_h, n) * ds(markers.left))), op=MPI.SUM) * norm_factor
+    q_tilde_out =  comm.allreduce(fem.assemble_scalar(fem.form(inner(q_h, n) * ds(markers.right))), op=MPI.SUM) * norm_factor
     simulation_metadata = {
         "qtilde in": q_tilde_in,
         "qtilde out": q_tilde_out,
