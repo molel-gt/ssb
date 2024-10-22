2aa47220a4e5dae8fbf32cee91be110a9b684652
Leshinka Molel <emolel3@gatech.edu>
2024-10-21 22:48:14 -0400

    update

--- experimental/schwarz_christoffel.cpp
+++ experimental/schwarz_christoffel.cpp
@@ -1,4 +1,4 @@
-// #include <complex.h>
+#include <complex.h>
 #include <exception>
 #include <fstream>
 #include <gsl/gsl_complex.h>
@@ -48,23 +48,43 @@ gsl_complex dz_dt(gsl_complex t, void *p){
 //     return res;
 // }
 
-// gsl_complex z_t(gsl_complex a, gsl_complex b, void *p){
-//     gsl_function F;
-//     struct Parameters *params = (struct Parameters *)p;
-//     F.function = &dz_dt;
-//     F.params = &params;
-//     vector<double> pts;
-//     pts.push_back(a);
-//     int n = sizeof(params->angles) / sizeof(params->angles[0]);
-//     for (int i = 0; i < n; i++){
-//         if (a < params->prevertices[i] < b){
-//             pts.push_back(params->prevertices[i]);
-//         }
-//     }
-//     pts.push_back(b);
-//     size_t npts = std::vector::size(pts);
-// }
+gsl_complex z_t(gsl_complex a, gsl_complex b, void *p){
+    gsl_complex result;
+    gsl_integration_workspace * w = gsl_integration_workspace_alloc (1000);
+    gsl_function F;
+    double error;
+    struct Parameters *params = (struct Parameters *)p;
+    F.function = &dz_dt;
+    F.params = &params;
+    vector<gsl_complex> pts;
+    pts.push_back(a);
+    // int n = sizeof(params->angles) / sizeof(params->angles[0]);
+    for (int i = 0; i < N; i++){
+        if (a < params->coefs[i].first < b){
+            pts.push_back(params->coefs[i].first);
+        }
+    }
+    pts.push_back(b);
+    size_t npts = pts.size();
+    gsl_integration_qagp(&F, pts, npts, 0, 1e-7, 1000,
+                        w, &result, &error);
+    return result;
+}
 
 int main(int argc, char** argv){
+    struct Parameters *params;
+    params->A = gsl_complex_rect(1.0, 0);
+    gsl_complex t0 = gsl_complex_rect(-1.05, 0);
+    gsl_complex t1 = gsl_complex_rect(-1.0, 0);
+    gsl_complex t2 = gsl_complex_rect(1.0, 0);
+    gsl_complex t3 = gsl_complex_rect(1.05, 0);
+    params->coefs[0] = std::pair<gsl_complex, double>(t0, -0.5);
+    params->coefs[1] = std::pair<gsl_complex, double>(t1, -0.5);
+    params->coefs[2] = std::pair<gsl_complex, double>(t2, -0.5);
+    params->coefs[3] = std::pair<gsl_complex, double>(t3, -0.5);
+    gsl_complex a = gsl_complex_rect(0.0, 0.0);
+    gsl_complex b = gsl_complex_rect(1.0, 0.0);
+    gsl_complex integ = z_t(a, b, params);
+
     return 0;
 }
