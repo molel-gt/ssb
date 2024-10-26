a9c2228415161b991dc67f6a410e334359cf64c4
Leshinka Molel <emolel3@gatech.edu>
2024-10-25 21:09:29 -0400

    update

--- experimental/secondary_current.c
+++ experimental/secondary_current.c
@@ -8,12 +8,12 @@ int main(int argc, char **argv)
     int Nel = 10;
     /* remove rows corresponding to dirichlet bc,
         then add extra DoF due to discontinuity */
-    int N = Nel - 1 + 1;
+    int N = Nel-1;
     double h = 1.0 / Nel; 
     int i;
     double ab[1] = {1.0/h};
     int j[1] = {N - 1};
-    double gamma = 10;
+    double gamma = 15.0;
 
     PetscInitialize(&argc, &argv, NULL, "ksp problem solution");
     VecCreate(PETSC_COMM_WORLD, &b);
@@ -27,39 +27,43 @@ int main(int argc, char **argv)
     MatSetSizes(A, PETSC_DECIDE, PETSC_DECIDE, N, N);
     MatSetFromOptions(A);
     MatSetUp(A);
-    int num_entries = 3;
+
     for (i = 0; i < N; i++)
     {
         if (i == 0){
-            num_entries = 2;
             int j[2] = {i, i+1};
             double entries[2] = {2.0 / h, -1.0 / h};
-            MatSetValues(A, 1, &i, num_entries, j, entries, INSERT_VALUES);
+            MatSetValues(A, 1, &i, 2, j, entries, INSERT_VALUES);
         } else if (i == N - 1){
-            num_entries = 2;
             int j[2] = {i-1, i};
             double entries[2] = {-1.0 / h, 2.0 / h};
-            MatSetValues(A, 1, &i, num_entries, j, entries, INSERT_VALUES);
+            MatSetValues(A, 1, &i, 2, j, entries, INSERT_VALUES);
         }
-        else if (i == 5){
-            /* inner(grad(u), grad(v)) */
-            num_entries = 2;
-            int j[2] = {i-1, i};
-            double entries[2] = {-1.0 / h, 1.0 / h};
-            MatSetValues(A, 1, &i, num_entries, j, entries, INSERT_VALUES);
+        else if (i == Nel/2-2){
+            int j[5] = {i-1, i, i+1, i+2, i+3};
+            double entries[5] = {-1.0/h, 1.0/h*(2.0 - gamma), -0.5/h, -0.5/h, gamma/h};
+            MatSetValues(A, 1, &i, 5, j, entries, INSERT_VALUES);
         }
-        else if (i == 6){
-            /* inner(grad(u), grad(v)) */
-            num_entries = 2;
-            int j[2] = {i, i+1};
-            double entries[2] = {1.0 / h, -1.0 / h};
-            MatSetValues(A, 1, &i, num_entries, j, entries, INSERT_VALUES);
+        else if (i == Nel/2-1){
+            int j[4] = {i-1, i, i+1, i+2};
+            double entries[4] = {-1.5/h, 1.0/h, 0.0/h, 0.5/h};
+            MatSetValues(A, 1, &i, 4, j, entries, INSERT_VALUES);
+        }
+        else if (i == Nel/2){
+            int j[4] = {i-2, i-1, i, i+1};
+            double entries[4] = {0.5/h, 0.0/h, 1.0/h, -1.5/h};
+            MatSetValues(A, 1, &i, 4, j, entries, INSERT_VALUES);
+        }
+        else if (i == Nel/2+1){
+            int j[5] = {i-3, i-2, i-1, i, i+1};
+            double entries[5] = {gamma/h, -0.5/h, -0.5/h, 1.0/h*(2.0 - gamma), -1.0/h};
+            MatSetValues(A, 1, &i, 5, j, entries, INSERT_VALUES);
         }
         else {
-            num_entries = 3;
+            printf("%d\n", i);
             int j[3] = {i-1, i, i+1};
             double entries[3] = {-1.0 / h, 2.0 / h, -1.0 / h};
-            MatSetValues(A, 1, &i, num_entries, j, entries, INSERT_VALUES);
+            MatSetValues(A, 1, &i, 3, j, entries, INSERT_VALUES);
         }
         
 
