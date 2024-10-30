32ef694f15de939fdf96215a32f1d7332942368d
Leshinka Molel <emolel3@gatech.edu>
2024-10-30 15:19:58 -0400

    rescale

--- interval_mesh.py
+++ interval_mesh.py
@@ -5,15 +5,15 @@ import os
 import gmsh
 import commons, utils
 N = 100
-scale = 100e-6
+scale = 1  # 100e-6
 points = [(scale*idx/N, 0, 0) for idx in range(int(N+1))]
-
+resolution = 0.01
 
 markers = commons.Markers()
 
 gmsh.initialize()
 gmsh.model.add('interval')
-gmsh.option.setNumber('Mesh.CharacteristicLengthMax', 0.001e-6)
+gmsh.option.setNumber('Mesh.CharacteristicLengthMax', resolution)
 gpoints = [gmsh.model.occ.addPoint(*p) for p in points]
 
 lines = [gmsh.model.occ.addLine(gpoints[int(idx2)], gpoints[int(idx2+1)]) for idx2 in range(N)]
@@ -25,6 +25,6 @@ gmsh.model.addPhysicalGroup(0, [gpoints[0]], markers.left, "left")
 gmsh.model.addPhysicalGroup(0, [gpoints[int(N/2)]], markers.electrolyte_v_positive_am, "interface")
 gmsh.model.addPhysicalGroup(0, [gpoints[-1]], markers.right, "right")
 gmsh.model.mesh.generate(1)
-workdir = "output/secondary_current/1-0-0/"
+workdir = "output/secondary_current/100-0-0/"
 utils.make_dir_if_missing(workdir)
 gmsh.write(os.path.join(workdir, "mesh.msh"))
