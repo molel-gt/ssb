dc24173f61f12fa977697d15480eb02ff4cf1656
Leshinka Molel <emolel3@gatech.edu>
2024-11-13 17:12:53 -0500

    install petsc

--- install-petsc.sh
+++ install-petsc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-./configure --download-f2cblaslapack --with-mpi-dir=~/opt/ --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-eigen --download-hdf5 PETSC_ARCH=real-int32 --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-mumps --download-scalapack --download-ml
+cd $HOME/softwares/petsc
+./configure --download-f2cblaslapack --with-mpi-dir=~/opt/ --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-ptscotch --download-eigen --download-hdf5 PETSC_ARCH=real-int32 --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-mumps --download-scalapack --download-ml --download-trilinos --download-suitesparse --download-spai
 make PETSC_DIR=$HOME/softwares/petsc PETSC_ARCH=real-int32 all
 make PETSC_DIR=$HOME/softwares/petsc PETSC_ARCH=real-int32 check
+source $HOME/python3-env/bin/activate
 python3 -m pip install src/binding/petsc4py
