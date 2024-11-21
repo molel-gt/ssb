c8886e9e911cee1e55c9a076a1cd4b6c4083cc4a
Leshinka Molel <emolel3@gatech.edu>
2024-11-21 14:42:15 -0500

    remove trilinos

--- install-petsc.sh
+++ install-petsc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME/softwares/petsc
-./configure --download-f2cblaslapack --with-mpi-dir=~/opt/ --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-ptscotch --download-eigen --download-hdf5 PETSC_ARCH=real-int32 --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-mumps --download-scalapack --download-ml --download-trilinos --download-suitesparse --download-spai --download-strumpack --download spooles
+./configure --download-f2cblaslapack --with-mpi-dir=~/opt/ --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-ptscotch --download-eigen --download-hdf5 PETSC_ARCH=real-int32 --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-mumps --download-scalapack --download-ml --download-suitesparse --download-spai --download-strumpack --download-spooles
 make PETSC_DIR=$HOME/softwares/petsc PETSC_ARCH=real-int32 all
 make PETSC_DIR=$HOME/softwares/petsc PETSC_ARCH=real-int32 check
 source $HOME/python3-env/bin/activate
