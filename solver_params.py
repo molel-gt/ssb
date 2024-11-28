c79622ae590c00f5ff65f6f9072a64ff1a101f83
Leshinka Molel <emolel3@gatech.edu>
2024-11-27 19:30:22 -0500

    solver parameters

--- solver_params.py
+++ solver_params.py
@@ -1,12 +1,16 @@
 gamg = {
-    "pc_gamg_agg_nsmooths": 0,
     "pc_gamg_type": 'agg',
-    "pc_gamg_threshold": -1,
+    "pc_gamg_threshold": 0,
+    "pc_gamg_repartition": True,
     "pc_gamg_aggressive_coarsening": 0,
     "pc_gamg_aggressive_square_graph": 0,
+    # "pc_gamg_agg_nsmooths": 1,
+    "pc_gamg_coarse_eq_limit": 1000,
+    "pc_mg_type": "kaskade",
 }
 
 boomeramg = {
+    "pc_hypre_type": "boomeramg",
     "pc_hypre_boomeramg_coarsen_type": "pmis",
     "pc_hypre_boomeramg_interp_type": "FF1",
     "pc_hypre_boomeramg_strong_threshold": 0.5,
@@ -16,13 +20,14 @@ boomeramg = {
     "pc_hypre_boomeramg_interp_type": "FF1",
     "pc_hypre_boomeramg_strong_threshold": 0.7,
     "pc_hypre_boomeramg_agg_nl": 2,
-    "pc_hypre_boomeramg_agg_num_paths": 10,
-    "pc_hypre_boomeramg_trunc_factor": 0.3,
+    "pc_hypre_boomeramg_agg_num_paths": 5,
+    "pc_hypre_boomeramg_truncfactor": 0.75,
     "pc_hypre_boomeramg_smooth_num_levels": 2,
     "pc_hypre_boomeramg_vec_interp_variant": 3,
     "pc_hypre_boomeramg_nodal_coarsen": 4,
     "pc_hypre_boomeramg_max_iter": 4,
     "pc_hypre_boomeramg_max_levels": 5,
     "pc_hypre_boomeramg_relax_type_all": "SOR/jacobi",
+    'pc_hypre_boomeramg_p_max': 2,
     "pc_hypre_boomeramg_print_statistics": 2,
 }
