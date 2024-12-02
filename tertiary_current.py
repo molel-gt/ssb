d316925d4afa195400f1906eaaafbdadda81d191
Leshinka Molel <emolel3@gatech.edu>
2024-12-01 23:56:11 -0500

    update

--- tertiary_current.py
+++ tertiary_current.py
@@ -546,7 +546,7 @@ if __name__ == '__main__':
             opts = PETSc.Options()
             opts['snes_linesearch_monitor'] = None
             opts['snes_monitor'] = None
-            opts['snes_linesearch_type'] = 'cp'
+            # opts['snes_linesearch_type'] = 'cp'
             opts[f"{snes.getKSP().getOptionsPrefix()}pc_fieldsplit_off_diag_use_amat"] = True
             # opts[f"{snes.getKSP().getOptionsPrefix()}pc_fieldsplit_detect_saddle_point"] = True
 
