afc61fba8d679bdf3c60a975f6729ba6d524368b
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 17:04:16 -0500

    install pkg-config

---
+++ install-pkgconfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $SOFTWARES_DIR
+
+if [ -d $SOFTWARES_DIR/pkg-config ]; then;
+    echo 'directory exists, skip cloning'
+else;
+    git clone https://gitlab.freedesktop.org/pkg-config/pkg-config.git .
+fi
+
+cd $SOFTWARES_DIR/pkg-config
+cmake . -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
+make -j && make install
