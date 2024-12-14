4c5dc5a5c9f9e9ed2983c189984a8f7d2bfa2520
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 18:44:47 -0500

    install pkgconf

--- install-pkgconf.sh
+++ install-pkgconf.sh
@@ -9,8 +9,9 @@ else
 fi
 
 cd $SOFTWARES_DIR/pkgconf
+./autogen.sh
 ./configure --prefix=$CMAKE_INSTALL_PREFIX
-cmake . -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
+# cmake . -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
 make -j && make install
 
 echo "export PKG_CONFIG=$CMAKE_INSTALL_PREFIX/bin/pkgconf" >> $HOME/.bashrc
