4a001123f6af0c24fe19dfb3caf20d0ea10fc29d
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 16:18:20 -0500

    update

--- install-adios2.sh
+++ install-adios2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd $HOME/softwares
+cd $SOFTWARES_DIR
 
-if -d $HOME/softwares/ADIOS2; then
+if -d $SOFTWARES_DIR/ADIOS2; then
     echo 'ADIOS2 directory exists'
 else
     git clone https://github.com/ornladios/ADIOS2.git ADIOS2
@@ -11,10 +11,10 @@ fi
 mkdir adios2-build && cd adios2-build
 
 
-cmake ../ADIOS2 -DCMAKE_PREFIX_PATH=$HOME/opt -DCMAKE_INSTALL_PREFIX=$HOME/opt -DADIOS2_BUILD_EXAMPLES=ON -DADIOS2_USE_MPI=ON -DADIOS2_USE_HDF5=ON -DADIOS2_USE_PIP=ON -DBUILD_SHARED_LIBS=ON -DADIOS2_USE_Python=ON -DADIOS2_USE_Fortran=ON -DCMAKE_CXX_FLAGS='-std=c++11'
+cmake ../ADIOS2 -DCMAKE_PREFIX_PATH=$DCMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DADIOS2_BUILD_EXAMPLES=ON -DADIOS2_USE_MPI=ON -DADIOS2_USE_HDF5=ON -DADIOS2_USE_PIP=ON -DBUILD_SHARED_LIBS=ON -DADIOS2_USE_Python=ON -DADIOS2_USE_Fortran=ON -DCMAKE_CXX_FLAGS='-std=c++11'
 
 make -j3
 make install
 
-#export ADIOS2_ROOT=$HOME/opt/adios2
-#export ADIOS2_DIR=$HOME/opt/adios2
+echo "export ADIOS2_ROOT=$CMAKE_INSTALL_PREFIX/adios2" >> $HOME/.bashrc
+echo "export ADIOS2_DIR=$CMAKE_INSTALL_PREFIX/adios2"  >> $HOME/.bashrc
