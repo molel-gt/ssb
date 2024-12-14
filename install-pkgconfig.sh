e24b45a1501d03443a07f787825b91429c9a6a0f
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 18:42:30 -0500

    install

--- install-pkgconfig.sh
+++ install-pkgconfig.sh
@@ -9,7 +9,7 @@ else
 fi
 
 cd $SOFTWARES_DIR/pkg-config
-./autogen.sh
+./autogen.sh --prefix=$CMAKE_INSTALL_PREFIX --with-internal-glib
 ./configure --prefix=$CMAKE_INSTALL_PREFIX
 cmake . -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
 make -j && make install
