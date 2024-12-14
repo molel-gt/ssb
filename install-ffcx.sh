81941c41ed820c996f23e3c9d48ee84421c21156
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 17:24:29 -0500

    install ffcx

--- install-ffcx.sh
+++ install-ffcx.sh
@@ -2,26 +2,26 @@
 
 cd $SOFTWARES_DIR
 
-BASIX_DIR=$SOFTWARES_DIR/basix
+FFCX_DIR=$SOFTWARES_DIR/ffcx
 
 . $PYTHON3_DIR/bin/activate
 
-if [ -d "$BASIX_DIR" ]; then
+if [ -d "$FFCX_DIR" ]; then
     echo "Skip cloning, directory exists"
 else
-    git clone https://github.com/FEniCS/basix.git .
+    git clone https://github.com/FEniCS/ffcx.git .
 fi
 
-cd $BASIX_DIR
+cd $FFCX_DIR
 mkdir $DOLFINX_BUILD_DIR
 
-cd $BASIX_DIR/cpp
+cd $FFCX_DIR/cpp
 mkdir build
-cd $BASIX_DIR/cpp/build
+cd $FFCX_DIR/cpp/build
 cmake .. -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
 make -j && make install
 
-cd $BASIX_DIR/python
+cd $FFCX_DIR/python
 $PYTHON3_DIR/bin/python3 -m pip install .
 
 # $PYTHON3_DIR/bin/python3 -m pip install -r build-requirements.txt
