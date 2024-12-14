af83f1ab947ceb8617eef9a96d9dc86558094a15
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 17:00:59 -0500

    install pugixml

---
+++ install-pugixml.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -d $SOFTWARES_DIR/pugixml ]; then
+    echo 'directory exists, not cloning'
+else
+    git git clone https://github.com/zeux/pugixml.git .
+fi
+
+cd $SOFTWARES_DIR/pugixml
+
+cmake . -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
+make -j && make install
