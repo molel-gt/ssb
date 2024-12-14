4828667ca97d6ceef147f937bd3a1c63df085580
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 02:39:37 -0500

    install slepc

--- install-slepc.sh
+++ install-slepc.sh
@@ -3,4 +3,4 @@ cd $HOME/softwares/slepc
 ./configure --prefix=$HOME/opt --with-clean
 make SLEPC_DIR=$SLEPC_DIR PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH
 make SLEPC_DIR=$SLEPC_DIR PETSC_DIR=$PETSC_DIR install
-$HOME/python3-env/bin/python3 -m pip install .
+$HOME/python3-env/bin/python3 -m pip install src/binding/slepc4py
