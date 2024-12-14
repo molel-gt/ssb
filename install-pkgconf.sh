74f6b1ecfc8faee7403af24e837dfebf10320f64
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 18:34:11 -0500

    install pkgconf

---
+++ install-pkgconf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $SOFTWARES_DIR
+
+if [ -d $SOFTWARES_DIR/pkgconf ]; then
+    echo 'directory exists, skip cloning'
+else
+    git clone https://github.com/pkgconf/pkgconf.git
+fi
+
+cd $SOFTWARES_DIR/pkgconf
+./configure --prefix=$CMAKE_INSTALL_PREFIX
+cmake . -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
+make -j && make install
