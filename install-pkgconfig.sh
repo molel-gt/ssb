0fb5352ed2ce5f7586b2b12a4430a5f01b85972e
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 18:25:01 -0500

    install

--- install-pkgconfig.sh
+++ install-pkgconfig.sh
@@ -2,7 +2,7 @@
 
 cd $SOFTWARES_DIR
 
-if [ -d $SOFTWARES_DIR/pkg-config ]; then;
+if [ -d $SOFTWARES_DIR/pkg-config ]; then
     echo 'directory exists, skip cloning'
 else;
     git clone https://gitlab.freedesktop.org/pkg-config/pkg-config.git
