d2424c7f9834098e55274fdbae3ef403534ac476
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 19:11:57 -0500

    update

--- setup-fea-env.sh
+++ setup-fea-env.sh
@@ -7,3 +7,13 @@ if [ $? -eq 0 ]; then
 else
     echo "NOT HPC environment"
 fi
+
+sh install-pkgconf.sh
+sh install-pugixml.sh
+sh install-adios2.sh
+sh install-petsc.sh
+sh install-slepc.sh
+sh install-basix.sh
+sh install-ffcx.sh
+
+$PYTHON3_DIR/bin/python3 -m pip install ufl
