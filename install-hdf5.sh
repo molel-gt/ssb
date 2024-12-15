f69e2363f557ed1b676f0f01dfcfcb6812876dc6
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 19:26:45 -0500

    hdf5

---
+++ install-hdf5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $SOFTWARES_DIR
+
+if [ -d $SOFTWARES_DIR/hdf5 ]; then
+    echo 'directory exists, skip cloning'
+else
+    git clone https://github.com/HDFGroup/hdf5.git
+fi
+
+cd $SOFTWARES_DIR/hdf5
+./autogen.sh
+./configure --enable-parallel --prefix=$CMAKE_INSTALL_PREFIX
+make -j && make install
