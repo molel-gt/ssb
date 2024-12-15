ae8aff076cecb12b948fd4ddfa15a0889129fca9
Leshinka Molel <emolel3@gatech.edu>
2024-12-15 02:38:58 -0500

    install

--- setup-fea-env.sh
+++ setup-fea-env.sh
@@ -17,4 +17,7 @@ sh install-basix.sh
 sh install-ufl.sh
 sh install-ffcx.sh
 export CC=$CMAKE_INSTALL_PREFIX/bin/mpicc
+# sh install-autoconf.sh
 sh install-hdf5.sh
+$PYTHON3_DIR/bin/python3 -m pip install python-gmsh
+sh install-fenicsx.sh
