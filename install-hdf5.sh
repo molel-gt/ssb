2b9e097afd8e3397ac8b2eb82aeafb7351cff47c
Leshinka Molel <emolel3@gatech.edu>
2024-12-14 19:57:04 -0500

    hdf5

--- install-hdf5.sh
+++ install-hdf5.sh
@@ -10,5 +10,5 @@ fi
 
 cd $SOFTWARES_DIR/hdf5
 ./autogen.sh
-./configure --enable-parallel --prefix=$CMAKE_INSTALL_PREFIX
+CC=$CMAKE_INSTALL_PREFIX/bin/mpicc ./configure --enable-parallel --prefix=$CMAKE_INSTALL_PREFIX
 make -j && make install
