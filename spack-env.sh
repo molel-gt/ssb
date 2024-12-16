72a296d02da25700b637dc423ec95dc235cacc46
Leshinka Molel <emolel3@gatech.edu>
2024-12-16 14:33:30 -0500

    fix version

--- spack-env.sh
+++ spack-env.sh
@@ -6,9 +6,10 @@ if [ -d $SOFTWARES_DIR/spack ]; then
     echo 'directory exists, skip cloning'
 else
     git clone https://github.com/spack/spack.git
+    cd $SOFTWARES_DIR/spack
     git checkout v0.23.0
     cp $WORK_DIR/../spack/config.yaml $SOFTWARES_DIR/spack/
-    . ./spack/share/spack/setup-env.sh
+    . $SOFTWARES_DIR/spack/share/spack/setup-env.sh
     spack env create fenicsx-env
     spack env activate fenicsx-env
     # spack add fenics-dolfinx@main%gcc@12.3.0+adios2 py-fenics-dolfinx%gcc@12.3.0 cflags="-O3" fflags="-O3"
