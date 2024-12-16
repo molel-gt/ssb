5c83bb95b1c60123a649a27630e83a019335b79b
Leshinka Molel <emolel3@gatech.edu>
2024-12-16 14:24:10 -0500

    fix version

--- spack-env.sh
+++ spack-env.sh
@@ -6,7 +6,8 @@ if [ -d $SOFTWARES_DIR/spack ]; then
     echo 'directory exists, skip cloning'
 else
     git clone https://github.com/spack/spack.git
-    # cp $WORK_DIR/../spack/config.yaml $SOFTWARES_DIR/spack/
+    git checkout v0.23.0
+    cp $WORK_DIR/../spack/config.yaml $SOFTWARES_DIR/spack/
     . ./spack/share/spack/setup-env.sh
     spack env create fenicsx-env
     spack env activate fenicsx-env
