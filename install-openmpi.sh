dd76021097db024a3a47ee0489cd3f47bc7a3634
Leshinka Molel <emolel3@gatech.edu>
2024-12-15 22:31:27 -0500

    update

--- install-openmpi.sh
+++ install-openmpi.sh
@@ -14,6 +14,6 @@ git checkout v5.0.6
 
 cd $OPENMPI_SRC_DIR
 ./autogen.pl
-./configure --prefix=$CMAKE_INSTALL_PREFIX --enable-shared --with-hwloc=embedded --without-cuda --without-ze --with-cuda=no --with-ze=no --enable-mca-no-build=pgpu --enable-mpi-ext=affinity,ftmpi,rocm,shortfloat --enable-mca-dso=null,rocm,ze --enable-fast=O3,avx,ndebug
+./configure --prefix=$CMAKE_INSTALL_PREFIX --enable-shared --with-pmix=internal --with-hwloc=embedded --without-cuda --without-ze --with-cuda=no --with-ze=no --enable-mca-no-build=pgpu --enable-mpi-ext=affinity,ftmpi,rocm,shortfloat --enable-mca-dso=null,rocm,ze
 make -j3
 make install
