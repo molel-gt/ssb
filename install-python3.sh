d7f464774d76c16ba4778ff833ef02bb4338365b
Leshinka Molel <emolel3@gatech.edu>
2024-12-15 20:56:30 -0500

    install python3

--- install-python3.sh
+++ install-python3.sh
@@ -6,8 +6,8 @@ if [ -d $SOFTWARES_DIR/Python-3.12.3 ]; then
     echo 'directory exists, not downloading'
 else
     wget https://www.python.org/ftp/python/3.12.3/Python-3.12.3.tgz
+    tar xvf Python-3.12.3.tgz
 fi
-tar xvf Python-3.12.3.tgz
 
 ./configure --prefix=$CMAKE_INSTALL_PREFIX
 make -j && make install
