37d3c5a35f51684d3aeccd173c2a74f069bc593f
Leshinka Molel <emolel3@gatech.edu>
2024-12-18 04:53:33 -0500

    update

--- install-petsc.sh
+++ install-petsc.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+if [ -d $SOFTWARES_DIR/petsc ]; then
+    echo 'Directory exists, not cloning'
+else
+    git clone https://github.com/molel-gt/petsc.git
+fi
 cd $SOFTWARES_DIR/petsc
+git checkout vary-restart
+cp src/ksp/ksp/impls/gmres/fgmres/fgmres.c src/ksp/ksp/impls/gmres/fgmres/fgmres-new.c
+git checkout v3.22.2
+cp src/ksp/ksp/impls/gmres/fgmres/fgmres-new.c src/ksp/ksp/impls/gmres/fgmres/fgmres.c
+
 ./configure --with-clean --download-f2cblaslapack --with-mpi-dir=$OPENMPI_DIR --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-ptscotch --download-eigen --download-hdf5 PETSC_ARCH=real-int32 --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-mumps --download-scalapack --with-log=1 --with-cuda=0 --use-gpu-aware-mpi=0  --download-strumpack # --download-netcdf --download-zlib --download-ml --download-suitesparse --download-spai --download-spooles --download-zfp --download-butterflypack
 make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH all
 make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH check
