e26140249c938cd971e28effc43100b34264242c
Leshinka Molel <emolel3@gatech.edu>
2024-12-18 18:15:17 -0500

    install fenics

--- install-fenicsx.sh
+++ install-fenicsx.sh
@@ -17,12 +17,12 @@ if [ -d "$DOLFINX_BUILD_DIR" ]; then rm -Rf $DOLFINX_BUILD_DIR; fi
 mkdir $DOLFINX_BUILD_DIR
 
 cd $DOLFINX_BUILD_DIR
-CC=$CC CPP=$CPP CXX=$CXX FC=$FC cmake .. -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
+cmake .. -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
 make && make install
 dolfinxconf=$(find $CMAKE_INSTALL_PREFIX/ -name dolfinx.conf -print)
 . $dolfinxconf
 
 cd $SOFTWARES_DIR/dolfinx/python
 
-CC=$CC CPP=$CPP CXX=$CXX FC=$FC $PYTHON3_DIR/bin/python3 -m pip install -r build-requirements.txt
-CC=$CC CPP=$CPP CXX=$CXX FC=$FC $PYTHON3_DIR/bin/python3 -m pip install --check-build-dependencies --no-build-isolation .
+$PYTHON3_DIR/bin/python3 -m pip install -r build-requirements.txt
+$PYTHON3_DIR/bin/python3 -m pip install --check-build-dependencies --no-build-isolation .
