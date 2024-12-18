837edacb0d4de6eeb46b2281bd515136cedec422
Leshinka Molel <emolel3@gatech.edu>
2024-12-18 17:29:56 -0500

    update

--- setup-fea-env.sh
+++ setup-fea-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # sh install-autoconf.sh
-sh install-openmpi.sh
-sh install-pkgconf.sh
+# sh install-openmpi.sh
+# sh install-pkgconf.sh
 sh install-pugixml.sh
 sh install-spdlog.sh
 sh install-adios2.sh
@@ -11,7 +11,7 @@ sh install-slepc.sh
 sh install-basix.sh
 sh install-ufl.sh
 sh install-ffcx.sh
-sh install-hdf5.sh
+# sh install-hdf5.sh
 sh install-fenicsx.sh
 
 $PYTHON3_DIR/bin/python3 -m pip install gmsh scipy matplotlib
