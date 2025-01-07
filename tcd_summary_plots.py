b513eb5a7798a3b7c6152b83b1f1670f64fe3a3d
Leshinka Molel <emolel3@gatech.edu>
2025-01-06 23:06:29 -0500

    filter

--- tcd_summary_plots.py
+++ tcd_summary_plots.py
@@ -77,6 +77,7 @@ if __name__ == '__main__':
     plt.savefig(solve_time_dofs_plots, bbox_inches='tight')
 
     strong_scaling_df = df[np.logical_and(np.isclose(df['dofs'], 3193836, atol=1e4), np.isclose(df['Positive Wa'], 1))]
+    strong_scaling_df = strong_scaling_df[np.isclose(strong_scaling_df['penalty parameter (gamma)'], 1e-4)]
     fig, ax = plt.subplots()
     solve_time_1proc = strong_scaling_df[np.isclose(strong_scaling_df['n_procs'], 1)]["solve time [s]"]
     ax.plot(strong_scaling_df['n_procs'], solve_time_1proc/strong_scaling_df['solve time [s]'], 'kx-', label='Actual')
