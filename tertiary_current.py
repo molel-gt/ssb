87b136aad803d1952afcb995c084ffebbac7302e
Leshinka Molel <emolel3@gatech.edu>
2025-01-06 19:12:48 -0500

    precond mat

--- tertiary_current.py
+++ tertiary_current.py
@@ -516,12 +516,12 @@ if __name__ == '__main__':
     # index sets
     IS_u0 = PETSc.IS().createGeneral(np.array(local_dofs_u0, dtype=np.int32), comm=comm).sort()
     IS_u1 = PETSc.IS().createGeneral(np.array(local_dofs_u1, dtype=np.int32), comm=comm).sort()
-    IS_u = IS_u0.sum(IS_u1)#PETSc.IS().createGeneral(np.array(local_dofs_u, dtype=np.int32), comm=comm).sort()
+    IS_u = IS_u0.sum(IS_u1)
     IS_c = PETSc.IS().createGeneral(np.array(local_dofs_c, dtype=np.int32), comm=comm).sort()
     t = 0
     cvtx = io.VTXWriter(comm, concentration_file, [c], engine="BP5")
     PETSc.Sys.Print(f"Setting up problem Wa: {args.Wa_p}, Kr: {args.kr}, #DoFs: {n_dofs:,}, nprocs: {comm.Get_size()}")
-    P = [[J00, J01, J02], [None, J11, J12], [None, None, J22]]
+    P = J  # [[J00, J01, J02], [None, J11, J12], [None, None, J22]]
 
     # log_viewer = PETSc.Viewer().STDOUT()
     # log_viewer.setFileName(log_datafile)
