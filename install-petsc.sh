a0c2a243c952d26cea18994fbec2ca0a5feb48f7
Leshinka Molel <emolel3@gatech.edu>
2025-01-07 15:55:51 -0500

    update installs

--- install-petsc.sh
+++ install-petsc.sh
@@ -14,7 +14,7 @@ fi
 
 cd $SOFTWARES_DIR/petsc
 
-./configure --with-clean --download-f2cblaslapack --with-mpi-dir=$OPENMPI_DIR --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-ptscotch --download-eigen --download-hdf5 PETSC_ARCH=real-int32 --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-mumps --download-scalapack --with-log=1 --with-cuda=0 --use-gpu-aware-mpi=0  --download-strumpack # --download-netcdf --download-zlib --download-ml --download-suitesparse --download-spai --download-spooles --download-zfp --download-butterflypack
+./configure --with-clean --download-f2cblaslapack --with-mpi-dir=$OPENMPI_DIR --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-ptscotch --download-eigen --download-hdf5 PETSC_ARCH=real-int32 --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-mumps --download-scalapack --with-log=1 --with-cuda=0 --use-gpu-aware-mpi=0  --download-strumpack --download-netcdf --download-zlib --download-ml --download-suitesparse --download-spai --download-spooles --download-zfp --download-butterflypack
 make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH all
 make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH check
 $PYTHON3_DIR/bin/python3 -m pip install src/binding/petsc4py
