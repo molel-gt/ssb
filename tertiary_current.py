09043d833f94e79987f0570e2d4980cd8c6aa3d9
Leshinka Molel <emolel3@gatech.edu>
2025-02-20 02:05:39 -0500

    fix interior penalty formulation for potential

--- tertiary_current.py
+++ tertiary_current.py
@@ -473,28 +473,28 @@ if __name__ == '__main__':
 
     F_0 = (
         - 0.5 * mixed_term(kappa_elec * u_l + kappa_pos_am * u_r, v_l, n_l) * dInterface
-        - 0.5 * mixed_term(0.5 * (kappa_elec + kappa_pos_am) * v_l, (u_r - u_l - jump_u), n_l) * dInterface
+        - 0.5 * mixed_term(kappa_elec * v_l, (u_r - u_l - jump_u), n_l) * dInterface
     )
 
     F_1 = (
         + 0.5 * mixed_term(kappa_elec * u_l + kappa_pos_am * u_r, v_r, n_l) * dInterface
-        - 0.5 * mixed_term(0.5 * (kappa_elec + kappa_pos_am) * v_r, (u_r - u_l - jump_u), n_l) * dInterface
+        - 0.5 * mixed_term(kappa_pos_am * v_r, (u_r - u_l - jump_u), n_l) * dInterface
     )
-    F_0 += -2 * gamma / (h_l + h_r) * (u_r - u_l - jump_u) * v_l * dInterface
-    F_1 += +2 * gamma / (h_l + h_r) * (u_r - u_l - jump_u) * v_r * dInterface
+    F_0 += -2 * gamma / (h_l + h_r) * 0.5 * (kappa_elec + kappa_pos_am) * (u_r - u_l - jump_u) * v_l * dInterface
+    F_1 += +2 * gamma / (h_l + h_r) * 0.5 * (kappa_elec + kappa_pos_am) * (u_r - u_l - jump_u) * v_r * dInterface
 
     if args.cycle_mode == galvanostatic:
         F_1 += - v_1 * lmbda * ds_f(3)
         F_1a = (V_cell - u_1) * mu * ds_f(3)
-        F_1b = w * (I_tot/(A_right_tilde * L_ref * phi_ref) + lmbda) * ds_f(3) - 1e-8/h * (V_cell - u_1) * w * ds_f(3)
+        F_1b = w * (I_tot/(A_right_tilde * L_ref * phi_ref) + lmbda) * ds_f(3) #- 1e-6/h * V_cell * w * ds_f(3)
 
     F_0 += F_00
     F_1 += F_11
 
     F_2 = (c - c0)/dt * q * dx_r + inner(ufl.grad(c), ufl.grad(q)) * dx_r
     # F_2 += -inner(kappa_pos_am * phi_ref/(D * faraday_const * c_ref) * grad(u_r), n_r) * q_r * dInterface
-    F_2 += -inner(1/2*grad(args.kr * u_l + u_r), n_r) * q_r * dInterface
-    F_2 += alpha * h_r * inner(inner(-1/2*grad(args.kr * u_l + u_r) + grad(c_r), n_r), inner(grad(q_r), n_r)) * dInterface
+    F_2 += -inner(1/2 * grad(args.kr * u_l + u_r), n_r) * q_r * dInterface
+    F_2 += alpha * h_r * inner(inner(-1/2 * grad(args.kr * u_l + u_r) - grad(c_r), n_r), inner(grad(q_r), n_r)) * dInterface
 
     u_left = fem.Function(V0)
     u_left.x.array[:] = 0/phi_ref
