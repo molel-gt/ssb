99f71b73492451ed7e59991857c69525fa382578
Leshinka Molel <emolel3@gatech.edu>
2025-02-26 15:01:00 -0500

    update

--- tertiary_current_geo.py
+++ tertiary_current_geo.py
@@ -38,11 +38,11 @@ if __name__ == '__main__':
     gmsh.model.add('ellipsoidals')
     gmsh.option.setNumber("Mesh.MeshSizeMax", args.resolution)
     # gmsh.option.setNumber('Geometry.ToleranceBoolean', 0.001)
-    gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 0)
     # gmsh.option.setNumber("Mesh.MinimumCirclePoints", 20)
     if args.hexahedron:
         gmsh.option.setNumber('Mesh.SubdivisionAlgorithm', 2)
     if args.min_elements_per_2pi > 0:
+        gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", args.resolution/10)
         gmsh.option.setNumber('Mesh.MinimumElementsPerTwoPi', args.min_elements_per_2pi)
     # gmsh.option.setNumber('Mesh.Algorithm', 6)
     gmsh.option.setNumber("Mesh.SmoothRatio", 10)
