50e2f0871034b6c2016bbdb219535c00aafa7f7c
Leshinka Molel <emolel3@gatech.edu>
2025-03-22 22:06:14 -0400

    fix bug

--- tertiary_current_renorm.py
+++ tertiary_current_renorm.py
@@ -1116,10 +1116,10 @@ if __name__ == '__main__':
 
         error = phi_ref * L_ref ** (-k) * (inner(kappa_elec * grad(u_l), n_l) + inner(kappa_pos_am * grad(u_r), n_r))
         i_x = inner(kappa_pos_am * grad(u_r), n_r) * phi_ref * L_ref ** (-k)
-        I_x_norm_sq = np.sqrt(comm.allreduce(fem.assemble_scalar(
+        I_x_norm = np.sqrt(comm.allreduce(fem.assemble_scalar(
                                     fem.form(inner(i_x, i_x) * L_ref ** 2 * dInterface, entity_maps=entity_maps)), op=MPI.SUM))
 
-        I_interface_error_norm_sq = np.sqrt(comm.allreduce(fem.assemble_scalar(
+        I_interface_error_norm = np.sqrt(comm.allreduce(fem.assemble_scalar(
                                     fem.form(inner(error, error) * L_ref ** 2 * dInterface, entity_maps=entity_maps)), op=MPI.SUM))
         u_avg_right_tilde = comm.allreduce(fem.assemble_scalar(fem.form(u_1 * ds(markers.right),
                                                                         entity_maps=entity_maps)), op=MPI.SUM)
@@ -1176,7 +1176,7 @@ if __name__ == '__main__':
         "I interface (electrolyte potential) [A]": I_interface_l,
         "I interface (active material potential) [A]": I_interface_r,
         "I_interface error (potential) [A]": I_interface_error,
-        "I_interface error norm (normalized)": np.sqrt(I_interface_error_norm_sq / I_x_norm_sq),
+        "I_interface error norm (normalized)": I_interface_error_norm / I_x_norm,
         "C-rate": args.C_rate,
         "u (avg) right [V]": u_avg_right,
         "u (stdev) right [v]": u_stdev_right,
