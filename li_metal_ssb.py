4e131ae2f23b9b4896457bc5eb98067c2e2bdb7a
Leshinka Molel <emolel3@gatech.edu>
2025-04-07 19:32:15 -0400

    reformulate

--- li_metal_ssb.py
+++ li_metal_ssb.py
@@ -1196,26 +1196,25 @@ if __name__ == '__main__':
         u_avg_left_tilde = comm.allreduce(fem.assemble_scalar(fem.form(u_0 * ds(markers.left),
                                                                         entity_maps=entity_maps)), op=MPI.SUM)
         u_avg_left = u_avg_left_tilde * phi_ref * L_ref ** 2 / A_left
-        u_stdev_left_tilde = comm.allreduce(fem.assemble_scalar(fem.form(
+        u_stdev_left_tilde = np.sqrt(comm.allreduce(fem.assemble_scalar(fem.form(
                                             (u_0 - u_avg_left_tilde) ** 2 * ds(markers.left),
-                                            entity_maps=entity_maps)), op=MPI.SUM)
-        u_stdev_left = np.sqrt(u_stdev_left_tilde  * (phi_ref * L_ref ** 2) ** 2 / A_left)
+                                            entity_maps=entity_maps)), op=MPI.SUM) / A_left_tilde)
+        u_stdev_left = u_stdev_left_tilde  * phi_ref * L_ref ** 2
 
         u_avg_right_tilde = comm.allreduce(fem.assemble_scalar(fem.form(u_1 * ds(markers.right),
                                                                         entity_maps=entity_maps)), op=MPI.SUM)
         c_surf_avg_tilde = comm.allreduce(fem.assemble_scalar(fem.form(c_r * dInterface,
                                                                         entity_maps=entity_maps)), op=MPI.SUM)
         c_surf_avg = c_surf_avg_tilde / A_se_am_tilde
-        c_surf_stdev_tilde = comm.allreduce(fem.assemble_scalar(fem.form(
+        c_surf_stdev = np.sqrt(comm.allreduce(fem.assemble_scalar(fem.form(
                                             (c_r - c_surf_avg_tilde) ** 2 * dInterface,
-                                            entity_maps=entity_maps)), op=MPI.SUM)
-        c_surf_stdev = np.sqrt(c_surf_stdev_tilde  * (c_ref * L_ref ** 2) ** 2 / A_se_am)
+                                            entity_maps=entity_maps)), op=MPI.SUM) / A_se_am_tilde)
 
         u_avg_right = u_avg_right_tilde * phi_ref * L_ref ** 2 / A_right
-        u_stdev_right_tilde = comm.allreduce(fem.assemble_scalar(fem.form(
+        u_stdev_right_tilde = np.sqrt(comm.allreduce(fem.assemble_scalar(fem.form(
                                             (u_1 - u_avg_right_tilde) ** 2 * ds(markers.right),
-                                            entity_maps=entity_maps)), op=MPI.SUM)
-        u_stdev_right = np.sqrt(u_stdev_right_tilde  * (phi_ref * L_ref ** 2) ** 2 / A_right)
+                                            entity_maps=entity_maps)), op=MPI.SUM) / A_right_tilde)
+        u_stdev_right = u_stdev_right_tilde  * phi_ref * L_ref ** 2
         i_avg_se_am = I_interface / A_se_am
         i_avg_left = I_left / A_left
         i_avg_right = I_right / A_right
