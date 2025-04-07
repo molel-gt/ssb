113838dfe40a39102ba9d15f5468e5623f231a49
Leshinka Molel <emolel3@gatech.edu>
2025-04-07 18:31:53 -0400

    Update OCV to account for concentration limits

--- li_metal_ssb.py
+++ li_metal_ssb.py
@@ -127,7 +127,9 @@ def arctanh(y):
 
 def ocv_chen2020(c, cmax):
     return  4.4875 - 0.8090 * c/cmax - 0.0428 * ufl.tanh(18.5138*(c/cmax - 0.5542)) +\
-    -17.7326 * ufl.tanh(15.7890*(c/cmax - 0.3117)) + 17.5842 * ufl.tanh(15.9308*(c/cmax - 0.3120))
+    -17.7326 * ufl.tanh(15.7890*(c/cmax - 0.3117)) + 17.5842 * ufl.tanh(15.9308*(c/cmax - 0.3120)) +\
+         ufl.conditional(ufl.gt(c/c_max, 1.0), -10.0, 0) +\
+         ufl.conditional(ufl.lt(c/c_max, 0.0), 10.0, 0)
 
 
 def get_Lref(dimensions, transport_direction):
