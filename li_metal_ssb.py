a72afe6557691bcf429fd0d5bcd0c61a1f71a787
Leshinka Molel <emolel3@gatech.edu>
2025-04-23 18:48:49 -0400

    remove unused OCP function

--- li_metal_ssb.py
+++ li_metal_ssb.py
@@ -138,18 +138,6 @@ def U_ocp(c, cmax=1.0, phi_ref=V_MAX):
          )
 
 
-# def U_ocp(c, cmax, phi_ref=V_MAX):
-#     """
-#     """
-#     return  1 / phi_ref * ( ufl.conditional(ufl.And(ufl.gt(1 - c/cmax, 0), ufl.lt(1-c/cmax, 0.5)),
-#                                             2 * (1-c/cmax) + 2.5, 0)+\
-#     ufl.conditional(ufl.And(ufl.ge(1-c/cmax, 0.5), ufl.le(1-c/cmax, 0.75)), 3.5, 0 )+\
-#     ufl.conditional(ufl.And(ufl.gt(1-c/cmax, 0.75), ufl.lt(1-c/cmax, 1.0)), 4 * (1-c/cmax) + 0.5, 0)+\
-#          ufl.conditional(ufl.ge(1-c/c_max, 1.0), 0.0, 0) +\
-#          ufl.conditional(ufl.lt(1-c/c_max, 0.0), V_MAX, 0)
-#          )
-
-
 def get_Lref(dimensions, transport_direction):
     direction = directions[transport_direction.lower()]
 
