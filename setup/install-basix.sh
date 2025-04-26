0538766b838ddcd82bf8a0a7ba8c0b8440c8f361
Leshinka Molel <emolel3@gatech.edu>
2025-04-26 17:55:11 -0400

    update fenicsx

--- setup/install-basix.sh
+++ setup/install-basix.sh
@@ -7,12 +7,14 @@ BASIX_DIR=$SOFTWARES_DIR/basix
 . $PYTHON3_DIR/bin/activate
 
 if [ -d "$BASIX_DIR" ]; then
+    cd $BASIX_DIR && git stash && git pull origin
     echo "Skip cloning, directory exists"
 else
     git clone https://github.com/FEniCS/basix.git
 fi
 
 cd $BASIX_DIR/cpp
+rm -r build
 mkdir build
 cd $BASIX_DIR/cpp/build
 cmake .. -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
--- setup/install-ffcx.sh
+++ setup/install-ffcx.sh
@@ -7,13 +7,14 @@ FFCX_DIR=$SOFTWARES_DIR/ffcx
 . $PYTHON3_DIR/bin/activate
 
 if [ -d "$FFCX_DIR" ]; then
+    cd $FFCX_DIR && git stash && git pull origin
     echo "Skip cloning, directory exists"
 else
     git clone https://github.com/FEniCS/ffcx.git
 fi
 
 cd $FFCX_DIR
-
+rm -r build
 mkdir -p build
 cd $FFCX_DIR/build
 cmake ../cmake -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
