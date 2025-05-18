616d4b01524c91c7e37509447a8515dcfc0e6aa0
Leshinka Molel <emolel3@gatech.edu>
2025-05-18 13:26:03 -0400

    visualizing polygons

--- detailed_microstructure.py
+++ detailed_microstructure.py
@@ -3,36 +3,58 @@ import argparse
 import json
 import os
 
+import bezier
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
+import open3d as o3d
+import shapely
+
 import skimage as ski
+# from matplotlib.patches import Polygon
+from PIL import Image
+from shapely import plotting
 from shapely.geometry import Polygon, Point
 from skimage.measure import find_contours, approximate_polygon, subdivide_polygon
 
-import grapher, plot_opts, utils
-plt.rcParams.update(plot_opts.params)
+import geometry, grapher, plot_opts, utils
+# plt.rcParams.update(plot_opts.params)
 
 
 data_folder = os.path.join(os.environ["HOME"], "OneDrive/PhD/Data/SEM Image/segmentation/")
-fig, ax = plt.subplots()
+# fig, ax = plt.subplots()
 
 
 class Segmentor:
-    def __init__(self, img_id, root_folder=data_folder):
+    def __init__(self, img_id, ax, fig, root_folder=data_folder):
         self._img_id = img_id
         self._root_folder = data_folder
         self._img_raw = None
         self._cam_contours = []
         self._sse_contours = []
+        self._current_phase = 1
+        self._fig = fig
+        self._ax = ax
 
     @property
     def img_id(self):
         return self._img_id
 
     @property
+    def ax(self):
+        return self._ax
+
+    @property
+    def fig(self):
+        return self._fig
+
+    @property
     def root_folder(self):
         return self._root_folder
+
+    @property
+    def current_phase(self):
+        return self._current_phase
 
     @property
     def img_raw(self):
@@ -47,26 +69,28 @@ class Segmentor:
         return self._sse_contours
 
     def setup(self):
-        self._img_raw = plt.imread(os.path.join(self.root_folder, f"{str(self.img_id).zfill(3)}.tif"))
+        self._img_raw = plt.imread(os.path.join(self.root_folder, "raw", f"{str(self.img_id).zfill(3)}.tif"))
 
         return
 
     def get_contours(self):
-        min_val = np.max(self.img_raw)
+        min_val = 125#np.max(self.img_raw)
         img_file_path = os.path.join(self.root_folder, "edges", f"{str(self.img_id).zfill(3)}.tif")
+
+        loops_img_file_path = os.path.join(self.root_folder, "loops", f"{str(self.img_id).zfill(3)}.tif")
         data = np.asarray(Image.open(img_file_path)).copy()
         img = data[:, :, 0]
         nx, ny = img.shape
         # pad boundaries
-        img[:, 0] = 255
-        img[:, -1] = 255
-        img[0, :] = 255
-        img[-1, :] = 255
+        # img[:, 0] = 255
+        # img[:, -1] = 255
+        # img[0, :] = 255
+        # img[-1, :] = 255
         curves = np.where(np.greater_equal(img, min_val))
-        # curves, nx, ny = get_curves("060.tif", 255)
-        img_1 = np.zeros((nx, ny))
+        img_1 = np.zeros((nx, ny), dtype=np.uint8)
         img_1[curves] = 255
-        contours = ski.measure.find_contours(img_1, 0.8)
+        # img_1 = np.greater_equal(data[:, :, 0], data[:, :, 3])
+        contours = ski.measure.find_contours(img_1, 0)#, fully_connected="high")#, "low", "low")
         contours = sorted(contours, key=len, reverse=True)
 
         return contours
@@ -82,25 +106,19 @@ class Segmentor:
 
         return
 
-
-def get_curves(img_file_path, min_val):
-    data = np.asarray(Image.open(img_file_path)).copy()
-    img = data[:, :, 0]
-    nx, ny = img.shape
-    # pad boundaries
-    img[:, 0] = 255
-    img[:, -1] = 255
-    img[0, :] = 255
-    img[-1, :] = 255
-    curves = np.where(np.greater_equal(img, min_val))
-
-    return curves, nx, ny
+    def on_press(self, event):
+        print(event.key)
+        if event.key == 'shift+right':
+            self._current_phase = 2
+        if event.key == 'shift+left':
+            self._current_phase = 1
+        self.fig.canvas.draw_idle()
 
 
 def extract_voids_polygons(img):
     img = img - np.min(img)
     img_1 = ski.filters.gaussian(img, sigma=1.0) < 0.125
-    contours = ski.measure.find_contours(img_1, 0.8)
+    contours = ski.measure.find_contours(img_1, 0)
     contours = sorted(contours, key=len, reverse=True)
 
     return contours
@@ -140,87 +158,30 @@ def write_voids_polygon(voids_dir, img_id):
     return
 
 
-def on_press(event):
-    print(event.key)
-    if event.key == 'shift+right':
-        ax.azim+=10
-    if event.key == 'shift+left':
-        ax.azim-=10
-    fig.canvas.draw_idle()
-
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='secondary current distribution')
-    parser.add_argument('--img_id', help='`image index` to process', required=True)
-    parser.add_argument("--extract_voids", help="whether to extract voids", default=False, action=argparse.BooleanOptionalAction)
+    parser = argparse.ArgumentParser(description='segmentation')
+    parser.add_argument('--img_id', help='id number of image to process', required=True)
     args = parser.parse_args()
-    voids_dir = os.path.join(os.environ["HOME"], "OneDrive/PhD/Data/SEM Image/segmentation/voids")
-    cam_dir = os.path.join(os.environ["HOME"], "OneDrive/PhD/Data/SEM Image/segmentation/cam")
-    sse_dir = os.path.join(os.environ["HOME"], "OneDrive/PhD/Data/SEM Image/segmentation/sse")
-    raw_dir = os.path.join(os.environ["HOME"], "OneDrive/PhD/Data/SEM Image/segmentation/raw")
-    if args.extract_voids:
-        write_voids_polygon(voids_dir, args.img_id)
-    curves, nx, ny = get_curves("060.tif", 255)
-    img_1 = np.zeros((nx, ny))
-    img_1[curves] = 255
-    # img_2 = Image.fromarray(img_1)
-    # img_2.save("loops.tif")
-
-    # Find contours at a constant value of 0.8
-    contours = ski.measure.find_contours(img_1, 0.8)
-    contours = sorted(contours, key=len, reverse=True)
-    print(f"There are {len(contours)} loops")
-    img_id = 60
-    img_raw = plt.imread(os.path.join(raw_dir, f"{str(img_id).zfill(3)}.tif"))
-    sse_polys = []
-    cam_polys = []
-
-    # # Display the image and plot all contours found
-    ax.imshow(img_raw, "gray")
-    for contour in contours:
-        if contour.shape[0] < 4:
+    fig, ax = plt.subplots()
+    segmentor = Segmentor(img_id=args.img_id, ax=ax, fig=fig)
+    segmentor.setup()
+    for contour in segmentor.get_contours()[1:]:
+        coords = ski.measure.approximate_polygon(contour, tolerance=1)
+        # coords = np.vstack((coords, coords[0, :]))
+        if len(coords) < 4:
             continue
-        ax.scatter(contour[:, 1], contour[:, 0], s=0.5, color="red")
-        fig.canvas.draw_idle()
-
+        polygon = Polygon([(c[1], c[0]) for c in coords])
+        if np.isclose(polygon.area, 0):
+            continue
+        if polygon.area < 25:
+            continue
+        hull_cc = shapely.concave_hull(polygon, ratio=0.1)
+        hull_cv = shapely.convex_hull(polygon)
+        plotting.plot_polygon(polygon, ax=ax, linewidth=0.5, add_points=False, facecolor="green", edgecolor="red")
+        # nodes = np.asfortranarray([coords[:, 1].tolist(), coords[:, 0].tolist()])
+        # curve = bezier.Curve(nodes)
+    ax.set_xlim([0, segmentor.img_raw.shape[0]])
+    ax.set_ylim([0, segmentor.img_raw.shape[1]])
+    ax.set_box_aspect(1)
+    plt.tight_layout()
     plt.show()
-
-    # ax.imshow(img_1, cmap=plt.cm.gray)
-    # count = 0
-    # # contour_sizes = [c.shape[0] for c in contours]
-    # # perm = np.argsort(contour_sizes)
-    # # perm.astype(int)
-    # # print(perm)
-    # contours = sorted(contours, key=len, reverse=True)
-    # for contour in contours:
-    #     print(contour.shape[0])
-    #     # if contour.shape[0] <= 20:
-    #     #       continue
-    #     ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
-    #     count += 1
-    # print(f"There are {count} big loops")
-
-    # ax.axis('image')
-    # ax.set_xticks([])
-    # ax.set_yticks([])
-    # plt.show()
-
-
-    # points = {}
-    # for (x, y) in curves:
-    #     idx = ny * x + y
-    #     points[idx] = (x, y) #=  idx
-    # print(f"{len(points.keys())}")
-    # fig, ax = plt.subplots()
-    # ax.imshow(img, "gray")
-    # # ax.scatter(curves[:, 0], curves[:, 1], s=0.1)
-    # ax.set_box_aspect(1)
-    # plt.tight_layout()
-    # plt.show()
-    # graph = grapher.PixelGraph(points)
-    # graph.build_graph()
-    # graph.get_graph_pieces()
-    # print(f"Graph has {graph.n_pieces} pieces")
-    # print(len(graph.pieces[-2]))
-    # for p in graph.pieces[:50]:
-    #     print(len(p))
