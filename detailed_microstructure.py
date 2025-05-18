095b653ea6eba033357fda8c13f0587da34a6433
Leshinka Molel <emolel3@gatech.edu>
2025-05-18 15:50:36 -0400

    update segmentor

--- detailed_microstructure.py
+++ detailed_microstructure.py
@@ -32,7 +32,8 @@ class Segmentor:
         self._img_raw = None
         self._cam_contours = []
         self._sse_contours = []
-        self._current_phase = 1
+        self._tbd_contours = []
+        self._current_phase = 0
         self._fig = fig
         self._ax = ax
 
@@ -68,6 +69,10 @@ class Segmentor:
     def sse_contours(self):
         return self._sse_contours
 
+    @property
+    def tbd_contours(self):
+        return self._tbd_contours
+
     def setup(self):
         self._img_raw = plt.imread(os.path.join(self.root_folder, "raw", f"{str(self.img_id).zfill(3)}.tif"))
 
@@ -98,20 +103,26 @@ class Segmentor:
     def write_polygons_to_file(self):
         cam_polygons_path = os.path.join(self.root_folder, "cam", f"{str(self.img_id).zfill(3)}.json")
         sse_polygons_path = os.path.join(self.root_folder, "sse", f"{str(self.img_id).zfill(3)}.json")
+        tbd_polygons_path = os.path.join(self.root_folder, "tbd", f"{str(self.img_id).zfill(3)}.json")
         with open(cam_polygons_path, "w", encoding='utf-8') as f:
             json.dump(self.cam_contours, f, ensure_ascii=False, indent=4)
 
         with open(sse_polygons_path, "w", encoding='utf-8') as f:
             json.dump(self.sse_contours, f, ensure_ascii=False, indent=4)
 
+        with open(tbd_polygons_path, "w", encoding='utf-8') as f:
+            json.dump(self.tbd_contours, f, ensure_ascii=False, indent=4)
+
         return
 
     def on_press(self, event):
         print(event.key)
-        if event.key == 'shift+right':
+        if event.key == '2':
             self._current_phase = 2
-        if event.key == 'shift+left':
+        elif event.key == '1':
             self._current_phase = 1
+        else:
+            self._current_phase = 0
         self.fig.canvas.draw_idle()
 
 
@@ -161,10 +172,28 @@ def write_voids_polygon(voids_dir, img_id):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='segmentation')
     parser.add_argument('--img_id', help='id number of image to process', required=True)
+    parser.add_argument("--extract_voids", help="whether to extract voids", default=False, action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
+    voids_dir = os.path.join(os.environ["HOME"], "OneDrive/PhD/Data/SEM Image/segmentation/voids")
+    cam_dir = os.path.join(os.environ["HOME"], "OneDrive/PhD/Data/SEM Image/segmentation/cam")
+    sse_dir = os.path.join(os.environ["HOME"], "OneDrive/PhD/Data/SEM Image/segmentation/sse")
+    raw_dir = os.path.join(os.environ["HOME"], "OneDrive/PhD/Data/SEM Image/segmentation/raw")
+    if args.extract_voids:
+        write_voids_polygon(voids_dir, args.img_id)
     fig, ax = plt.subplots()
     segmentor = Segmentor(img_id=args.img_id, ax=ax, fig=fig)
     segmentor.setup()
+    ax.imshow(segmentor.img_raw, "gray")
+    fig.canvas.mpl_connect('key_press_event', segmentor.on_press)
+    ax.set_xlim([0, segmentor.img_raw.shape[0]])
+    ax.set_ylim([0, segmentor.img_raw.shape[1]])
+    ax.set_box_aspect(1)
+    fig.canvas.manager.resize(*fig.canvas.manager.window.maxsize())
+    poly_plot = None
+    cam_coords = []
+    sse_coords = []
+    pending_coords = []
+    wrote_data = False
     for contour in segmentor.get_contours()[1:]:
         coords = ski.measure.approximate_polygon(contour, tolerance=1)
         # coords = np.vstack((coords, coords[0, :]))
@@ -173,15 +202,32 @@ if __name__ == '__main__':
         polygon = Polygon([(c[1], c[0]) for c in coords])
         if np.isclose(polygon.area, 0):
             continue
-        if polygon.area < 25:
+        if polygon.area < 100:
             continue
         hull_cc = shapely.concave_hull(polygon, ratio=0.1)
         hull_cv = shapely.convex_hull(polygon)
-        plotting.plot_polygon(polygon, ax=ax, linewidth=0.5, add_points=False, facecolor="green", edgecolor="red")
+        if poly_plot is not None:
+            poly_plot.remove()
+        coords_arr = np.array(coords)
+        # poly_plot = ax.scatter(coords[:, 1], coords[:, 0], s=0.5, linestyle="-.", color="white")
+        poly_plot = plotting.plot_polygon(polygon, ax=ax, linewidth=0.5, add_points=False, facecolor=None, edgecolor="red")
+        keypressed = False
+        while not keypressed:
+            keypressed = plt.waitforbuttonpress()
+        if segmentor.current_phase == 1:
+            segmentor.sse_contours.append(coords.tolist())
+        elif segmentor.current_phase == 2:
+            segmentor.cam_contours.append(coords.tolist())
+        else:
+            segmentor.tbd_contours.append(coords.tolist())
+    segmentor.write_polygons_to_file()
+    wrote_data = True
+    segmentor.cam_contours.clear()
+    segmentor.sse_contours.clear()
+    segmentor.tbd_contours.clear()
         # nodes = np.asfortranarray([coords[:, 1].tolist(), coords[:, 0].tolist()])
         # curve = bezier.Curve(nodes)
-    ax.set_xlim([0, segmentor.img_raw.shape[0]])
-    ax.set_ylim([0, segmentor.img_raw.shape[1]])
-    ax.set_box_aspect(1)
     plt.tight_layout()
     plt.show()
+    if wrote_data:
+        quit()
