218c2d98d89772bd66adf528c810d1d5ebf07352
Leshinka Molel <emolel3@gatech.edu>
2025-05-21 19:50:42 -0400

    geo box

---
+++ geo_box.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import gmsh
+Lx = 10
+Ly = 10
+Lz = 5
+coords = [
+    (0, 0, 0),
+    (Lx, 0, 0),
+    (Lx, Ly, 0),
+    (0, Ly, 0),
+    (0, 0, Lz),
+    (Lx, 0, Lz),
+    (Lx, Ly, Lz),
+    (0, Ly, Lz),
+]
+gmsh.initialize()
+points = [gmsh.model.geo.addPoint(*p) for p in coords]
+lines = [gmsh.model.geo.addLine(points[i], points[i+1]) for i in range(4-1)]
+lines.append(gmsh.model.geo.addLine(points[3], points[0])) # line 4
+lines.extend([gmsh.model.geo.addLine(points[i], points[i+1]) for i in range(4, 7)])
+lines.append(gmsh.model.geo.addLine(points[7], points[4])) # line 7
+print(len(lines))
+lines.append(gmsh.model.geo.addLine(points[3], points[7])) # line 8
+lines.append(gmsh.model.geo.addLine(points[4], points[0])) # line 9
+lines.append(gmsh.model.geo.addLine(points[1], points[5])) # line 10
+lines.append(gmsh.model.geo.addLine(points[2], points[6])) # line 11
+loops = []
+gmsh.model.geo.synchronize()
+loops.append(gmsh.model.geo.addCurveLoop([lines[idx] for idx in [0, 1, 2, 3]], reorient=True))
+loops.append(gmsh.model.geo.addCurveLoop([lines[idx] for idx in [4, 5, 6, 7]], reorient=True))
+loops.append(gmsh.model.geo.addCurveLoop([lines[idx] for idx in [8, 7, 9, 3]], reorient=True))
+loops.append(gmsh.model.geo.addCurveLoop([lines[idx] for idx in [9, 0, 10, 4]], reorient=True))
+loops.append(gmsh.model.geo.addCurveLoop([lines[idx] for idx in [10, 5, 11, 1]], reorient=True))
+loops.append(gmsh.model.geo.addCurveLoop([lines[idx] for idx in [11, 6, 8, 2]], reorient=True))
+gmsh.model.geo.synchronize()
+surfs = [gmsh.model.geo.addPlaneSurface([loop]) for loop in loops]
+gmsh.model.geo.synchronize()
+surf_loop = gmsh.model.geo.addSurfaceLoop(surfs)
+vol = gmsh.model.geo.addVolume([surf_loop])
+gmsh.model.mesh.generate(3)
+gmsh.write("mesh.msh")
+gmsh.finalize()
