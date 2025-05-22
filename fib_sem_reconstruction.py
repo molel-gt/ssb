243dceb9df7c42ffef17e47e3f0e76e7dc0b1757
Leshinka Molel <emolel3@gatech.edu>
2025-05-22 00:51:20 -0400

    padding

--- fib_sem_reconstruction.py
+++ fib_sem_reconstruction.py
@@ -272,11 +272,12 @@ if __name__ == '__main__':
     tomo = np.zeros((Lx + 1, Ly + 1, Lz + 1), dtype=np.bool)
     tomo = tomo.astype(np.uint8)
     for img_id in range(1, Lz + 2):
-        img_cam = plt.imread(os.path.join(cam_dir, f"{str(img_id).zfill(3)}.tif"))[:Lx+1, :Ly+1]
+        img_cam = np.asarray(plt.imread(os.path.join(cam_dir, f"{str(img_id).zfill(3)}.tif")).copy()[:Lx+1, :Ly+1])
+        img_cam[:, :10] = 2
         # img_voids = plt.imread(os.path.join(voids_dir, f"{str(img_id).zfill(3)}.tif"))
         tomo[np.isclose(img_cam, 2), img_id - 1] = 1
         # padding of AM
-        tomo[:, :5, img_id - 1] = 1
+        # tomo[:, :10, img_id - 1] = 1
         # tomo[np.isclose(img_voids, 1), img_id - 1] = 0
     get_aggregates_and_write_to_file(tomo, phase=phase, data_shape=tomo.shape, workdir=workdir)
     gmsh.initialize()  # Initialize the gmsh API
