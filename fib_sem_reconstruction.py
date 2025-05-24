3b1f30c45ce8a0a23b03bf30bcab7077f6ad55ca
Leshinka Molel <emolel3@gatech.edu>
2025-05-23 22:59:14 -0400

    update

--- fib_sem_reconstruction.py
+++ fib_sem_reconstruction.py
@@ -296,11 +296,10 @@ if __name__ == '__main__':
     tomo[:, -1, :] = 0
     tomo[:, :, 0] = 0
     tomo[:, :, -1] = 0
-    # get_aggregates_and_write_to_file(tomo, phase=phase, data_shape=tomo.shape, workdir=workdir)
+    get_aggregates_and_write_to_file(tomo, phase=phase, data_shape=tomo.shape, workdir=workdir)
     gmsh.initialize()  # Initialize the gmsh API
     gmsh.option.setNumber("Mesh.Smoothing", 10)
 
-    #gmsh.option.setNumber("Mesh.forceReparametrizablePatches", 1)
     phase_volumes, agg_surf_loop_list, surfaces_to_combine = create_volumes_from_stl(phase=phase, workdir=workdir)
     gmsh.model.geo.synchronize()
     gmsh.option.setNumber('Geometry.Tolerance', 1e-4)
