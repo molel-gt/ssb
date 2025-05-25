57df6bb8145702858e9366d9729f3e24041357f5
Leshinka Molel <emolel3@gatech.edu>
2025-05-25 14:30:19 -0400

    update

--- mesher.py
+++ mesher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import math
 import os
 import sys
 
@@ -124,6 +125,30 @@ if __name__ == '__main__':
     Lz = (LZ+5) * SCALING[2]
     gmsh.initialize()
     gmsh.model.add("fib_sem")
+    gmsh.onelab.set("""
+        [
+          {
+            "type":"number",
+            "name":"Parameters/Angle for surface detection",
+            "values":[40],
+            "min":20,
+            "max":120,
+            "step":1
+          },
+          {
+            "type":"number",
+            "name":"Parameters/Create surfaces guaranteed to be parametrizable",
+            "values":[0],
+            "choices":[0, 1]
+          },
+          {
+            "type":"number",
+            "name":"Parameters/Apply funny mesh size field?",
+            "values":[0],
+            "choices":[0, 1]
+          }
+          ]
+    """)
     # gmsh.option.setNumber("Mesh.Algorithm", 6)
     # gmsh.option.setNumber("Mesh.CharacteristicLengthMin", 0.1)
     # gmsh.option.setNumber("Mesh.CharacteristicLengthMax", 1)
@@ -132,18 +157,21 @@ if __name__ == '__main__':
     # matrix_volume = gmsh.model.geo.addVolume([sloop] + agg_surf_loop_list)
     # gmsh.model.geo.synchronize()
     # gmsh.model.geo.synchronize()
-    for idx in range(1, 2):
+    for idx in range(1, 3):
         gmsh.merge(f"output/segmentation/cam/201-201-201/0-0-0/aggs/agg_{idx}.ply")
     # gmsh.model.geo.synchronize()
     # gmsh.model.mesh.createTopology()
     vols = gmsh.model.getEntities(3)
     # gmsh.option.setNumber('Geometry.Tolerance', 1e-12)
     # gmsh.option.setNumber('Mesh.Optimize', 1)
-    gmsh.option.setNumber('Mesh.Algorithm', 5)
+    # gmsh.option.setNumber('Mesh.Algorithm', 5)
     # gmsh.model.mesh.removeDuplicateNodes()
-    angle = 180/180. * np.pi
-    curveAngle = 1.0 * np.pi
-    gmsh.model.mesh.classifySurfaces(angle, True, True, curveAngle)
+    # angle = 180/180. * np.pi
+    angle = gmsh.onelab.getNumber('Parameters/Angle for surface detection')[0]
+    forceParametrizablePatches = gmsh.onelab.getNumber(
+        'Parameters/Create surfaces guaranteed to be parametrizable')[0]
+    curveAngle = 180#1.0 * np.pi
+    gmsh.model.mesh.classifySurfaces(angle * math.pi/180., True, forceParametrizablePatches, curveAngle * math.pi/180.)
     # gmsh.model.mesh.createGeometry()
     surfaces_adjacencies = []
 
@@ -178,17 +206,17 @@ if __name__ == '__main__':
         lxs.extend([xmin, xmax])
         lys.extend([ymin, ymax])
         lzs.extend([zmin, zmax])
-    tol = 1e-8
+    tol = 1e-9
     lx = np.max(lxs) + tol
     ly = np.max(lys) + tol
     lz = np.max(lzs) + tol
     # gmsh.model.geo.remove([(3, agg_last_idx+1)])
     # gmsh.model.geo.dilate(gmsh.model.getEntities(0)+gmsh.model.getEntities(1)+gmsh.model.getEntities(2)+gmsh.model.getEntities(3), 1, 1, 1, 0.0858e-6, 0.0858e-6, 0.05e-6)
     # gmsh.model.geo.synchronize()
-    # gmsh.model.geo.removeAllDuplicates()
+    gmsh.model.geo.removeAllDuplicates()
     # gmsh.model.geo.synchronize()
     surf_loops = []
-    sloop = create_box_surface_loop(Lx=lx, Ly=ly, Lz=lz, L_sep=L_sep, origin=(np.min(lxs) - tol, np.min(lys) - tol, np.min(lzs) - tol))
+    sloop = create_box_surface_loop(Lx=lx, Ly=ly, Lz=lz, L_sep=L_sep, origin=(np.min(lxs), np.min(lys), np.min(lzs)))
     gmsh.model.geo.synchronize()
     bndry = [s[1] for s in gmsh.model.getBoundary([(3, v) for v in phase_volumes])]
     # hole = gmsh.model.geo.addSurfaceLoop(bndry)
@@ -214,28 +242,22 @@ if __name__ == '__main__':
     lzs = []
     for surf in surfs:
         xmin, ymin, zmin, xmax, ymax, zmax = gmsh.model.get_bounding_box(*surf)
-        lxs.extend([xmin, xmax])
-        lys.extend([ymin, ymax])
-        lzs.extend([zmin, zmax])
         if np.isclose(ymin, ymax) and np.isclose(ymin, 0):
             right_surfs.append(surf[1])
-        elif np.isclose(ymin, ymax) and np.isclose(ymin, Ly + args.L_sep):
+        elif np.isclose(ymin, ymax) and np.isclose(ymin, ly + L_sep):
             left_surfs.append(surf[1])
-        elif np.isclose(xmin, xmax) and (np.isclose(xmin, 0) or np.isclose(xmin, Lx)):
+        elif np.isclose(xmin, xmax) and (np.isclose(xmin, 0) or np.isclose(xmin, lx)):
             insulated_am.append(surf[1])
-        elif np.isclose(zmin, zmax) and (np.isclose(zmin, 0) or np.isclose(zmin, Lz)):
+        elif np.isclose(zmin, zmax) and (np.isclose(zmin, 0) or np.isclose(zmin, lz)):
             insulated_am.append(surf[1])
         else:
             # if surf[1] in agg_surf_loop_list:
             interface_surfs.append(surf[1])
             # else:
             #     print(surf[1])
-    print(np.min(lxs), np.max(lxs), Lx)
-    print(np.min(lys), np.max(lys), Ly)
-    print(np.min(lzs), np.max(lzs), Lz)
-    # gmsh.model.addPhysicalGroup(2, left_surfs, markers.left, "Left")
-    # gmsh.model.addPhysicalGroup(2, right_surfs, markers.right, "Right")
-    # gmsh.model.addPhysicalGroup(2, interface_surfs, markers.electrolyte_v_positive_am, "SE/AM")
+    gmsh.model.addPhysicalGroup(2, left_surfs, markers.left, "Left")
+    gmsh.model.addPhysicalGroup(2, right_surfs, markers.right, "Right")
+    gmsh.model.addPhysicalGroup(2, interface_surfs, markers.electrolyte_v_positive_am, "SE/AM")
 
     gmsh.model.geo.synchronize()
 
