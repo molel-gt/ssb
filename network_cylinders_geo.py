79ec9322785c214d324072981d6508d4b644bb2b
Leshinka Molel <emolel3@gatech.edu>
2025-05-29 15:15:01 -0400

    network cylinders

---
+++ network_cylinders_geo.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import random
+
+import gmsh
+import numpy as np
+
+import commons, utils
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Estimates Effective Conductivity.')
+    parser.add_argument("-n", "--name_of_study", help="name_of_study", nargs='?', const=1, default="network_cylinders")
+    parser.add_argument("-d", '--dimensions', help='integer representation of Lx-Ly-Lz of the grid',  nargs='?', const=1, default='20-20-80')
+    parser.add_argument("-r", '--resolution', help=f'max resolution (units of microns)', nargs='?', const=1, default=1, type=float)
+    parser.add_argument("-f", "--refine", help="compute current distribution stats", default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument("-hexahedron", "--hexahedron", help="compute current distribution stats", default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument("-format", "--format", help="Mesh format", default="msh", nargs='?', const=1)
+    parser.add_argument("-min_elements_per_2pi", "--min_elements_per_2pi", help="minimum number of elements per two pi", default=-1, nargs='?', const=1, type=int)
+    args = parser.parse_args()
+
+    workdir = os.path.join("output", args.name_of_study, args.dimensions, str(args.resolution))
+    if args.refine:
+        workdir = os.path.join("output", args.name_of_study, args.dimensions, str(args.resolution), "refined")
+
+    L_CELL = 80
+    L_SEP = 25
+    L_slab_am = 5
+    LY = 20
+    LX = 20
+
+    random.seed(0) # get same sequence of values every run
+    markers = commons.Markers()
+
+    utils.make_dir_if_missing(workdir)
+    mshpath = os.path.join(workdir, f"mesh.{args.format}")
+    geometry_metafile = os.path.join(workdir, "geometry.json")
+    gmsh.initialize()
+    gmsh.model.add('ellipsoidals')
+    gmsh.option.setNumber("Mesh.MeshSizeMax", args.resolution)
+    gmsh.option.setNumber('Mesh.Optimize', 1)
+    gmsh.option.setNumber("Mesh.OptimizeThreshold", 0.9)
+    gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 20)
+    if args.hexahedron:
+        gmsh.option.setNumber('Mesh.SubdivisionAlgorithm', 2)
+
+    box_am = gmsh.model.occ.addBox(-0.5*LX/L_CELL, -0.5*LY/L_CELL, (L_CELL - L_slab_am)/L_CELL, LX/L_CELL, LY/L_CELL, L_slab_am/L_CELL)
+    spheres = []
+    cylinders = []
+
+    lxs = np.arange(-0.5*LX/L_CELL+2.5/80, 0.5*LX/L_CELL, 5/L_CELL)
+    lys = np.arange(-0.5*LY/L_CELL+2.5/80, 0.5*LY/L_CELL, 5/L_CELL)
+
+    for x in lxs:
+        for y in lys:
+            cyl = gmsh.model.occ.addCylinder(x, y, L_SEP/L_CELL, 0, 0, 1 - (L_SEP + L_slab_am)/L_CELL, 1.5/L_CELL)
+            cylinders.append((3, cyl))
+            gmsh.model.occ.synchronize()
+
+    z_pos = (L_CELL - L_slab_am)/L_CELL
+    while z_pos > 0.3175:
+        for x in lxs:
+            for y in lys:
+                p_val = random.uniform(0, 1)
+                if p_val <= 0.25:
+                    p_val2 = random.uniform(0, 1)
+                    r = 1.75 + p_val2 * 5.0
+                    if (x + r/L_CELL) >= 0.5 * LX/L_CELL or (x - r/L_CELL) <= -0.5 * LX/L_CELL:
+                        continue
+                    if (y + r/L_CELL) >= 0.5 * LY/L_CELL or (y - r/L_CELL) <= -0.5 * LY/L_CELL:
+                        continue
+                    if (z_pos + r/L_CELL >= 1 - L_slab_am/L_CELL) or (z_pos -r/L_CELL) <= L_SEP/L_CELL:
+                        continue
+                    # sphere = gmsh.model.occ.addSphere(x, y, z_pos, r/L_CELL)
+                    cyl = gmsh.model.occ.addCylinder(x, y, z_pos, 0, r/L_CELL, 0, r/L_CELL)
+                    spheres.append((3, cyl))
+                    gmsh.model.occ.synchronize()
+        z_pos -= 2.5/L_CELL
+
+    tol = 0.1/L_CELL
+    # vols = gmsh.model.getEntities(3)
+    # join = gmsh.model.occ.getEntitiesInBoundingBox(-0.5*LX/L_CELL - tol, -0.5*LY/L_CELL - tol, L_SEP/L_CELL - tol, LX/L_CELL, LX/L_CELL, 1 - L_slab_am/L_CELL-tol)
+    for v in cylinders:
+        print(v)
+        e = gmsh.model.getBoundary(gmsh.model.getBoundary([v]), False)
+        gmsh.model.occ.fillet([v[1]], [abs(i[1]) for i in e], [tol])
+    # ov = gmsh.model.occ.fillet([v[1] for v in vols[1:]], [i[1] for i in join if i[0] == 1], [tol])
+    gmsh.model.occ.synchronize()
+    # print(ov)
+    # vols = [v[1] for v in ov]
+    # print(vols)
+    vols = gmsh.model.getEntities(3)
+    ov, ovv = gmsh.model.occ.fuse(vols[:1], vols[1:])
+    gmsh.model.occ.synchronize()
+    gmsh.model.occ.synchronize()
+    ov, ovv = gmsh.model.occ.fuse([(3, box_am)], ov)
+    gmsh.model.occ.synchronize()
+    vols = gmsh.model.getEntities(3)
+    box_se = gmsh.model.occ.addBox(-0.5*LX/L_CELL, -0.5*LY/L_CELL, 0, LX/L_CELL, LY/L_CELL, 1)
+    gmsh.model.occ.synchronize()
+    res = gmsh.model.occ.cut([(3, box_se)], vols, removeTool=False)
+    gmsh.model.occ.synchronize()
+    gmsh.model.mesh.removeDuplicateElements()
+    gmsh.model.occ.synchronize()
+    vols = gmsh.model.getEntities(3)
+    gmsh.model.occ.synchronize()
+    centers = []
+    for v in vols:
+        com = gmsh.model.occ.getCenterOfMass(*v)
+        centers.append(com[2])
+    if centers[0] > centers[1]:
+        gmsh.model.addPhysicalGroup(3, [vols[1][1]], markers.electrolyte, "electrolyte")
+        gmsh.model.addPhysicalGroup(3, [vols[0][1]], markers.positive_am, "positive am")
+    else:
+        gmsh.model.addPhysicalGroup(3, [vols[0][1]], markers.electrolyte, "electrolyte")
+        gmsh.model.addPhysicalGroup(3, [vols[1][1]], markers.positive_am, "positive am")
+    gmsh.model.occ.synchronize()
+    left = []
+    right = []
+    insulated_am = []
+    insulated_se = []
+    interface = []
+    main_xface = []
+    pieces = []
+    for surf in gmsh.model.getEntities(2):
+        com = gmsh.model.occ.getCenterOfMass(*surf)
+        area = gmsh.model.occ.getMass(*surf)
+        if np.isclose(com[2], 0):
+            left.append(surf[1])
+            continue
+        elif np.isclose(com[2], 1):
+            right.append(surf[1])
+            continue
+        elif np.isclose(com[2], 1 - 0.5 * L_slab_am/L_CELL):
+            insulated_am.append(surf[1])
+            continue
+        elif np.isclose(com[2], 0.5 * (L_CELL - L_slab_am)/L_CELL):
+            insulated_se.append(surf[1])
+            continue
+        else:
+            if np.isclose(area, (LX/L_CELL) ** 2):
+                main_xface.append(surf)
+            else:
+                pieces.append(surf)
+            if not np.isclose(com[2], 1 - L_slab_am/L_CELL, atol=1e-6):
+                interface.append(surf[1])
+            else:
+                interface.append(surf[1])
+
+    gmsh.model.addPhysicalGroup(2, left, markers.left, "left")
+    gmsh.model.addPhysicalGroup(2, right, markers.right, "right")
+    gmsh.model.addPhysicalGroup(2, insulated_am, markers.insulated_positive_am, "insulated_positive_am")
+    gmsh.model.addPhysicalGroup(2, insulated_se, markers.insulated_electrolyte, "insulated_electrolyte")
+    gmsh.model.addPhysicalGroup(2, interface, markers.electrolyte_v_positive_am, "electrolyte_v_positive_am")
+    gmsh.model.occ.synchronize()
+    # gmsh.model.mesh.setTransfiniteAutomatic([(2, s) for s in left + interface + right], cornerAngle=np.pi/6, recombine=False)
+    gmsh.model.occ.synchronize()
+    if args.refine:
+        gmsh.model.mesh.field.add("Distance", 1)
+        gmsh.model.mesh.field.setNumbers(1, "SurfacesList", left + interface + right)
+        gmsh.model.mesh.field.setNumber(1, "Sampling", 1000)
+
+        gmsh.model.mesh.field.add("Threshold", 2)
+        gmsh.model.mesh.field.setNumber(2, "InField", 1)
+        gmsh.model.mesh.field.setNumber(2, "SizeMin", args.resolution / 5)
+        gmsh.model.mesh.field.setNumber(2, "SizeMax", args.resolution)
+        gmsh.model.mesh.field.setNumber(2, "DistMin", 0)
+        gmsh.model.mesh.field.setNumber(2, "DistMax", 0.01)
+
+        gmsh.model.mesh.field.add("Max", 5)
+        gmsh.model.mesh.field.setNumbers(5, "FieldsList", [2])
+        gmsh.model.mesh.field.setAsBackgroundMesh(5)
+        gmsh.model.occ.synchronize()
+    gmsh.model.mesh.generate(3)
+    gmsh.write(mshpath)
+    gmsh.finalize()
