24394bb01823c361f4a419ad1dd02499c084f84d
Leshinka Molel <emolel3@gatech.edu>
2025-05-31 04:26:20 -0400

    update

--- reconstruct-volume.sh
+++ reconstruct-volume.sh
@@ -2,4 +2,4 @@
 
 python3 volsurf.py --size 201-201-201 --scale 0.0858e-6,0.0858e-6,0.2e-6 --L_sep 15e-6
 tetgen -pk output/segmentation/cam/201-201-201/0-0-0/cam.stl
-python3 volsurf.py --size 201-201-201 --scale 0.0858e-6,0.0858e-6,0.2e-6 --L_sep 15e-6
+python3 mesher.py --size 201-201-201 --scale 0.0858e-6,0.0858e-6,0.2e-6 --L_sep 15e-6
