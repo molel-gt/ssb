9bd0b353962bb6f7864b35369f576ebd35ad36aa
Leshinka Molel <emolel3@gatech.edu>
2025-05-31 18:26:54 -0400

    input arguments to reconstruct volume

--- reconstruct-volume.sh
+++ reconstruct-volume.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-python3 volsurf.py --size 201-201-201 --scale 0.0858e-6,0.0858e-6,0.2e-6 --L_sep 15e-6
-tetgen -pkA output/segmentation/201-201-201/0-0-0/voids.stl
-tetgen -pkA output/segmentation/201-201-201/0-0-0/cam.stl
-tetgen -pkA output/segmentation/201-201-201/0-0-0/sse.stl
-python3 mesher.py --size 201-201-201 --scale 0.0858e-6,0.0858e-6,0.2e-6 --L_sep 15e-6
+if [ "$#" -eq 0 ]; then
+  echo "Error: No arguments provided."
+  echo "Usage: $0 Nx-Ny-Nz"
+  exit 1
+fi
+
+python3 volsurf.py --size $1 --scale 0.0858e-6,0.0858e-6,0.2e-6 --L_sep 15e-6
+tetgen -pkA output/segmentation/$1/0-0-0/voids.stl
+tetgen -pkA output/segmentation/$1/0-0-0/cam.stl
+tetgen -pkA output/segmentation/$1/0-0-0/sse.stl
+python3 mesher.py --size $1 --scale 0.0858e-6,0.0858e-6,0.2e-6 --L_sep 15e-6
