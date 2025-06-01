e6f81f1d8be4cc7f663597e724b5c54465edd2d3
Leshinka Molel <emolel3@gatech.edu>
2025-06-01 08:19:12 -0400

    convert to ascii

--- reconstruct-volume.sh
+++ reconstruct-volume.sh
@@ -7,7 +7,14 @@ if [ "$#" -eq 0 ]; then
 fi
 
 python3 volsurf.py --size $1 --scale 0.08e-6,0.08e-6,0.2e-6 --L_sep 15e-6
+
+meshio ascii output/segmentation/$1/0-0-0/voids.stl
 tetgen -pkAY output/segmentation/$1/0-0-0/voids.stl
+
+meshio ascii output/segmentation/$1/0-0-0/cam.stl
 tetgen -pkAY output/segmentation/$1/0-0-0/cam.stl
+
+meshio ascii output/segmentation/$1/0-0-0/sse.stl
 tetgen -pkAY output/segmentation/$1/0-0-0/sse.stl
+
 python3 mesher.py --size $1 --scale 0.08e-6,0.08e-6,0.2e-6 --L_sep 15e-6
