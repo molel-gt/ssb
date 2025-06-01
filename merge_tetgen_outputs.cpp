c6334995dcd8257146818d91b7ba152e531701b1
Leshinka Molel <emolel3@gatech.edu>
2025-06-01 00:31:22 -0400

    update

--- merge_tetgen_outputs.cpp
+++ merge_tetgen_outputs.cpp
@@ -50,7 +50,7 @@ std::map<Point, std::vector<int>> read_tetgen_nodes_to_map(std::string nodes_fil
                 int idx = parts[0];
                 int attribute_val = -1;
                 int boundary_val = -1;
-                Point p = {parts[0], parts[1], parts[2]};
+                Point p = {parts[1], parts[2], parts[3]};
                 if (parts.size() == 5){
                     if (attribute){
                         attribute_val = parts[4];
@@ -95,6 +95,60 @@ void renumber_tetgen_faces(std::map<int, int> old_to_new);
 void renumber_tetgen_tets(std::map<int, int> old_to_new);
 
 std::map<Triangle, std::vector<int> > read_tetgen_faces_to_map(std::string faces_file){
+    // remove lines begining with #
+    std::map<Triangle, std::vector<int>> output_faces;
+    strip_leading_character(nodes_file, "#");
+    ifstream file(faces_file);
+    std::string line;
+    int count = 0;
+    int n_entities;
+    int entity_size = 3;
+    bool attribute = false;
+    bool boundary = false; 
+    if (file.is_open()) {
+        // Read each line from the file and store it in the
+        // 'line' variable.
+        while (getline(file, line)) {
+            std::vector<float> parts = split_string_into_array(line, " ");
+            if (count == 0){
+                n_entities = parts[0];
+                entity_size = parts[1]
+                attribute = parts[2];
+                boundary = parts[3];
+            }
+            else {
+                int idx = parts[0];
+                int attribute_val = -1;
+                int boundary_val = -1;
+                Triangle f = {parts[1], parts[2], parts[3]};
+                if (parts.size() == 5){
+                    if (attribute){
+                        attribute_val = parts[4];
+
+                    }
+                    if (boundary){
+                        boundary_val = parts[4];
+                    }
+                else if (parts.size() == 6){
+                    attribute_val = parts[5];
+                    boundary_val = parts[6];
+                }
+            }
+            output_faces[f] = {idx, attribute_val, boundary_val};
+            count ++;
+        }
+
+        // Close the file stream once all lines have been
+        // read.
+        file.close();
+    }
+    else {
+        // Print an error message to the standard error
+        // stream if the file cannot be opened.
+        std::cerr << "Unable to open file!" << endl;
+    }
+
+    return output_faces;
 
 }
 
@@ -103,6 +157,60 @@ void merge_tetgen_faces(std::vector<<std::string>> node_files, std::string faces
 }
 
 std::map<Tetrahedron, std::vector<int> > read_tetgen_tets_to_map(std::string tets_file){
+    // remove lines begining with #
+    std::map<Tetrahedron, std::vector<int>> output_tets;
+    strip_leading_character(nodes_file, "#");
+    ifstream file(nodes_file);
+    std::string line;
+    int count = 0;
+    int n_entities;
+    int entity_size = 4;
+    bool attribute = false;
+    bool boundary = false; 
+    if (file.is_open()) {
+        // Read each line from the file and store it in the
+        // 'line' variable.
+        while (getline(file, line)) {
+            std::vector<float> parts = split_string_into_array(line, " ");
+            if (count == 0){
+                n_entities = parts[0];
+                entity_size = parts[1]
+                attribute = parts[2];
+                boundary = parts[3];
+            }
+            else {
+                int idx = parts[0];
+                int attribute_val = -1;
+                int boundary_val = -1;
+                Tetrahedron t = {parts[1], parts[2], parts[3], parts[4]};
+                if (parts.size() == 6){
+                    if (attribute){
+                        attribute_val = parts[5];
+
+                    }
+                    if (boundary){
+                        boundary_val = parts[5];
+                    }
+                else if (parts.size() == 7){
+                    attribute_val = parts[5];
+                    boundary_val = parts[6];
+                }
+            }
+            output_tets[t] = {idx, attribute_val, boundary_val};
+            count ++;
+        }
+
+        // Close the file stream once all lines have been
+        // read.
+        file.close();
+    }
+    else {
+        // Print an error message to the standard error
+        // stream if the file cannot be opened.
+        std::cerr << "Unable to open file!" << endl;
+    }
+
+    return output_tets;
 
 }
 
