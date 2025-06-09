c95c434ecbe510176bb4f4a43871d9dca72a0a9e
Leshinka Molel <emolel3@gatech.edu>
2025-06-09 15:49:45 -0400

    Add active contact area

--- corrugated_cylinders_geo.py
+++ corrugated_cylinders_geo.py
@@ -4,9 +4,53 @@ import os
 import random
 
 import gmsh
+import matplotlib.pyplot as plt
 import numpy as np
+import alphashape
 
-import commons, utils
+import commons, configs, geometry, grapher, utils
+
+area_frac_to_img_id = {
+    "98.41": 6,
+    "36.38": 11,
+    "6.30": 16,
+    "0.45": 22,
+}
+
+
+def build_active_contact_area_map(img, scale_x, scale_y, LX, LY, L_CELL):
+    max_surf_id = max([s[1] for s in gmsh.model.occ.getEntities(2)])
+    side_loops = []
+    insulated = []
+    right = []
+    left_active = []
+    left = []
+    process_count = 0
+    image = img.copy()
+    image[0, :] = 0
+    image[-1, :] = 0
+    image[:, 0] = 0
+    image[:, -1] = 0
+    boundary_pieces, count, points, points_view = geometry.get_phase_boundary_pieces(image)
+    for hull in boundary_pieces:
+        hull_arr = np.asarray(hull)
+        hull_points = []
+        for pp in hull[:-1]:
+            idx = gmsh.model.occ.addPoint(int(pp[0]) * scale_x - 0.5*LX/L_CELL, int(pp[1]) * scale_y - 0.5*LY/L_CELL, 0)
+            hull_points.append(
+                idx
+            )
+        hull_lines = []
+        for i in range(-1, len(hull_points) - 1):
+            idx = gmsh.model.occ.addLine(hull_points[i], hull_points[i + 1])
+            hull_lines.append(
+                idx
+            )
+        idx = gmsh.model.occ.addCurveLoop(hull_lines)
+        side_loops.append(idx)
+        idx2 = gmsh.model.occ.addPlaneSurface((idx, ))
+        left.append(idx2)
+    return left
 
 
 if __name__ == '__main__':
@@ -18,6 +62,7 @@ if __name__ == '__main__':
     parser.add_argument("-hexahedron", "--hexahedron", help="compute current distribution stats", default=False, action=argparse.BooleanOptionalAction)
     parser.add_argument("-format", "--format", help="Mesh format", default="msh", nargs='?', const=1)
     parser.add_argument("-min_elements_per_2pi", "--min_elements_per_2pi", help="minimum number of elements per two pi", default=-1, nargs='?', const=1, type=int)
+    parser.add_argument("-A", "--active_area_fraction", help="active area fraction (%)", default=100, nargs='?', const=1, type=float)
     args = parser.parse_args()
 
     workdir = os.path.join("output", args.name_of_study, args.dimensions, str(args.resolution))
@@ -29,6 +74,15 @@ if __name__ == '__main__':
     L_slab_am = 5
     LY = 20
     LX = 20
+    scale_x = LX/L_CELL * 1.0/470.0
+    scale_y = LY/L_CELL * 1.0/470.0
+    img_id = area_frac_to_img_id.get(f"{args.active_area_fraction:.2f}")
+    if not np.isclose(args.active_area_fraction, 100) and img_id is not None:
+        img = np.asarray(plt.imread(f'data/current_constriction/test{str(int(img_id))}.tif')[:, :, 0], dtype=np.uint8)
+
+    workdir = os.path.join("output", args.name_of_study, args.dimensions, str(args.resolution), f"{args.active_area_fraction:.2f}")
+    if args.refine:
+        workdir = os.path.join("output", args.name_of_study, args.dimensions, str(args.resolution), f"{args.active_area_fraction:.2f}", "refined")
 
     random.seed(0) # get same sequence of values every run
     markers = commons.Markers()
@@ -79,11 +133,6 @@ if __name__ == '__main__':
 
     ov, ovv = gmsh.model.occ.fuse(cylinders[:1], cylinders[1:] + spheres)
     gmsh.model.occ.synchronize()
-    # tol = 0.1/L_CELL
-    # vols = gmsh.model.getEntities(3)
-    # join = gmsh.model.occ.getEntitiesInBoundingBox(-0.5*LX/L_CELL - tol, -0.5*LY/L_CELL - tol, L_SEP/L_CELL - tol, LX/L_CELL, LX/L_CELL, 1 - L_slab_am/L_CELL-tol)
-    # ov = gmsh.model.occ.fillet([v[1] for v in vols[1:]], [i[1] for i in join if i[0] == 1], [tol], removeVolume=True)
-    # gmsh.model.occ.synchronize()
     ov, ovv = gmsh.model.occ.fuse([(3, box_am)], ov)
     gmsh.model.occ.synchronize()
     vols = gmsh.model.getEntities(3)
@@ -100,12 +149,19 @@ if __name__ == '__main__':
         com = gmsh.model.occ.getCenterOfMass(*v)
         centers.append(com[2])
     if centers[0] > centers[1]:
-        gmsh.model.addPhysicalGroup(3, [vols[1][1]], markers.electrolyte, "electrolyte")
-        gmsh.model.addPhysicalGroup(3, [vols[0][1]], markers.positive_am, "positive am")
+        se_vols = [vols[1][1]]
+        am_vols = [vols[0][1]]
     else:
-        gmsh.model.addPhysicalGroup(3, [vols[0][1]], markers.electrolyte, "electrolyte")
-        gmsh.model.addPhysicalGroup(3, [vols[1][1]], markers.positive_am, "positive am")
+        am_vols = [vols[1][1]]
+        se_vols = [vols[0][1]]
+    left_active = None
+    if img_id is not None:
+        left_active = build_active_contact_area_map(img, scale_x, scale_y, LX, LY, L_CELL)
+        ov, ovv = gmsh.model.occ.fragment([(3, se_vols[0])], [(2, s) for s in left_active], removeTool=False)
+        se_vols = [s[1] for s in ov if s[0] == 3]
     gmsh.model.occ.synchronize()
+    gmsh.model.addPhysicalGroup(3, se_vols, markers.electrolyte, "electrolyte")
+    gmsh.model.addPhysicalGroup(3, am_vols, markers.positive_am, "positive am")
     left = []
     right = []
     insulated_am = []
@@ -137,7 +193,8 @@ if __name__ == '__main__':
                 interface.append(surf[1])
             else:
                 interface.append(surf[1])
-
+    if img_id is not None:
+        left = left_active
     gmsh.model.addPhysicalGroup(2, left, markers.left, "left")
     gmsh.model.addPhysicalGroup(2, right, markers.right, "right")
     gmsh.model.addPhysicalGroup(2, insulated_am, markers.insulated_positive_am, "insulated_positive_am")
