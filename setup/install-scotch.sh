ffea57820d5c5e7ea88fdc9f34e7b41866e21b9d
Leshinka Molel <emolel3@gatech.edu>
2025-06-11 12:43:20 -0400

    install scotch

--- setup/install-scotch.sh
+++ setup/install-scotch.sh
@@ -4,12 +4,16 @@ cd $SOFTWARES_DIR
 
 if [ -d $SOFTWARES_DIR/scotch ]; then
     echo 'directory exists, not cloning'
+    git pull origin
 else
     git clone https://gitlab.inria.fr/scotch/scotch.git
 fi
 
 cd $SOFTWARES_DIR/scotch
 
+if [ -d build ]; then
+    rm -r build
+fi
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DMPI_HOME=$SOFTWARES_DIR/openmpi-5.0.5
-make -j5 DESTDIR=$CMAKE_INSTALL_PREFIX && make install DESTDIR=$CMAKE_INSTALL_PREFIX 
+cmake ..  -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DMPI_HOME=$SOFTWARES_DIR/openmpi-5.0.5
+make -j5 && make install
