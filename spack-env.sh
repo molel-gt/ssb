5e9e18699458dfc5d96b7d92ae309affa47d2df6
Leshinka Molel <emolel3@gatech.edu>
2025-06-14 03:09:18 -0400

    install spack env

--- spack-env.sh
+++ spack-env.sh
@@ -7,12 +7,11 @@ if [ -d $SOFTWARES_DIR/spack ]; then
 else
     git clone https://github.com/spack/spack.git
     cd $SOFTWARES_DIR/spack
-    git checkout v0.23.0
+    # git checkout v0.23.0
     cp $WORK_DIR/../spack/config.yaml $SOFTWARES_DIR/spack/
     . $SOFTWARES_DIR/spack/share/spack/setup-env.sh
     spack env create fenicsx-env
     spack env activate fenicsx-env
-    spack add fenics-dolfinx@main%gcc@12.3.0+adios2 py-fenics-dolfinx%gcc@12.3.0 cflags="-O3" fflags="-O3"
-    # spack add openmpi%gcc@12.3.0
+    spack add fenics-dolfinx@main%gcc@12.3.0+adios2+petsc py-fenics-dolfinx%gcc@12.3.0 cflags="-O3" fflags="-O3"
     spack install
 fi
