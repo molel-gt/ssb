f2b2f1eb22f676ea3bba0d0eab85dee8441fdced
Leshinka Molel <emolel3@gatech.edu>
2025-06-22 03:10:26 -0400

    update

--- setup/install-slepc.sh
+++ setup/install-slepc.sh
@@ -7,7 +7,7 @@ else
     git clone https://gitlab.com/slepc/slepc
 fi
 cd $SOFTWARES_DIR/slepc
-git stash && git pull origin && git checkout v3.23.0
+git stash && git checkout main && git pull origin && git checkout v3.23.0
 ./configure --prefix=$CMAKE_INSTALL_PREFIX --with-clean
 make SLEPC_DIR=$SLEPC_DIR PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH
 make SLEPC_DIR=$SLEPC_DIR PETSC_DIR=$PETSC_DIR install
