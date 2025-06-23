76000df71569c86caae0df96034981833eaea520
Leshinka Molel <emolel3@gatech.edu>
2025-06-23 16:47:31 -0400

    boost

---
+++ setup/install-boost.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $SOFTWARES_DIR
+version=1_86_0
+BOOST_DIR=$SOFTWARES_DIR/boost_$version
+if [ -d $BOOST_DIR ]; then
+    echo "directory exists"
+else
+    wget https://archives.boost.io/release/$(echo $version | tr _ . )/source/boost_$version.tar.gz
+    tar xvzf boost_$version.tar.gz
+fi
+
+cd $BOOST_DIR
+./bootstrap.sh prefix=$CMAKE_INSTALL_PREFIX
+./b2 prefix=$CMAKE_INSTALL_PREFIX
