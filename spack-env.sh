cd4eedbee1eb13245f3488a360f3be0ca46a273d
Leshinka Molel <emolel3@gatech.edu>
2025-06-24 17:51:08 -0400

    spack install

--- spack-env.sh
+++ spack-env.sh
@@ -6,13 +6,15 @@ if [ -d $SOFTWARES_DIR/spack ]; then
     echo 'directory exists, skip cloning'
 else
     git clone https://github.com/spack/spack.git
-    cd $SOFTWARES_DIR/spack
-    # git checkout v0.23.0
-    cp $WORK_DIR/../spack/config.yaml $SOFTWARES_DIR/spack/
-    . $SOFTWARES_DIR/spack/share/spack/setup-env.sh
-    spack env create fenicsx-env
-    spack env activate fenicsx-env
-    spack add fenics-dolfinx@main%gcc@12.3.0+adios2+petsc py-fenics-dolfinx%gcc@12.3.0 cflags="-O3" fflags="-O3"
-    spack add py-gmsh
-    spack install
 fi
+
+cd $SOFTWARES_DIR/spack
+# git checkout v0.23.0
+sed -i 's#$tempdir/$user/spack-stage#/storage/coda1/p-tf74/0/shared/leshinka/spack-temp#g' ./etc/spack/defaults/config.yaml
+. $SOFTWARES_DIR/spack/share/spack/setup-env.sh
+spack env create fenicsx-env
+spack env activate fenicsx-env
+spack add fenics-dolfinx@main+adios2+petsc py-fenics-dolfinx@main cflags="-O3" fflags="-O3"
+spack add py-gmsh
+spack add petsc+strumpack
+spack install
