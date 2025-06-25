3c2176e7e231d0904ce936dfeff0391a37deed3f
Leshinka Molel <emolel3@gatech.edu>
2025-06-25 01:56:22 -0400

    update

--- spack-env.sh
+++ spack-env.sh
@@ -16,7 +16,7 @@ spack env create fenicsx-env
 spack env activate fenicsx-env
 spack add petsc^strumpack~slate
 spack add adios2
-spack add fenics-dolfinx@main+adios2+petsc^strumpack py-fenics-dolfinx@main cflags="-O3" fflags="-O3"
+spack add fenics-dolfinx@main+adios2+petsc^strumpack~slate py-fenics-dolfinx@main cflags="-O3" fflags="-O3"
 #spack add py-gmsh
 spack install
 spack load py-pip
