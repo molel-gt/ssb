167625cc6e6f748f19e4ef94ba17085ca23156ca
Leshinka Molel <emolel3@gatech.edu>
2025-06-26 21:57:26 -0400

    update

--- setup/install-autoconf.sh
+++ setup/install-autoconf.sh
@@ -6,7 +6,7 @@ if [ -d $SOFTWARES_DIR/autoconf ]; then
 else
     git clone git://git.sv.gnu.org/autoconf
 fi
-
+git stash && git checkout master && git pull origin && git checkout v2.72
 cd $SOFTWARES_DIR/autoconf
 ./bootstrap
 ./configure --prefix=$CMAKE_INSTALL_PREFIX
