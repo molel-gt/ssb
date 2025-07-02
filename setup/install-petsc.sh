3c958bfc8d6ec86ad9fd6cb3a12cf57f69781455
Leshinka Molel <emolel3@gatech.edu>
2025-07-02 18:11:02 -0400

    petsc upgrade

--- setup/install-petsc.sh
+++ setup/install-petsc.sh
@@ -9,7 +9,7 @@ else
 fi
 
 cd $SOFTWARES_DIR/petsc
-git checkout v3.23.2
+git checkout v3.23.3
 
 ./configure --with-clean --download-cmake --download-f2cblaslapack COPTFLAGS='-O2' CXXOPTFLAGS='-O2' FOPTFLAGS='-O2' --with-mpi-dir=$CMAKE_INSTALL_PREFIX --download-strumpack --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-ptscotch --download-eigen --download-hdf5 PETSC_ARCH=$PETSC_ARCH --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-scalapack --with-log=1 --with-cuda=0 --use-gpu-aware-mpi=0 --download-netcdf --download-zlib --download-ml --download-suitesparse --download-spai
 make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH all
