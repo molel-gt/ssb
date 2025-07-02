4713ca7a9e52374f58ecef376a18f230d8de4210
Leshinka Molel <emolel3@gatech.edu>
2025-07-02 18:01:34 -0400

    version

--- setup/install-pkgconfig.sh
+++ setup/install-pkgconfig.sh
@@ -7,8 +7,9 @@ if [ -d $SOFTWARES_DIR/pkg-config ]; then
 else
     git clone https://gitlab.freedesktop.org/pkg-config/pkg-config.git
 fi
-git checkout 0.29.2
+
 cd $SOFTWARES_DIR/pkg-config
+git checkout pkg-config-0.29.2
 ./autogen.sh --prefix=$CMAKE_INSTALL_PREFIX --with-internal-glib
 ./configure --prefix=$CMAKE_INSTALL_PREFIX
 cmake . -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH
