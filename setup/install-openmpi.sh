9fefab72fd53a435da421d15a6cde576b89eb655
Leshinka Molel <emolel3@gatech.edu>
2025-07-03 19:43:59 -0400

    openmpi version

--- setup/install-openmpi.sh
+++ setup/install-openmpi.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-
-OPENMPI_SRC_DIR=$SOFTWARES_DIR/openmpi-5.0.5
+version=5.0.5
+OPENMPI_SRC_DIR=$SOFTWARES_DIR/openmpi-$version
 
 cd $SOFTWARES_DIR
 
 if [ -d "$OPENMPI_SRC_DIR" ]; then
     echo 'directory exists, skip cloning'
 else
-    # git clone https://github.com/open-mpi/ompi.git --recursive
-    wget https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.5.tar.gz
-    tar xvzf openmpi-5.0.5.tar.gz
+    wget https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-$version.tar.gz
+    tar xvzf openmpi-$version.tar.gz
 fi
 
-# git checkout v5.0.5
-
 cd $OPENMPI_SRC_DIR
-# ./autogen.pl
 ./configure --prefix=$CMAKE_INSTALL_PREFIX
 make -j && make install
