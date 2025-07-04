062d89526984717ec32a66d1001a7795e69ec5d0
Leshinka Molel <emolel3@gatech.edu>
2025-07-04 13:34:27 -0400

    use system openmpi

--- spack-env.sh
+++ spack-env.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-cd $SOFTWARES_DIR
+cd $HPC_DIR
 
-if [ -d $SOFTWARES_DIR/spack ]; then
+if [ -d $HPC_DIR/spack ]; then
     echo 'directory exists, skip cloning'
 else
     git clone https://github.com/spack/spack.git
 fi
 
-cd $SOFTWARES_DIR/spack
+cd $HPC_DIR/spack
 # git checkout v0.23.0
 sed -i 's#$tempdir/$user/spack-stage#/storage/coda1/p-tf74/0/shared/leshinka/spack-temp#g' ./etc/spack/defaults/config.yaml
-. $SOFTWARES_DIR/spack/share/spack/setup-env.sh
+. $HPC_DIR/spack/share/spack/setup-env.sh
 spack env create fenicsx-env
 spack env activate fenicsx-env
+spack external find slurm
+spack external find openmpi
 spack add petsc^strumpack~slate
 spack add adios2
-spack add fenics-dolfinx@main+adios2+petsc^strumpack~slate py-fenics-dolfinx@main cflags="-O3" fflags="-O3"
-#spack add py-gmsh
+spack add fenics-dolfinx@main+adios2+petsc^strumpack~slate py-fenics-dolfinx@main^openmpi@4.1.5 cflags="-O3" fflags="-O3"
+spack add py-gmsh
 spack install
 spack load py-pip
 python3 -m pip install matplotlib scipy
