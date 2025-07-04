970f1deeebe41040965ae465b90f0606ca0aedd7
Leshinka Molel <emolel3@gatech.edu>
2025-07-04 14:56:07 -0400

    revert changes until v10.0.0

--- li_metal_ssb.py
+++ li_metal_ssb.py
@@ -492,6 +492,7 @@ if __name__ == '__main__':
     interface_i_density_file = os.path.join(results_dir, "i_x_density.json")
     stats_csv_file = os.path.join(results_dir, "stats.csv")
     i_interface_density_plot = os.path.join(results_dir, "i_x_density.eps")
+    i_interface_density_json = os.path.join(results_dir, "i_x_density.json")
     convergence_history = os.path.join(results_dir, "convergence.eps")
     se_am_frequency_plot = os.path.join(results_dir, "se_am_frequency.eps")
     se_am_cdf_plot = os.path.join(results_dir, "se_am_cdf.eps")
@@ -1406,6 +1407,11 @@ if __name__ == '__main__':
         i_intervals = np.linspace(0, 1.05 * np.max([np.abs(i_avg_left), np.abs(i_avg_right)]), 101)
         densities = current_density_distribution(comm, current_h(r_res), n_r, tol_fun_left, tol_fun_right, dInterface, entity_maps, i_intervals)
         densities[:, 2] /= A_se_am_tilde
+        _freqs = {"t [s]": cycler.time*t_ref, "i_min [A/m2]": densities[:, 0].tolist(), "i_max [A/m2]": densities[:, 1].tolist(), "areal density": densities[:, 2].tolist()}
+        output_i_x_density_json_file = i_interface_density_json[:-5] + "_" + f"{cycler.time*t_ref:.1f}.json"
+
+        with open(output_i_x_density_json_file, "w", encoding="utf-8") as f_ix:
+            json.dump(_freqs, f_ix, indent=4, ensure_ascii=False)
         if comm_rank == 0 and args.plot:
             fig, ax = plt.subplots()
             ax.bar(0.5*(densities[:, 0] + densities[:, 1]), densities[:, 2], width=i_intervals[1], align="center")
@@ -1416,7 +1422,7 @@ if __name__ == '__main__':
             ax.set_ylim([0, 1.01 * np.max(densities[:, 2])])
             ax.set_xlim([0, np.max(i_intervals)])
             plt.tight_layout()
-            plt.savefig(i_interface_density_plot, )
+            plt.savefig(i_interface_density_plot[:-4] + "_" + f"{cycler.time*t_ref:.1f}.eps")
             # plt.show()
 
         if comm_rank == 0:
