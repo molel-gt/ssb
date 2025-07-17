2c203a11a8d6183fe8083ed5101766da07c596f1
Leshinka Molel <emolel3@gatech.edu>
2025-07-17 02:32:21 -0400

    update petsc install

--- setup/install-petsc.sh
+++ setup/install-petsc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+version=v3.23.4
 cd $SOFTWARES_DIR
 if [ -d $SOFTWARES_DIR/petsc ]; then
     echo 'Directory exists, not cloning'
@@ -9,9 +9,9 @@ else
 fi
 
 cd $SOFTWARES_DIR/petsc
-git checkout v3.23.3
+git checkout $version
 
-./configure --with-clean --download-cmake --download-f2cblaslapack COPTFLAGS='-O2' CXXOPTFLAGS='-O2' FOPTFLAGS='-O2' --with-mpi-dir=$CMAKE_INSTALL_PREFIX --download-strumpack --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-ptscotch --download-eigen --download-hdf5 PETSC_ARCH=$PETSC_ARCH --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-scalapack --with-log=1 --with-cuda=0 --use-gpu-aware-mpi=0 --download-netcdf --download-zlib --download-ml --download-suitesparse --download-spai
+./configure --with-clean --download-f2cblaslapack COPTFLAGS='-O2 -fPIC' CXXOPTFLAGS='-O2 -fPIC' FOPTFLAGS='-O2 -fPIC' --download-mumps --with-mpi-dir=$CMAKE_INSTALL_PREFIX --download-hypre --with-64-bit-indices=no --download-metis --download-parmetis --download-ptscotch --download-eigen --download-hdf5 PETSC_ARCH=$PETSC_ARCH --with-scalar-type=real --with-shared-libraries --with-debugging=no --download-superlu_dist --download-scalapack --with-log=1 --with-cuda=0 --use-gpu-aware-mpi=0 --download-netcdf --download-zlib --download-ml --download-suitesparse --download-spai
 make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH all
 make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH check
 $PYTHON3_DIR/bin/python3 -m pip install src/binding/petsc4py
