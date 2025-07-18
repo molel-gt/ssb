e2d005ee144d0d2c5946a264cc5a7a972357ad98
Leshinka Molel <emolel3@gatech.edu>
2025-07-17 21:04:48 -0400

    spdlog

--- setup/install-spdlog.sh
+++ setup/install-spdlog.sh
@@ -8,7 +8,7 @@ else
     git clone https://github.com/gabime/spdlog.git
 fi
 
-git checkout master && git checkout v1.15.0
+git checkout master && git checkout v1.15.3
 
 cd $SOFTWARES_DIR/spdlog
 if [ -d $SOFTWARES_DIR/spdlog/build ]; then
@@ -16,5 +16,5 @@ if [ -d $SOFTWARES_DIR/spdlog/build ]; then
 fi
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_CXX_FLAGS='-fPIC' -DSPDLOG_USE_STD_FORMAT=ON
+cmake .. -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_CXX_FLAGS='-fPIC -std=c++20' #-DSPDLOG_USE_STD_FORMAT=ON
 make -j && make install
