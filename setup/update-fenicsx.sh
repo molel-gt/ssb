70519f9851c1792d436816f6fda17e5095be6825
Leshinka Molel <emolel3@gatech.edu>
2025-07-17 21:21:59 -0400

    accept installation

--- setup/update-fenicsx.sh
+++ setup/update-fenicsx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$PYTHON3_DIR/bin/python3 -m pip uninstall scikit-build-core nanobind
+$PYTHON3_DIR/bin/python3 -m pip uninstall scikit-build-core -y nanobind -y
 cd $WORK_DIR/setup
 sh install-ufl.sh
 sh install-ffcx.sh
