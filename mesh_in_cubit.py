10cdfbbe24902f05812623d7c076bb92c29deab9
Leshinka Molel <emolel3@gatech.edu>
2025-07-19 12:46:09 -0400

    update

--- mesh_in_cubit.py
+++ mesh_in_cubit.py
@@ -10,9 +10,11 @@ LX = 20
 LY = 20
 lxs = np.arange(-0.5*LX+2.5, 0.5*LX, 5)
 lys = np.arange(-0.5*LY+2.5, 0.5*LY, 5)
+radius = 1.5
+height = 50
 for x in lxs:
     for y in lys:
-        cubit.cmd(f"create cylinder radius 1.5 height 50")
+        cubit.cmd(f"create cylinder radius {radius} height {height}")
         last_id += 1
         cubit.cmd(f"volume {last_id} move x {x} y {y} z 50")
 
@@ -28,6 +30,12 @@ volume_ids = [v for v in cubit.parse_cubit_list('volume', 'all') if v not in pos
 # cubit.cmd(f"chop vol {volume_ids[0]} with vol {pos_am[0]} keep")
 # cubit.cmd(f"subtract vol {pos_am[0]} from vol {volume_ids[0]} keep")
 cubit.cmd(f"remove overlap volume {pos_am[0]} {volume_ids[0]} modify larger")
+curves = cubit.parse_cubit_list('curve', 'all')
+circle_arcs = []
+for curve in curves:
+    if np.isclose(cubit.get_arc_length(curve), 2 * np.pi * radius):
+        circle_arcs.append(curve)
+cubit.cmd(f"modify curve {' '.join(map(str, circle_arcs))} blend radius 0.025")
 cubit.cmd("vol all scheme tetmesh")
 cubit.cmd("mesh volume all")
 filename = "mesh.bdf"
