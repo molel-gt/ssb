646b6d958a0760268bba1750c1e3ee169281c3e0
Leshinka Molel <emolel3@gatech.edu>
2025-08-24 18:13:01 -0400

    update

---
+++ corrugated_contact_loss_cylinders_geo.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import random
+
+import gmsh
+import matplotlib.pyplot as plt
+import numpy as np
+import alphashape
+
+import commons, configs, geometry, grapher, utils
+
+area_frac_to_img_id = {
+    "98.41": 6,
+    "36.38": 11,
+    "6.30": 16,
+    "0.45": 22,
+}
+
+
+def build_active_contact_area_map(img, scale_x, scale_y, LX, LY, L_CELL):
+    max_surf_id = max([s[1] for s in gmsh.model.occ.getEntities(2)])
+    side_loops = []
+    insulated = []
+    right = []
+    left_active = []
+    left = []
+    process_count = 0
+    image = img.copy()
+    image[0, :] = 0
+    image[-1, :] = 0
+    image[:, 0] = 0
+    image[:, -1] = 0
+    boundary_pieces, count, points, points_view = geometry.get_phase_boundary_pieces(image)
+    count = 0
+    for hull in boundary_pieces:
+        count += 1
+        print(count, len(hull))
+        if len(hull) < 8:
+            continue
+        hull_arr = np.asarray(hull)
+        print(hull_arr)
+        # hull_arr = np.sort(hull_arr, axis=0)
+        hull_points = []
+        for pp in hull[:-1]:
+            idx = gmsh.model.occ.addPoint(int(pp[0]) * scale_x - 0.5*LX/L_CELL, int(pp[1]) * scale_y - 0.5*LY/L_CELL, 0)
+            hull_points.append(
+                idx
+            )
+        hull_lines = []
+        points = np.array(hull_points)
+        pieces = np.array_split(points, 4)
+        C1 = gmsh.model.occ.addBSpline(pieces[0], degree=2)
+        C2 = gmsh.model.occ.addBSpline(pieces[1], degree=2)
+        C3 = gmsh.model.occ.addBSpline(pieces[2], degree=2)
+        C4 = gmsh.model.occ.addBSpline(pieces[3].tolist() + [pieces[0][0]], degree=2)
+        W1 = gmsh.model.occ.addWire([C1, C2, C3, C4])
+        idx2 = gmsh.model.occ.addBSplineFilling(W1, type="Curved")
+        # for i in range(-1, len(hull_points) - 1):
+        #     idx = gmsh.model.occ.addLine(hull_points[i], hull_points[i + 1])
+        #     hull_lines.append(
+        #         idx
+        #     )
+        # idx = gmsh.model.occ.addCurveLoop(hull_lines)
+        # side_loops.append(idx)
+        # idx2 = gmsh.model.occ.addPlaneSurface((idx, ))
+        # b_curve = gmsh.model.occ.addBSpline(hull_points + hull_points[:1], degree=2)
+        # b_loop = gmsh.model.occ.addCurveLoop([b_curve])
+        # idx2 = gmsh.model.occ.addPlaneSurface([b_loop])
+        left.append(idx2)
+    return left
+
+
+def create_void_phases(vol_frac, L_slab_am, L_SEP, LX, LY, L_CELL):
+    print(L_slab_am, L_SEP, LX, LY, L_CELL)
+    c = 0.2 * 1e-6 / L_CELL
+    b = 0.2 * 1e-6 / L_CELL
+    a = 1.0 * 1e-6 / L_CELL
+    typical_void_vol = 4/3 * np.pi * a * b * c  # volume of void on order of 1 
+    vol_cathode = LX * LY * (L_CELL - L_SEP) / (L_CELL ** 3)
+    total_vol_voids = vol_frac * vol_cathode
+    N_void_particles = int(total_vol_voids / typical_void_vol)
+    np.random.seed(0)
+    ellipsoids = []
+    print(f"Adding {N_void_particles} void particles")
+    for idx in range(N_void_particles):
+        v = np.random.rand(3)
+        vhat = v / np.linalg.norm(v)
+        xpos = (vhat[0] * LX/2) * L_CELL - a
+        ypos = (vhat[1] * LY/2) * L_CELL - a
+        zpos = L_SEP / L_CELL + (np.abs(vhat[2]) * (L_CELL - L_SEP - L_slab_am)/L_CELL)
+        if not np.isclose(zpos, 1 - L_slab_am/L_CELL, atol=0.1):
+            continue
+        print(idx)
+        sphere = gmsh.model.occ.addSphere(xpos, ypos, zpos, c)
+        gmsh.model.occ.synchronize()
+        gmsh.model.occ.dilate([(3, sphere)], xpos, ypos, zpos, 1, b/c, a/c)
+        u = np.random.rand(3)
+        uhat = u / np.linalg.norm(u)
+        angle = np.random.rand(1)[0] * np.pi
+        gmsh.model.occ.rotate([(3, sphere)], xpos, ypos, zpos, uhat[0], uhat[1], uhat[2], angle)
+        ellipsoids.append((3, sphere))
+        gmsh.model.occ.synchronize()
+    print(len(ellipsoids))
+    return ellipsoids
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Estimates Effective Conductivity.')
+    parser.add_argument("-n", "--name_of_study", help="name_of_study", nargs='?', const=1, default="corrugated_cylinders")
+    parser.add_argument("-d", '--dimensions', help='integer representation of Lx-Ly-Lz of the grid',  nargs='?', const=1, default='20-20-80')
+    parser.add_argument("-r", '--resolution', help=f'max resolution (units of microns)', nargs='?', const=1, default=1, type=float)
+    parser.add_argument("-v", '--void_vol_frac', help=f'volume fraction of voids', nargs='?', const=1, default=0.05, type=float)
+    parser.add_argument("-f", "--refine", help="compute current distribution stats", default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument("-hexahedron", "--hexahedron", help="compute current distribution stats", default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument("-format", "--format", help="Mesh format", default="msh", nargs='?', const=1)
+    parser.add_argument("-min_elements_per_2pi", "--min_elements_per_2pi", help="minimum number of elements per two pi", default=-1, nargs='?', const=1, type=int)
+    parser.add_argument("-A", "--active_area_fraction", help="active area fraction (%)", default=100, nargs='?', const=1, type=float)
+    args = parser.parse_args()
+
+    workdir = os.path.join("output", args.name_of_study, args.dimensions, str(args.resolution))
+    if args.refine:
+        workdir = os.path.join("output", args.name_of_study, args.dimensions, str(args.resolution), "refined")
+
+    L_CELL = 80
+    L_SEP = 25
+    L_slab_am = 5
+    LY = 20
+    LX = 20
+    scale_x = LX/L_CELL * 1.0/470.0
+    scale_y = LY/L_CELL * 1.0/470.0
+    img_id = area_frac_to_img_id.get(f"{args.active_area_fraction:.2f}")
+    if not np.isclose(args.active_area_fraction, 100) and img_id is not None:
+        img = np.asarray(plt.imread(f'data/current_constriction/test{str(int(img_id))}.tif')[:, :, 0], dtype=np.uint8)
+
+    workdir = os.path.join("output", args.name_of_study, args.dimensions, str(args.resolution), f"{args.active_area_fraction:.2f}")
+    if args.refine:
+        workdir = os.path.join("output", args.name_of_study, args.dimensions, str(args.resolution), f"{args.active_area_fraction:.2f}", "refined")
+
+    random.seed(0) # get same sequence of values every run
+    markers = commons.Markers()
+
+    utils.make_dir_if_missing(workdir)
+    mshpath = os.path.join(workdir, f"mesh.{args.format}")
+    geometry_metafile = os.path.join(workdir, "geometry.json")
+    gmsh.initialize()
+    gmsh.model.add('ellipsoidals')
+    # gmsh.option.setNumber('Mesh.Optimize', 1)
+    # gmsh.option.setNumber("Mesh.OptimizeThreshold", 0.9)
+    # gmsh.option.setNumber("Mesh.Smoothing", 1)
+    gmsh.option.setNumber("Mesh.Algorithm", 5)
+    # gmsh.option.setNumber("Mesh.Algorithm3D", 10)
+    # gmsh.option.setNumber("General.NumThreads", 4)
+    # gmsh.option.setNumber("Mesh.CharacteristicLengthExtendFromBoundary", 1)
+    # gmsh.option.setNumber("Mesh.ColorCarousel", 2)
+    if args.hexahedron:
+        gmsh.option.setNumber('Mesh.SubdivisionAlgorithm', 2)
+
+    box_am = gmsh.model.occ.addBox(-0.5*LX/L_CELL, -0.5*LY/L_CELL, (L_CELL - L_slab_am)/L_CELL, LX/L_CELL, LY/L_CELL, L_slab_am/L_CELL)
+    spheres = []
+    cylinders = []
+
+    lxs = np.arange(-0.5*LX/L_CELL+2.5/80, 0.5*LX/L_CELL, 5/L_CELL)
+    lys = np.arange(-0.5*LY/L_CELL+2.5/80, 0.5*LY/L_CELL, 5/L_CELL)
+
+    for x in lxs:
+        for y in lys:
+            cyl = gmsh.model.occ.addCylinder(x, y, L_SEP/L_CELL, 0, 0, 1 - (L_SEP + L_slab_am)/L_CELL, 1.5/L_CELL)
+            cylinders.append((3, cyl))
+            gmsh.model.occ.synchronize()
+
+    z_pos = (L_CELL - L_slab_am)/L_CELL
+    while z_pos > 0.3175:
+        for x in lxs:
+            for y in lys:
+                p_val = random.uniform(0, 1)
+                if p_val <= 0.25:
+                    p_val2 = random.uniform(0, 1)
+                    r = 1.75 + p_val2 * 5.0
+                    if (x + r/L_CELL) >= 0.5 * LX/L_CELL or (x - r/L_CELL) <= -0.5 * LX/L_CELL:
+                        continue
+                    if (y + r/L_CELL) >= 0.5 * LY/L_CELL or (y - r/L_CELL) <= -0.5 * LY/L_CELL:
+                        continue
+                    if (z_pos + r/L_CELL >= 1 - L_slab_am/L_CELL) or (z_pos -r/L_CELL) <= L_SEP/L_CELL:
+                        continue
+                    sphere = gmsh.model.occ.addSphere(x, y, z_pos, r/L_CELL)
+                    spheres.append((3, sphere))
+                    gmsh.model.occ.synchronize()
+        z_pos -= 2.5/L_CELL
+
+    ov, ovv = gmsh.model.occ.fuse(cylinders[:1], cylinders[1:] + spheres)
+    # gmsh.model.occ.synchronize()
+    # vols = []
+    # tol = 0.005
+    # for out in ov:
+    #     e = gmsh.model.getBoundary(gmsh.model.getBoundary([out]), False)
+    #     edges = []
+    #     sizes = []
+    #     for edge in e:
+    #         size = gmsh.model.occ.getMass(edge[0], abs(edge[1]))
+    #         if tol/2 <= size:
+    #             sizes.append(size)
+    #             edges.append(edge)
+    #     ratio = max(sizes)/min(sizes)
+    #     if ratio < 100:
+    #         v = gmsh.model.occ.fillet([out[1]], [abs(i[1]) for i in edges], [tol])
+    #         vols.extend(v)
+    #     else:
+    #         vols.append(out)
+    # gmsh.model.occ.synchronize()
+    ov, ovv = gmsh.model.occ.fuse([(3, box_am)], ov)
+    gmsh.model.occ.synchronize()
+    vols = gmsh.model.getEntities(3)
+    box_se = gmsh.model.occ.addBox(-0.5*LX/L_CELL, -0.5*LY/L_CELL, 0, LX/L_CELL, LY/L_CELL, 1)
+    gmsh.model.occ.synchronize()
+    res = gmsh.model.occ.cut([(3, box_se)], vols, removeTool=False)
+    gmsh.model.occ.synchronize()
+    gmsh.model.mesh.removeDuplicateElements()
+    gmsh.model.occ.synchronize()
+    vols = gmsh.model.getEntities(3)
+    gmsh.model.occ.synchronize()
+    centers = []
+    for v in vols:
+        com = gmsh.model.occ.getCenterOfMass(*v)
+        centers.append(com[2])
+    if centers[0] > centers[1]:
+        se_vols = [vols[1][1]]
+        am_vols = [vols[0][1]]
+    else:
+        am_vols = [vols[1][1]]
+        se_vols = [vols[0][1]]
+    # create voids
+    ellipsoids = create_void_phases(args.void_vol_frac, L_slab_am*1e-6, L_SEP*1e-6, LX*1e-6, LY*1e-6, L_CELL*1e-6)
+    res1 = gmsh.model.occ.cut([(3, v) for v in am_vols], ellipsoids, removeTool=False)
+    res2 = gmsh.model.occ.cut([(3, v) for v in se_vols], ellipsoids, removeTool=False)
+    gmsh.model.occ.synchronize()
+    print(res1, res2)
+    left_active = None
+    if img_id is not None:
+        left_active = build_active_contact_area_map(img, scale_x, scale_y, LX, LY, L_CELL)
+        ov, ovv = gmsh.model.occ.fragment([(3, se_vols[0])], [(2, s) for s in left_active], removeTool=False)
+        se_vols = [s[1] for s in ov if s[0] == 3]
+    gmsh.model.occ.synchronize()
+    gmsh.model.addPhysicalGroup(3, se_vols, markers.electrolyte, "electrolyte")
+    gmsh.model.addPhysicalGroup(3, am_vols, markers.positive_am, "positive am")
+    gmsh.model.addPhysicalGroup(3, [v[1] for v in ellipsoids], markers.void, "voids")
+    left = []
+    right = []
+    insulated_am = []
+    insulated_se = []
+    interface = []
+    main_xface = []
+    pieces = []
+    for surf in gmsh.model.getEntities(2):
+        com = gmsh.model.occ.getCenterOfMass(*surf)
+        area = gmsh.model.occ.getMass(*surf)
+        if np.isclose(com[2], 0):
+            left.append(surf[1])
+            continue
+        elif np.isclose(com[2], 1):
+            right.append(surf[1])
+            continue
+        elif np.isclose(com[2], 1 - 0.5 * L_slab_am/L_CELL):
+            insulated_am.append(surf[1])
+            continue
+        elif np.isclose(com[2], 0.5 * (L_CELL - L_slab_am)/L_CELL):
+            insulated_se.append(surf[1])
+            continue
+        else:
+            if np.isclose(area, (LX/L_CELL) ** 2):
+                main_xface.append(surf)
+            else:
+                pieces.append(surf)
+            if not np.isclose(com[2], 1 - L_slab_am/L_CELL, atol=1e-6):
+                interface.append(surf[1])
+            else:
+                interface.append(surf[1])
+    if img_id is not None:
+        left = left_active
+    # if not args.refine:
+    #     boundary = [line[1] for line in gmsh.model.getBoundary([(2, s) for s in left + right + interface + insulated_am + insulated_se], oriented=False)]
+    #     # boundary = []
+    #     # for s in left + right:
+    #     #     loop = gmsh.model.occ.getCurveLoops(s)
+    #     #     for c in loop:
+    #     #         if not isinstance(c, list):
+    #     #             boundary.extend(c.tolist())
+    #     #         else:
+    #     #             boundary.extend(c[0].tolist())
+    #     # boundary = list(set(boundary))
+    #     gmsh.model.mesh.field.add("Distance", 1)
+    #     gmsh.model.mesh.field.setNumbers(1, "CurvesList", boundary)
+    #     gmsh.model.mesh.field.setNumber(1, "Sampling", 100)
+
+    #     gmsh.model.mesh.field.add("Threshold", 2)
+    #     gmsh.model.mesh.field.setNumber(2, "IField", 1)
+    #     gmsh.model.mesh.field.setNumber(2, "LcMin", args.resolution / 5)
+    #     gmsh.model.mesh.field.setNumber(2, "LcMax", args.resolution)
+    #     gmsh.model.mesh.field.setNumber(2, "DistMin", args.resolution/10)
+    #     gmsh.model.mesh.field.setNumber(2, "DistMax", args.resolution)
+    #     gmsh.model.mesh.field.add("Max", 5)
+    #     gmsh.model.mesh.field.setNumbers(5, "FieldsList", [2])
+    #     gmsh.model.mesh.field.setAsBackgroundMesh(5)
+    gmsh.model.occ.synchronize()
+    gmsh.model.addPhysicalGroup(2, left, markers.left, "left")
+    gmsh.model.addPhysicalGroup(2, right, markers.right, "right")
+    gmsh.model.addPhysicalGroup(2, insulated_am, markers.insulated_positive_am, "insulated_positive_am")
+    gmsh.model.addPhysicalGroup(2, insulated_se, markers.insulated_electrolyte, "insulated_electrolyte")
+    gmsh.model.addPhysicalGroup(2, interface, markers.electrolyte_v_positive_am, "electrolyte_v_positive_am")
+    gmsh.model.occ.synchronize()
+
+    if args.refine:
+        distance = gmsh.model.mesh.field.add("Distance")
+        gmsh.model.mesh.field.setNumbers(distance, "FacesList", left + interface + right)
+
+        threshold = gmsh.model.mesh.field.add("Threshold")
+        gmsh.model.mesh.field.setNumber(threshold, "InField", distance)
+        gmsh.model.mesh.field.setNumber(threshold, "LcMin", 0.1/L_CELL)
+        gmsh.model.mesh.field.setNumber(threshold, "LcMax", args.resolution)
+        gmsh.model.mesh.field.setNumber(threshold, "DistMin", 0.1/L_CELL)
+        gmsh.model.mesh.field.setNumber(threshold, "DistMax", args.resolution)
+
+        gmsh.model.mesh.field.add("Max", 5)
+        gmsh.model.mesh.field.setNumbers(5, "FieldsList", [threshold])
+        gmsh.model.mesh.field.setAsBackgroundMesh(5)
+        gmsh.model.occ.synchronize()
+    gmsh.model.mesh.generate(3)
+    # gmsh.model.mesh.optimize("Netgen")
+    gmsh.write(mshpath)
+    gmsh.finalize()
