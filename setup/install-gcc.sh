cbcc52b3c7f803ed781246a6d444b50e0f09b554
Leshinka Molel <emolel3@gatech.edu>
2025-09-26 22:12:52 -0400

    faster mirror

--- setup/install-gcc.sh
+++ setup/install-gcc.sh
@@ -5,7 +5,7 @@ version=13.3.0
 if [ -d $SOFTWARES_DIR/gcc-$version ]; then
     echo 'Folder exists'
 else
-    wget https://ftp.gnu.org/gnu/gcc/gcc-$version/gcc-$version.tar.gz
+    wget https://mirrors.ocf.berkeley.edu/gnu/gcc/gcc-13.3.0/gcc-13.3.0.tar.gz #https://ftp.gnu.org/gnu/gcc/gcc-$version/gcc-$version.tar.gz
     tar xvzf gcc-$version.tar.gz
 fi
 
