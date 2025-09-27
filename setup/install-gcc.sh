9bbfb666d8be7cdabef5bd0d2f493b9d74d2897e
Leshinka Molel <emolel3@gatech.edu>
2025-09-26 22:22:25 -0400

    faster mirror

--- setup/install-gcc.sh
+++ setup/install-gcc.sh
@@ -11,6 +11,6 @@ fi
 
 cd gcc-$version
 
-./configure --prefix=$CMAKE_INSTALL_PREFIX --enable-languages=c,c++,fortran,go
+./configure --prefix=$CMAKE_INSTALL_PREFIX --enable-languages=c,c++,fortran,go --disable-multilib
 make
 make install
