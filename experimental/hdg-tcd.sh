c6bab49044e5cd269226525b15524375359193f1
Leshinka Molel <emolel3@gatech.edu>
2025-10-10 07:16:00 +0300

    pass mesh folder

--- experimental/hdg-tcd.sh
+++ experimental/hdg-tcd.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [[ "$#" -ne 1 ]]; then
-    echo 'Usage: ./hdg-tcd.sh procs'
+if [ "$#" -ne 2 ]; then
+    echo 'Usage: ./hdg-tcd.sh procs mesh_folder'
     exit -1
 fi
 
-echo 'Running for stability parameter '$1
-area_frac=6.30
-gamma=0.01
+area_frac=36.38
+gamma=0.001
+echo 'Running for stability parameter: '$gamma
 for kr in 0.01 1 100; do
-    for Wa_p in 1e-3 1.0 1e3; do
+    for Wa_p in 1e-3 1 1e3; do
         echo 'Processing kr: '$kr ' and Wa_p: '$Wa_p
         rm -r ~/.cache/fenics
-        mpiexec -n $1 python3 prog_copy.py -m ../output/cylinders/20-20-80/0.0125/$area_frac -kr $kr -gamma $gamma -L_C 80e-6 -Wa_p $Wa_p
+        mpiexec -n $1 python3 prog_copy.py -m $2/$area_frac -kr $kr -gamma $gamma -L_C 80e-6 -Wa_p $Wa_p
     done
 done
